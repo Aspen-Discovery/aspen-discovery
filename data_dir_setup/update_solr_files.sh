538ee3f85fa4bf67a14728ccec801f89f19725bf
Mark Noble <mdnoble73@gmail.com>
2019-04-29 06:39:46 -0600

    Update update_solr_files.sh

--- data_dir_setup/update_solr_files.sh
+++ data_dir_setup/update_solr_files.sh
@@ -7,6 +7,9 @@ fi
 echo "Updating $1"
 cp -r solr7 /data/aspen-discovery/$1
 
+echo "cd ../sites/$1"
 cd "../sites/$1"
+echo "exec $1.sh restart"
 exec $1.sh restart
+echo "cd ../../data_dir_setup"
 cd "../../data_dir_setup"
