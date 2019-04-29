b2d4cba07a2b33c8327492718ed765d9a62b6bb0
Mark Noble <mdnoble73@gmail.com>
2019-04-29 06:37:55 -0600

    Update update_solr_files.sh

--- data_dir_setup/update_solr_files.sh
+++ data_dir_setup/update_solr_files.sh
@@ -4,8 +4,9 @@ if [ -z "$1" ]
   then
     echo "Please provide the server name to update as the first argument."
 fi
+echo "Updating $1"
 cp -r solr7 /data/aspen-discovery/$1
 
-cd ../sites/$1
+cd "../sites/$1"
 exec $1.sh restart
-cd ../../data_dir_setup
+cd "../../data_dir_setup"
