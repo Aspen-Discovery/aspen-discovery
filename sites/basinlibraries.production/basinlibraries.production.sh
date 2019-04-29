3f1e8e2bac871944e9292cc7b583362efe905514
Mark Noble <mdnoble73@gmail.com>
2019-04-29 06:45:21 -0600

    Update basinlibraries.production.sh

--- sites/basinlibraries.production/basinlibraries.production.sh
+++ sites/basinlibraries.production/basinlibraries.production.sh
@@ -7,10 +7,10 @@ fi
 
 if [[ ( "$1" == "stop" ) || ( "$1" == "restart") ]]
 	then
-    	runuser solr -c '../default/solr-7.6.0/bin/solr stop -p 8080 -s "/data/aspen-discovery/basinlibraries.production/solr7" -d "/usr/local/aspen-discovery/sites/default/solr-7.6.0/server"'
+    	runuser solr -c '/usr/local/aspen-discovery/sites/default/solr-7.6.0/bin/solr stop -p 8080 -s "/data/aspen-discovery/basinlibraries.production/solr7" -d "/usr/local/aspen-discovery/sites/default/solr-7.6.0/server"'
 fi
 
 if [[ ( "$1" == "start" ) || ( "$1" == "restart") ]]
 	then
-    	runuser solr -c '../default/solr-7.6.0/bin/solr start -m 4g -p 8080 -s "/data/aspen-discovery/basinlibraries.production/solr7" -d "/usr/local/aspen-discovery/sites/default/solr-7.6.0/server"'
+    	runuser solr -c '/usr/local/aspen-discovery/sites/default/solr-7.6.0/bin/solr start -m 4g -p 8080 -s "/data/aspen-discovery/basinlibraries.production/solr7" -d "/usr/local/aspen-discovery/sites/default/solr-7.6.0/server"'
 fi
