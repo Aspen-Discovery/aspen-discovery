a3d02be393c5757567fac3e09640c1ce39e5bd4b
Mark Noble <mdnoble73@gmail.com>
2019-04-30 06:38:42 -0600

    Update start_background_processes.sh

--- sites/basinlibraries.production/start_background_processes.sh
+++ sites/basinlibraries.production/start_background_processes.sh
@@ -5,17 +5,17 @@ if [ -z "$1" ]
     echo "Please provide the server name to update as the first argument."
 fi
 echo "Starting background processes for $1"
-/usr/local/aspen-discovery/sites/basinlibraries.production/basinlibraries.production.sh restart
+/usr/local/aspen-discovery/sites/$1/$1.sh restart
 
 #Wait for solr to restart
 sleep 60
 # Continuous Re-Indexing
 echo "Starting koha export"
-cd /usr/local/aspen-discovery/code/koha_export; java -jar koha_export.jar basinlibraries.production continuous &
+cd /usr/local/aspen-discovery/code/koha_export; java -jar koha_export.jar $1 continuous &
 echo "Starting Overdrive export"
-cd /usr/local/aspen-discovery/code/overdrive_api_extract; java -jar overdrive_extract.jar basinlibraries.production continuous &
+cd /usr/local/aspen-discovery/code/overdrive_api_extract; java -jar overdrive_extract.jar $1 continuous &
 echo "Starting rbdigital export"
-cd /usr/local/aspen-discovery/code/rbdigital_export; java -jar rbdigital_export.jar basinlibraries.production continuous &
+cd /usr/local/aspen-discovery/code/rbdigital_export; java -jar rbdigital_export.jar $1 continuous &
 echo "Starting User List Indexing"
-cd /usr/local/aspen-discovery/code/user_list_indexer; java -jar user_list_indexer.jar basinlibraries.production continuous &
+cd /usr/local/aspen-discovery/code/user_list_indexer; java -jar user_list_indexer.jar $1 continuous &
 
