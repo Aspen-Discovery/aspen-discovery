0a448f34ce8bb08f3c0aed87cc09d0a59ec02c2f
Mark Noble <mdnoble73@gmail.com>
2019-05-02 09:37:23 -0600

    Update updateSuggesters.php

--- code/web/cron/updateSuggesters.php
+++ code/web/cron/updateSuggesters.php
@@ -4,8 +4,8 @@ require_once __DIR__ . '/../bootstrap.php';
 global $configArray;
 $solrBaseUrl = $configArray['Index']['url'];
 
-//Rebuilding can take quite awhile, give each 5 minutes to complete
-set_time_limit(300);
+//Rebuilding can take quite awhile, give each 5 minutes to complete, other than grouped works which we will give 10
+set_time_limit(600);
 file_get_contents($solrBaseUrl . '/grouped_works/suggest?suggest.build=true');
 set_time_limit(300);
 file_get_contents($solrBaseUrl . '/open_archives/suggest?suggest.build=true');
