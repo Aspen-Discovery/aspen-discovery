6161a623d6122ec6ff1f389c9de49c328bca8368
Mark Noble <mdnoble73@gmail.com>
2019-06-03 13:02:37 -0600

    Remove sitemaps from robots.txt since they aren't working right now

--- code/web/robots.php
+++ code/web/robots.php
@@ -5,6 +5,7 @@ global $configArray;
 global $library;
 global $serverName;
 global $interface;
+header('Content-Type:text/plain');
 if ($configArray['Site']['isProduction']){
 	echo(@file_get_contents('robots.txt'));
 
@@ -18,16 +19,17 @@ if ($configArray['Site']['isProduction']){
 		// Open the file to get existing content
 		$current = file_get_contents($file);
 
-		$fileName = $subdomain . '.xml';
-		$siteMap_Url = 'Sitemap: ' . $configArray['Site']['url'] . '/sitemaps/' .$fileName;
-		// Append a new line char
-		echo "\n";
-		//Append the site map index file url
-		echo $siteMap_Url . "\n";
-
-		//Google may want this with a lower case sitemap even though they specify capitalized.  Provide both.
-		$siteMap_Url2 = 'sitemap: ' . $configArray['Site']['url'] . '/sitemaps/' .$fileName;
-		echo $siteMap_Url2 . "\n";
+		//TODO: Figure out how to do site maps?  Make dynamic?
+//		$fileName = $subdomain . '.xml';
+//		$siteMap_Url = 'Sitemap: ' . $configArray['Site']['url'] . '/sitemaps/' .$fileName;
+//		// Append a new line char
+//		echo "\n";
+//		//Append the site map index file url
+//		echo $siteMap_Url . "\n";
+//
+//		//Google may want this with a lower case sitemap even though they specify capitalized.  Provide both.
+//		$siteMap_Url2 = 'sitemap: ' . $configArray['Site']['url'] . '/sitemaps/' .$fileName;
+//		echo $siteMap_Url2 . "\n";
 	}
 }else {
 	echo("User-agent: *\r\nDisallow: /\r\n");
