57b37dbadf2e2183c2a45e1cd07951fec002278f
Mark Noble <mdnoble73@gmail.com>
2019-08-29 07:20:21 -0600

    Update SideLoad.php

--- code/web/sys/Indexing/SideLoad.php
+++ code/web/sys/Indexing/SideLoad.php
@@ -71,6 +71,7 @@ class SideLoad extends DataObject{
 
 			'recordNumberTag' => array('property' => 'recordNumberTag', 'type' => 'text', 'label' => 'Record Number Tag', 'maxLength' => 3, 'description' => 'The MARC tag where the record number can be found', 'required' => true, 'default' => '001'),
             'recordNumberSubfield' => array('property' => 'recordNumberSubfield', 'type' => 'text', 'label' => 'Record Number Subfield', 'maxLength' => 1, 'description' => 'The subfield where the record number is stored', 'required' => true, 'default'=>'a'),
+			'recordNumberPrefix' => array('property' => 'recordNumberPrefix', 'type' => 'text', 'label' => 'Record Number Prefix', 'maxLength' => 10, 'description' => 'A prefix to identify the bib record number if multiple MARC tags exist'),
 
 			'itemSection' => ['property'=>'itemSection', 'type' => 'section', 'label' =>'Item Information', 'hideInLists' => true, 'properties' => [
 				'suppressItemlessBibs' => array('property' => 'suppressItemlessBibs', 'type' => 'checkbox', 'label' => 'Suppress Itemless Bibs', 'description' => 'Whether or not Itemless Bibs can be suppressed', 'default' => false),
