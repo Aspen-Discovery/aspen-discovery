03f8b0a4214e2dfb2e173a5a1f0dbb9485ad0c42
Mark Noble <mdnoble73@gmail.com>
2019-10-26 09:03:52 -0600

    Make sure not to build suggesters on startup

--- code/web/cron/updateSuggesters.php
+++ code/web/cron/updateSuggesters.php
@@ -7,9 +7,9 @@ $solrBaseUrl = $configArray['Index']['url'];
 //Rebuilding can take quite awhile, give each 5 minutes to complete, other than grouped works which we will give 10
 set_time_limit(600);
 file_get_contents($solrBaseUrl . '/grouped_works/suggest?suggest.build=true');
-set_time_limit(300);
+set_time_limit(600);
 file_get_contents($solrBaseUrl . '/open_archives/suggest?suggest.build=true');
-set_time_limit(300);
+set_time_limit(600);
 file_get_contents($solrBaseUrl . '/genealogy/suggest?suggest.build=true');
-set_time_limit(300);
+set_time_limit(600);
 file_get_contents($solrBaseUrl . '/lists/suggest?suggest.build=true');
