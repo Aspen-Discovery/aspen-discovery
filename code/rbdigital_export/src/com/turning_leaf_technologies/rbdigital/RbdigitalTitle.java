e036198cfd40e61cea33faec4bc5e475601347b4
Mark Noble <mdnoble73@gmail.com>
2019-11-25 14:03:02 -0700

    Whitespace fixes

--- code/rbdigital_export/src/com/turning_leaf_technologies/rbdigital/RbdigitalTitle.java
+++ code/rbdigital_export/src/com/turning_leaf_technologies/rbdigital/RbdigitalTitle.java
@@ -1,31 +1,31 @@
 package com.turning_leaf_technologies.rbdigital;
 
 class RbdigitalTitle {
-    private long id;
-    private String rbdigitalId;
-    private long checksum;
-    private boolean deleted;
+	private long id;
+	private String rbdigitalId;
+	private long checksum;
+	private boolean deleted;
 
-    RbdigitalTitle(long id, String rbdigitalId, long checksum, boolean deleted) {
-        this.id = id;
-        this.rbdigitalId = rbdigitalId;
-        this.checksum = checksum;
-        this.deleted = deleted;
-    }
+	RbdigitalTitle(long id, String rbdigitalId, long checksum, boolean deleted) {
+		this.id = id;
+		this.rbdigitalId = rbdigitalId;
+		this.checksum = checksum;
+		this.deleted = deleted;
+	}
 
-    long getId() {
-        return id;
-    }
+	long getId() {
+		return id;
+	}
 
-    String getRbdigitalId() {
-        return rbdigitalId;
-    }
+	String getRbdigitalId() {
+		return rbdigitalId;
+	}
 
-    long getChecksum() {
-        return checksum;
-    }
+	long getChecksum() {
+		return checksum;
+	}
 
-    boolean isDeleted() {
-        return deleted;
-    }
+	boolean isDeleted() {
+		return deleted;
+	}
 }
