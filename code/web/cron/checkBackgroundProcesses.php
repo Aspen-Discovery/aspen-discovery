96fa34c86a74ac74963cc29ef2ef315a24bd743b
Mark Noble <mdnoble73@gmail.com>
2019-12-02 06:12:12 -0700

    Ignore hoopla_export background process since it runs as cron now

--- code/web/cron/checkBackgroundProcesses.php
+++ code/web/cron/checkBackgroundProcesses.php
@@ -81,7 +81,7 @@ while ($module->fetch()){
 }
 
 foreach ($runningProcesses as $process){
-	if ($process['name'] != 'cron' && $process['name'] != 'oai_indexer' && $process['name'] != 'reindexer'){
+	if ($process['name'] != 'cron' && $process['name'] != 'oai_indexer' && $process['name'] != 'reindexer' && $process['name'] != 'hoopla_export'){
 		$results .= "Found process '{$process['name']}' that does not have a module for it\r\n";
 	}
 }
