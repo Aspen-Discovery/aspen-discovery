e8d7f5d3c4c507d39459aecab701a2d96412895d
Mark Noble <mdnoble73@gmail.com>
2019-12-10 18:57:15 -0700

    Sideload updates
    Make it easier to setup sideloads by:
    - showing relationships in 2 directions
    - create a default scope when creating a new sideload
    - Easily add a sideload scope to all libraries
    - Easily add a sideload scope to all locations
    Also
    - Change edit and delete links in oneToMany editor to buttons
    - Move utility methods for updating oneToManyLinks to DataObject

--- code/web/sys/Indexing/LocationSideLoadScope.php
+++ code/web/sys/Indexing/LocationSideLoadScope.php
@@ -18,10 +18,21 @@ class LocationSideLoadScope extends DataObject
 		while ($sideLoadScope->fetch()){
 			$sideLoadScopes[$sideLoadScope->id] = $sideLoadScope->name;
 		}
+		$locationsList = [];
+		$location = new Location();
+		$location->orderBy('displayName');
+		if (UserAccount::userHasRole('libraryAdmin')){
+			$homeLibrary = Library::getPatronHomeLibrary();
+			$location->libraryId = $homeLibrary->libraryId;
+		}
+		$location->find();
+		while ($location->fetch()){
+			$locationsList[$location->locationId] = $location->displayName;
+		}
 		$structure = array(
 			'id' => array('property'=>'id', 'type'=>'label', 'label'=>'Id', 'description'=>'The unique id'),
 			'sideLoadScopeId' => array('property' => 'sideLoadScopeId', 'type' => 'enum', 'values' => $sideLoadScopes, 'label' => 'Side Load Scope', 'description' => 'The Scope to add to the library', 'required' => true),
-			'locationId' => array('property'=>'locationId', 'type'=>'integer', 'label'=>'Location', 'description'=>'The Location to associate the scope to', 'required' => true),
+			'locationId' => array('property'=>'locationId', 'type'=>'enum','values'=>$locationsList, 'label'=>'Location', 'description'=>'The Location to associate the scope to', 'required' => true),
 		);
 		return $structure;
 	}
