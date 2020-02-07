a9230e256e99f936a706c5d8c062e4eb2f8b2b22
Mark Noble <mdnoble73@gmail.com>
2020-02-07 13:31:37 -0700

    Cleanup code

--- code/web/sys/LocalEnrichment/UserWorkReview.php
+++ code/web/sys/LocalEnrichment/UserWorkReview.php
@@ -1,31 +1,32 @@
 <?php
 
 require_once ROOT_DIR . '/sys/DB/DataObject.php';
+
 class UserWorkReview extends DataObject
 {
-    public $__table = 'user_work_review';
-    public $id;
-    public $groupedRecordPermanentId;
-    public $userId;
-    public $rating;
+	public $__table = 'user_work_review';
+	public $id;
+	public $groupedRecordPermanentId;
+	public $userId;
+	public $rating;
 	public $review;
 	public $dateRated;
 
 	private $_displayName;
 
-    /**
-     * @return mixed
-     */
-    public function getDisplayName()
-    {
-        return $this->_displayName;
-    }
+	/**
+	 * @return mixed
+	 */
+	public function getDisplayName()
+	{
+		return $this->_displayName;
+	}
 
-    /**
-     * @param mixed $displayName
-     */
-    public function setDisplayName($displayName): void
-    {
-        $this->_displayName = $displayName;
-    }
+	/**
+	 * @param mixed $displayName
+	 */
+	public function setDisplayName($displayName): void
+	{
+		$this->_displayName = $displayName;
+	}
 }
