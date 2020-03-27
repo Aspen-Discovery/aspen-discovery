bd331b2d591d0d912020354cf25e362d7a97c46c
Mark Noble <mdnoble73@gmail.com>
2020-03-27 12:57:50 -0600

    file cleanup

--- code/web/interface/themes/responsive/js/aspen/base.js
+++ code/web/interface/themes/responsive/js/aspen/base.js
@@ -11,7 +11,7 @@ var AspenDiscovery = (function(){
 		AspenDiscovery.initCarousels();
 
 		$("#modalDialog").modal({show:false});
-		$('[data-toggle="tooltip"]').tooltip()
+		$('[data-toggle="tooltip"]').tooltip();
 
 		$('.panel')
 				.on('show.bs.collapse', function () {
