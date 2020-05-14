0f6e86a5c959e4dba7f40ab4c5e6a32ce11ce5ba
Mark Noble <mdnoble73@gmail.com>
2020-05-14 14:09:24 -0600

    Fix IE compatibility issue

--- code/web/interface/themes/responsive/js/aspen.js
+++ code/web/interface/themes/responsive/js/aspen.js
@@ -1646,7 +1646,7 @@ AspenDiscovery.Account = (function(){
 			return false;
 		},
 
-		updateAutoRenewal(patronId) {
+		updateAutoRenewal:function(patronId) {
 			let url = Globals.path + "/MyAccount/AJAX";
 			let params = {
 				method: "updateAutoRenewal",
