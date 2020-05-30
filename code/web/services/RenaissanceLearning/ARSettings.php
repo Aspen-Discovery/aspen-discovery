0424bbfdc5bedb9eb68fe9f0ea42f96a659872be
Mark Noble <mdnoble73@gmail.com>
2020-05-30 14:53:58 -0600

    Help Manual updates
    Link in additional help pages, remove extra images

--- code/web/services/RenaissanceLearning/ARSettings.php
+++ code/web/services/RenaissanceLearning/ARSettings.php
@@ -6,50 +6,74 @@ require_once ROOT_DIR . '/sys/RenaissanceLearning/ARSetting.php';
 
 class RenaissanceLearning_ARSettings extends ObjectEditor
 {
-    function getObjectType(){
-        return 'ARSetting';
-    }
-    function getToolName(){
-        return 'ARSettings';
-    }
-    function getModule(){
-        return 'RenaissanceLearning';
-    }
-    function getPageTitle(){
-        return 'Accelerated Reader Settings';
-    }
-    function getAllObjects(){
-        $object = new ARSetting();
-        $object->find();
-        $objectList = array();
-        while ($object->fetch()){
-            $objectList[$object->id] = clone $object;
-        }
-        return $objectList;
-    }
-    function getObjectStructure(){
-        return ARSetting::getObjectStructure();
-    }
-    function getPrimaryKeyColumn(){
-        return 'id';
-    }
-    function getIdKeyColumn(){
-        return 'id';
-    }
-    function getAllowableRoles(){
-        return array('opacAdmin', 'cataloging');
-    }
-    function canAddNew(){
-        return UserAccount::userHasRole('opacAdmin');
-    }
-    function canDelete(){
-        return UserAccount::userHasRole('opacAdmin');
-    }
-    function getAdditionalObjectActions($existingObject){
-        return [];
-    }
+	function getObjectType()
+	{
+		return 'ARSetting';
+	}
 
-    function getInstructions(){
-        return '/Admin/HelpManual?page=Accelerated-Reader';
-    }
+	function getToolName()
+	{
+		return 'ARSettings';
+	}
+
+	function getModule()
+	{
+		return 'RenaissanceLearning';
+	}
+
+	function getPageTitle()
+	{
+		return 'Accelerated Reader Settings';
+	}
+
+	function getAllObjects()
+	{
+		$object = new ARSetting();
+		$object->find();
+		$objectList = array();
+		while ($object->fetch()) {
+			$objectList[$object->id] = clone $object;
+		}
+		return $objectList;
+	}
+
+	function getObjectStructure()
+	{
+		return ARSetting::getObjectStructure();
+	}
+
+	function getPrimaryKeyColumn()
+	{
+		return 'id';
+	}
+
+	function getIdKeyColumn()
+	{
+		return 'id';
+	}
+
+	function getAllowableRoles()
+	{
+		return array('opacAdmin', 'cataloging');
+	}
+
+	function canAddNew()
+	{
+		return UserAccount::userHasRole('opacAdmin');
+	}
+
+	function canDelete()
+	{
+		return UserAccount::userHasRole('opacAdmin');
+	}
+
+	function getAdditionalObjectActions($existingObject)
+	{
+		return [];
+	}
+
+	function getInstructions()
+	{
+		return '/Admin/HelpManual?page=Accelerated-Reader';
+	}
 }
