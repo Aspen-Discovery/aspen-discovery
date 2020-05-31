2f8f688a380652e657cbd2316feb3859585605b6
Mark Noble <mdnoble73@gmail.com>
2020-05-30 21:32:54 -0600

    Add required fields for website indexing settings

--- code/web/sys/WebsiteIndexing/WebsiteIndexSetting.php
+++ code/web/sys/WebsiteIndexing/WebsiteIndexSetting.php
@@ -14,16 +14,15 @@ class WebsiteIndexSetting extends DataObject
 
 	public static function getObjectStructure()
 	{
-		$structure = array(
+		return array(
 			'id' => array('property'=>'id', 'type'=>'label', 'label'=>'Id', 'description'=>'The unique id'),
-			'name' => array('property'=>'name', 'type'=>'text', 'label'=>'Name', 'description'=>'The name of the website to index'),
-			'searchCategory' => array('property'=>'searchCategory', 'type'=>'text', 'label'=>'Search Category', 'description'=>'The category of the index.  All sites with the same category will be searched together'),
-			'siteUrl' => array('property'=>'siteUrl', 'type'=>'url', 'label'=>'Site URL', 'description'=>'The URL to the Website'),
+			'name' => array('property'=>'name', 'type'=>'text', 'label'=>'Name', 'description'=>'The name of the website to index', 'required' => true),
+			'searchCategory' => array('property'=>'searchCategory', 'type'=>'text', 'label'=>'Search Category', 'description'=>'The category of the index.  All sites with the same category will be searched together', 'required' => true, 'default' => 'Library Website'),
+			'siteUrl' => array('property'=>'siteUrl', 'type'=>'url', 'label'=>'Site URL', 'description'=>'The URL to the Website', 'required' => true),
 			'pathsToExclude' => array('property' => 'pathsToExclude', 'type'=>'textarea', 'label'=>'Paths to exclude', 'description'=>'A list of paths to exclude from the index with each on it\'s own line.', 'hideInLists' => true),
 			'indexFrequency' => array('property'=>'indexFrequency', 'type'=>'enum', 'values' => ['hourly'=>'Hourly', 'daily'=>'Daily', 'weekly'=>'Weekly', 'monthly'=>'Monthly', 'yearly'=>'Yearly', 'once'=>'Once'], 'label'=>'Frequency to Fetch', 'description'=>'How often the records should be fetched'),
 			'lastIndexed' => array('property'=>'lastIndexed', 'type'=>'integer', 'label'=>'Last Fetched (clear to force a new fetch)', 'description'=>'When the record was last fetched'),
 		);
-		return $structure;
 	}
 
 	public function update()
