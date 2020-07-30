cbdb885d2e8d0b8f278761ce3299f064b8f279a0
James Staub <james.staub@nashville.gov>
2020-07-30 16:12:04 -0500

    Fixes Rosen LevelUP DB Maintenance, again

--- code/web/services/Admin/DBMaintenance.php
+++ code/web/services/Admin/DBMaintenance.php
@@ -2202,7 +2202,7 @@ class Admin_DBMaintenance extends Admin_Admin
 							lu_school_name VARCHAR(50) NOT NULL,						
 							lu_ptypes_1 VARCHAR(50),
 							lu_ptypes_2 VARCHAR(50),
-							lu_ptypes_k VARCHAR(50),
+							lu_ptypes_k VARCHAR(50)
 						) ENGINE = INNODB;'
 					]
 				],
