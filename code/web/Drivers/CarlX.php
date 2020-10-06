46781c099735d423e5dea95d3e96062992e8d406
James Staub <james.staub@nashville.gov>
2020-10-06 14:50:03 -0500

    Fixes crippling typo in HoldsReport
    https://trello.com/c/HGPj4MpX/950-create-custom-fill-list-for-schools-pulling-holds#comment-5f7be5741ec7c42170cc16c2

--- code/web/Drivers/CarlX.php
+++ code/web/Drivers/CarlX.php
@@ -1894,7 +1894,7 @@ class CarlX extends AbstractIlsDriver{
 			and i.status='S'
 			and pb.branchcode = '$location'
 			and t.transcode='R*'
-			and t.renew = ib.branchnumbers
+			and t.renew = ib.branchnumber
 			order by l.locname, i.cn
 EOT;
 		$stid = oci_parse($this->dbConnection, $sql);
