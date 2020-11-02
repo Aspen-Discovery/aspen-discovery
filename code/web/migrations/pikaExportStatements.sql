5d51b843069682ed653f7457d2c3f0195a058962
Mark Noble <mdnoble73@gmail.com>
2020-11-02 11:49:20 -0700

    Update pikaExportStatements.sql

--- code/web/migrations/pikaExportStatements.sql
+++ code/web/migrations/pikaExportStatements.sql
@@ -17,3 +17,28 @@ SELECT cat_username, user_not_interested.dateMarked, grouped_work.full_title, gr
 SELECT primaryUser.cat_username, linkedUser.cat_username, user_link.linkingDisabled FROM user_link inner join user as primaryUser on primaryAccountId = primaryUser.id inner join user as linkedUser on linkedAccountId = linkedUser.id INTO OUTFILE '/data/aspen-discovery/{sitename}/pika_export/patronLinkedUsers.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"';
 
 SELECT primaryUser.cat_username, blockedUser.cat_username, user_link_blocks.blockLinking FROM user_link_blocks inner join user as primaryUser on primaryAccountId = primaryUser.id inner join user as blockedUser on blockedLinkAccountId = blockedUser.id INTO OUTFILE '/data/aspen-discovery/{sitename}/pika_export/patronLinkBlocking.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"';
+
+SELECT user.cat_username, search.id, search.session_id, search.folder_id, search.created, search.title, search.saved, search.search_object, search.searchSource FROM search LEFT JOIN user on search.user_id = user.id where saved = 1 INTO OUTFILE '/data/aspen-discovery/{sitename}/pika_export/saved_searches.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"';
+
+SELECT * from list_widgets INTO OUTFILE '/data/aspen-discovery/{sitename}/pika_export/list_widgets.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"';
+
+SELECT * from list_widget_lists INTO OUTFILE '/data/aspen-discovery/{sitename}/pika_export/list_widget_lists.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"';
+
+SELECT source_grouped_work.full_title, source_grouped_work.author, sourceGroupedWorkId, GROUP_CONCAT(CONCAT(source_grouped_work_primary_identifiers.type, ':', source_grouped_work_primary_identifiers.identifier)) from merged_grouped_works INNER JOIN grouped_work as source_grouped_work on permanent_id = sourceGroupedWorkId inner join grouped_work_primary_identifiers as source_grouped_work_primary_identifiers on grouped_work_id = grouped_work.id group by cat_username, user_not_interested.dateMarked, grouped_work.full_title, grouped_work.author, permanent_id INTO OUTFILE '/data/aspen-discovery/{sitename}/pika_export/patronNotInterested.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"';
+
+SELECT source_grouped_work.full_title as sourceTitle,
+       source_grouped_work.author as sourceAuthor,
+       sourceGroupedWorkId,
+       GROUP_CONCAT(CONCAT(source_grouped_work_primary_identifiers.type, ':', source_grouped_work_primary_identifiers.identifier)),
+       destination_grouped_work.full_title as destinationTitle,
+       destination_grouped_work.author as destinationAuthor,
+       destinationGroupedWorkId,
+       GROUP_CONCAT(CONCAT(destination_grouped_work_primary_identifiers.type, ':', destination_grouped_work_primary_identifiers.identifier)),
+       notes
+from merged_grouped_works
+         INNER JOIN grouped_work as source_grouped_work on source_grouped_work.permanent_id = sourceGroupedWorkId
+         inner join grouped_work_primary_identifiers as source_grouped_work_primary_identifiers on source_grouped_work_primary_identifiers.grouped_work_id = source_grouped_work.id
+         INNER JOIN grouped_work as destination_grouped_work on destination_grouped_work.permanent_id = destinationGroupedWorkId
+         inner join grouped_work_primary_identifiers as destination_grouped_work_primary_identifiers on destination_grouped_work_primary_identifiers.grouped_work_id = destination_grouped_work.id
+GROUP BY sourceTitle, sourceAuthor, sourceGroupedWorkId, destinationTitle, destinationAuthor, destinationGroupedWorkId, notes
+INTO OUTFILE '/data/aspen-discovery/{sitename}/pika_export/mergedGroupedWorks.csv' FIELDS TERMINATED BY ',' ENCLOSED BY '"';
