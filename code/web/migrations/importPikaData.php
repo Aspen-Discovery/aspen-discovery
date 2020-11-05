23fd529223bd8528b316111f21c50d3ada9515b1
Mark Noble <mdnoble73@gmail.com>
2020-11-05 16:46:45 -0700

    Update importPikaData.php

--- code/web/migrations/importPikaData.php
+++ code/web/migrations/importPikaData.php
@@ -167,7 +167,9 @@ function importUsers($startTime, $exportPath, &$existingUsers, &$missingUsers, $
 			//User no longer exists in the ILS
 			$missingUsers[$userFromCSV->cat_username] = $userFromCSV->cat_username;
 		}
-		$existingUser->__destruct();
+		if (!empty($existingUser)) {
+			$existingUser->__destruct();
+		}
 		$existingUser = null;
 		$userFromCSV = null;
 
