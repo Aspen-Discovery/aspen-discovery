3193ed4984dd592fa3c7d02f5c9fb853097ab9b4
Mark Noble <mdnoble73@gmail.com>
2020-12-23 14:54:32 -0700

    Fix marking full index not needed for OverDrive after running the full index

--- code/overdrive_extract/src/com/turning_leaf_technologies/overdrive/ExtractOverDriveInfo.java
+++ code/overdrive_extract/src/com/turning_leaf_technologies/overdrive/ExtractOverDriveInfo.java
@@ -423,7 +423,7 @@ class ExtractOverDriveInfo {
 			lastUpdateTimeParam = lastUpdateTimeParam.substring(0, lastUpdateTimeParam.length() - 2) + ":" + lastUpdateTimeParam.substring(lastUpdateTimeParam.length() - 2);
 		}else{
 			//Update the settings to mark the full update as not needed
-			dbConn.prepareStatement("UPDATE overdrive_settings set runFullUpdate = 0 where settingId = " + settings.getId()).executeUpdate();
+			dbConn.prepareStatement("UPDATE overdrive_settings set runFullUpdate = 0 where id = " + settings.getId()).executeUpdate();
 		}
 
 		PreparedStatement advantageCollectionMapStmt = dbConn.prepareStatement("SELECT libraryId, overdriveAdvantageName, overdriveAdvantageProductsKey FROM library INNER JOIN overdrive_scopes on library.overDriveScopeId = overdrive_scopes.id where overdriveAdvantageName != '' and settingId = ?");
