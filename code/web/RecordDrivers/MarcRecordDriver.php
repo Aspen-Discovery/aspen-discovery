61c7594b0ff639a0d3fd37b37ef29f223038692c
Mark Noble <mdnoble73@gmail.com>
2021-01-10 15:09:07 -0700

    Fix for volume holds

--- code/web/RecordDrivers/MarcRecordDriver.php
+++ code/web/RecordDrivers/MarcRecordDriver.php
@@ -903,13 +903,18 @@ class MarcRecordDriver extends GroupedWorkSubDriver
 			$id = $this->id;
 			if (!is_null($volumeData) && count($volumeData) > 0) {
 				//Check the items to see which volumes are holdable
+				$hasItemsWithoutVolumes = false;
 				$holdableVolumes = [];
 				foreach ($relatedRecord->getItems() as $itemDetail){
-					if ($itemDetail->holdable && !empty($itemDetail->volumeId)){
-						$holdableVolumes[$itemDetail->volumeId] = $itemDetail->volume;
+					if ($itemDetail->holdable) {
+						if (!empty($itemDetail->volumeId)) {
+							$holdableVolumes[$itemDetail->volumeId] = $itemDetail->volume;
+						}else{
+							$hasItemsWithoutVolumes = true;
+						}
 					}
 				}
-				if (count($holdableVolumes) > 3){
+				if (count($holdableVolumes) > 3 || $hasItemsWithoutVolumes){
 					//Show a dialog to enable the patron to select a volume to place a hold on
 					$actions[] = array(
 						'title' => 'Place Hold',
