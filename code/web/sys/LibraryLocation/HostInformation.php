eacacdc057f56445da49b35b8b676bc44a8c223c
Mark Noble <mdnoble73@gmail.com>
2021-05-09 11:57:48 -0600

    Properly utilize themes that have been set per location in addition to themes set at the library level.

--- code/web/sys/LibraryLocation/HostInformation.php
+++ code/web/sys/LibraryLocation/HostInformation.php
@@ -19,7 +19,7 @@ class HostInformation extends DataObject
 			'host' => array('property' => 'host', 'type' => 'text', 'label' => 'Host name', 'description' => 'The name of the host.  I.e. discover.library.org or www.library.org', 'maxLength' => 100, 'required' => true),
 			'libraryId' => array('property' => 'libraryId', 'type'=>'enum','values'=>$libraryList,'label'=> 'Library', 'description' => 'The default library that will be active for this host'),
 			'locationId' => array('property' => 'locationId', 'type'=>'enum','values'=>$locationList,'label'=> 'Location', 'description' => 'The default location that will be active for this host', 'default'=>-1),
-			'defaultPath' => array('property' => 'defaultPath', 'type'=>'text', 'Default Path', 'description' => 'The default path that will be used if no path is provided', 'maxLength' => 50, 'default'=>'/Search/Home'),
+			'defaultPath' => array('property' => 'defaultPath', 'type'=>'text', 'label'=>'Default Path', 'description' => 'The default path that will be used if no path is provided', 'maxLength' => 50, 'default'=>'/Search/Home'),
 		];
 	}
 }
