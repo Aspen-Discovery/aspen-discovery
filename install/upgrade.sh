2887d302ddd1c6cd3cd37fd8541d6a471afc138a
Mark Noble <mdnoble73@gmail.com>
2021-05-13 08:35:53 -0600

    Add new upgrade script to make upgrades easier

---
+++ install/upgrade.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+#Updates a site to a new version
+if [ -z "$1" ]
+  then
+    echo "Please provide the server name to update as the first argument."
+    exit 1
+fi
+if [ -z "$2" ]
+  then
+    echo "Please provide the version number to upgrade to as the second argument."
+    exit 1
+fi
+
+service crond stop
+
+cd /usr/local/aspen-discovery
+git pull origin $2
+
+cd /usr/local/aspen-discovery/install
+if [ -f "/usr/local/aspen-discovery/install/$2.sh" ]; then
+  exec /usr/local/aspen-discovery/install/$2.sh
+fi
+
+echo "Run database maintenance, and then press return when done"
+# shellcheck disable=SC2034
+read waitOver
+
+pkill java
+sudo service mysqld restart
+apachectl restart
+cd data_dir_setup
+./update_solr_files.sh $1
+
+service crond start
+
+echo "Upgrade completed."
+
