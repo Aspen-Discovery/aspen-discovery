d8cf2d44a5a839e04166587bb4a6b6919ebd2123
Mark Noble <mdnoble73@gmail.com>
2021-06-08 10:30:48 -0600

    Update 21.07.01.php

--- code/web/sys/DBMaintenance/version_updates/21.07.01.php
+++ code/web/sys/DBMaintenance/version_updates/21.07.01.php
@@ -3,7 +3,6 @@
 function getUpdates21_07_01() : array
 {
 	return [
-
 		'library_field_permission_updates_21_07_01' => [
 			'title' => 'Library Field Permission Updates 21.07.01',
 			'description' => 'Fix Library Field Permission Updates 21.07.01',
@@ -13,6 +12,15 @@ function getUpdates21_07_01() : array
 				"INSERT INTO role_permissions(roleId, permissionId) VALUES ((SELECT roleId from roles where name  = 'opacAdmin'), (SELECT id from permissions where name='Library Archive Options'))",
 				"INSERT INTO role_permissions(roleId, permissionId) VALUES ((SELECT roleId from roles where name  = 'libraryAdmin'), (SELECT id from permissions where name='Library Archive Options'))",
 			]
-		], //library_archive_permission_21_07_01
+		], //library_field_permission_updates_21_07_01
+		'library_default_materials_request_permissions' => [
+			'title' => 'Library Field Permission updates for materials request',
+			'description' => 'Library Field Permission updates for materials request',
+			'continueOnError' => true,
+			'sql' => [
+				"INSERT INTO role_permissions(roleId, permissionId) VALUES ((SELECT roleId from roles where name  = 'opacAdmin'), (SELECT id from permissions where name='Library Materials Request Options'))",
+				"INSERT INTO role_permissions(roleId, permissionId) VALUES ((SELECT roleId from roles where name  = 'libraryAdmin'), (SELECT id from permissions where name='Library Materials Request Options'))",
+			]
+		], //library_field_permission_updates_21_07_01
 	];
 }
