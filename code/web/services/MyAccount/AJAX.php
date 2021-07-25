b80554aad80a68603ffe676f0c459a6350c7c924
Mark Noble <mdnoble73@gmail.com>
2021-07-25 14:33:19 -0600

    Comprise Updates

--- code/web/services/MyAccount/AJAX.php
+++ code/web/services/MyAccount/AJAX.php
@@ -2948,7 +2948,7 @@ class MyAccount_AJAX extends JSON_Action
 				$paymentRequestUrl .= '&UserName=' . urlencode($compriseSettings->username);
 				$paymentRequestUrl .= '&Password=' . $compriseSettings->password;
 				$paymentRequestUrl .= '&Amount=' . $currencyFormatter->format($payment->totalPaid);
-				$paymentRequestUrl .= "&URLPostBack=" . urlencode($configArray['Site']['url'] . '/MyAccount/CompleteComprisePayment');
+				$paymentRequestUrl .= "&URLPostBack=" . urlencode($configArray['Site']['url'] . '/MyAccount/AJAX?method=completeComprisePayment&payment=' . $payment->id);
 				$paymentRequestUrl .= "&URLReturn=" . urlencode($configArray['Site']['url'] . '/MyAccount/CompriseCompleted?payment=' . $payment->id);
 				$paymentRequestUrl .= "&URLCancel=" . urlencode($configArray['Site']['url'] . '/MyAccount/CompriseCancel?payment=' . $payment->id);
 				$paymentRequestUrl .= '&INVNUM=' . $payment->id;
@@ -2965,11 +2965,91 @@ class MyAccount_AJAX extends JSON_Action
 	}
 
 	function completeComprisePayment() {
-		if (!empty($_REQUEST['INVNUMBER'])) {
+		global $logger;
+		$logger->log("Starting CompleteComprisePayment method", Logger::LOG_ERROR);
+		$success = false;
+		$error = '';
+		$message = '';
+		if (empty($_REQUEST['INVNUM'])) {
+			$error = 'No Payment ID was provided, could not complete the payment';
+		}else{
+			$paymentId = $_REQUEST['INVNUM'];
+			require_once ROOT_DIR . '/sys/Account/UserPayment.php';
+			$userPayment = new UserPayment();
+			$userPayment->id = $paymentId;
+			if ($userPayment->find(true)){
+				if ($userPayment->error || $userPayment->completed || $userPayment->cancelled){
+					$userPayment->error = true;
+					$userPayment->message .= "This payment has already been completed. ";
+				}else{
+					$result = $_REQUEST['RESULT'];
+					$message = $_REQUEST['RESPMSG'];
+					$amountPaid = $_REQUEST['AMT'];
+					$troutD = $_REQUEST['TROUTD'];
+					$authCode = $_REQUEST['AUTHCODE'];
+					$ccNumber = $_REQUEST['CCNUMBER'];
+					if ($amountPaid != $userPayment->totalPaid){
+						$userPayment->message = "Payment amount did not match, was $userPayment->totalPaid, paid $amountPaid. ";
+						$userPayment->totalPaid = $amountPaid;
+					}
+					$user = new User();
+					$user->id = $userPayment->userId;
 
-			global $logger;
-			$logger->log(print_r($_REQUEST, true), Logger::LOG_ERROR);
+					if ($result == 0) {
+						if ($user->find(true)){
+							//Make sure the payment is for the active user or one of the users the primary user is linked to
+							$userIsValid = false;
+							if ($user->id == UserAccount::getActiveUserId()){
+								$userIsValid = true;
+							}else{
+								$activeUser = UserAccount::getActiveUserObj();
+								foreach ($activeUser->getLinkedUsers() as $linkedUser){
+									if ($linkedUser->id == $user->id){
+										$userIsValid = true;
+									}
+								}
+							}
+							if ($userIsValid) {
+								$finePaymentCompleted = $user->completeFinePayment($userPayment);
+								if ($finePaymentCompleted['success']) {
+									$success = true;
+									$message = 'Your payment has been completed. ';
+									$userPayment->message .= "Payment completed, TROUTD = $troutD, AUTHCODE = $authCode, CCNUMBER = $ccNumber. ";
+								} else {
+									$userPayment->error = true;
+									$userPayment->message .= $finePaymentCompleted['message'];
+								}
+							}else{
+								$userPayment->error = true;
+								$userPayment->message .= 'Incorrect user was found, could not update the ILS. ';
+							}
+						}else{
+							$userPayment->error = true;
+							$userPayment->message .= "Could not find user to mark the fine paid in the ILS. ";
+						}
+						$userPayment->completed = true;
+					}else{
+						$userPayment->error = true;
+					}
+				}
+
+				$userPayment->update();
+				if ($userPayment->error){
+					$error = $userPayment->message;
+				}else{
+					$message = $userPayment->message;
+				}
+			}else{
+				$error = 'Incorrect Payment ID provided';
+				global $logger;
+				$logger->log('Incorrect Payment ID provided', Logger::LOG_ERROR);
+			}
 		}
+		$result = [
+			'success' => $success,
+			'message' => $success ? $message : $error
+		];
+		return $result;
 	}
 
 	/** @noinspection PhpUnused */
