7f4a6b0cbfec1b79e99b4c9a9016a6d18a1d55d8
Mark Noble <mdnoble73@gmail.com>
2021-07-30 02:26:55 -0600

    Setup Greenhouse to easily get information about multiple Aspen installations

---
+++ code/web/services/Greenhouse/SiteStatus.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once ROOT_DIR . '/sys/Greenhouse/AspenSite.php';
+class SiteStatus extends Action
+{
+
+	function launch()
+	{
+		$sites = new AspenSite();
+		$sites->whereAdd('implementationStatus != 4 AND implementationStatus != 0');
+		$sites->orderBy('siteType ASC, implementationStatus DESC, name ASC');
+		$sites->find();
+		$siteStatuses = [];
+		$allChecks = [];
+		while ($sites->fetch()){
+			$siteStatus = $sites->getStatus();
+			$siteStatuses[] = $siteStatus;
+			foreach ($siteStatus['checks'] as $key => $check){
+				$allChecks[$key] = $check['name'];
+			}
+		}
+		global $interface;
+		$interface->assign('allChecks', $allChecks);
+		$interface->assign('siteStatuses', $siteStatuses);
+		$this->display('siteStatus.tpl', 'Aspen Site Status',false);
+	}
+
+	function getBreadcrumbs(): array
+	{
+		$breadcrumbs = [];
+		$breadcrumbs[] = new Breadcrumb('/Greenhouse/Home', 'Greenhouse Home');
+		$breadcrumbs[] = new Breadcrumb('/Greenhouse/Sites', 'Sites');
+		$breadcrumbs[] = new Breadcrumb('', 'Status');
+		return $breadcrumbs;
+	}
+}
--- code/web/sys/DBMaintenance/version_updates/21.10.00.php
+++ code/web/sys/DBMaintenance/version_updates/21.10.00.php
@@ -3,6 +3,23 @@
 function getUpdates21_10_00() : array
 {
 	return [
-		
+		'aspen_sites' => [
+			'title' => 'Create Aspen Sites',
+			'description' => 'Create Sites for the greenhouse',
+			'sql' => [
+				'CREATE TABLE IF NOT EXISTS aspen_sites (
+					id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+					name VARCHAR(50) UNIQUE,
+					baseUrl VARCHAR(255) UNIQUE, 
+					siteType INT DEFAULT 0,
+					libraryType INT DEFAULT 0,
+					libraryServes INT DEFAULT 0,
+					implementationStatus INT DEFAULT 0,
+					hosting VARCHAR(75), 
+					operatingSystem VARCHAR(75), 
+					notes TEXT
+				) ENGINE INNODB'
+			]
+		], //aspen_sites
 	];
 }
