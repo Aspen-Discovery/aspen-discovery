dd36bbdd7a9167bb1433c81c855cd7b3539bf962
Mark Noble <mdnoble73@gmail.com>
2021-08-12 13:23:32 -0600

    Finish ProPay payment completion and updating Symphony with the results

--- code/web/sys/Account/UserPayment.php
+++ code/web/sys/Account/UserPayment.php
@@ -85,4 +85,138 @@ class UserPayment extends DataObject
 
 		return $result;
 	}
+
+	public static function completeProPayPayment($queryParams){
+		$paymentId = $_REQUEST['id'];
+		$success = false;
+		$error = '';
+		$message = '';
+
+		require_once ROOT_DIR . '/sys/Account/UserPayment.php';
+		$userPayment = new UserPayment();
+		$userPayment->id = $paymentId;
+		if ($userPayment->find(true)){
+			if (false && $userPayment->completed == true){
+				$success = true;
+				$message = 'Your payment has been completed.';
+			}else{
+				$user = new User();
+				$user->id = $userPayment->userId;
+				if ($user->find(true)) {
+					$userLibrary = $user->getHomeLibrary();
+
+					$proPaySetting = new ProPaySetting();
+					$proPaySetting->id = $userLibrary->proPaySettingId;
+
+					if ($proPaySetting->find(true)) {
+						$proPayResult = $_REQUEST['result'];
+
+						//Get results from ProPay
+						$proPayHostedTransactionIdentifier = $userPayment->orderId;
+						if ($proPaySetting->useTestSystem) {
+							$url = 'https://xmltestapi.propay.com/protectpay/HostedTransactionResults/';
+						}else{
+							$url = 'https://api.propay.com/protectpay/HostedTransactionResults/';
+						}
+						$url .= $proPayHostedTransactionIdentifier;
+
+						$curlWrapper = new CurlWrapper();
+						$authorization = $proPaySetting->billerAccountId . ':' . $proPaySetting->authenticationToken;
+						$authorization = 'Basic ' . base64_encode($authorization);
+						$curlWrapper->addCustomHeaders([
+							'User-Agent: Aspen Discovery',
+							'Accept: application/json',
+							'Cache-Control: no-cache',
+							'Content-Type: application/json',
+							'Accept-Encoding: gzip, deflate',
+							'Authorization: ' . $authorization
+						], true);
+						$hostedTransactionResultsResponse = $curlWrapper->curlGetPage($url);
+						$jsonResponse = null;
+						if ($hostedTransactionResultsResponse && $curlWrapper->getResponseCode() == 200){
+							$jsonResponse = json_decode($hostedTransactionResultsResponse);
+						}
+
+						if ($proPayResult == 'Failure') {
+							$userPayment->completed = true;
+							$userPayment->error = true;
+							$proPayMessage = $_REQUEST['message'];
+							$userPayment->message = $proPayMessage;
+							$userPayment->update();
+							$error = $userPayment->message;
+						} else {
+							$userPayment->completed = true;
+							if ($jsonResponse == null){
+								$userPayment->error = true;
+								$userPayment->message = 'Could not receive transaction response from ProPay.  Please visit the library with your receipt to have the fine removed from your account.';
+							}else{
+								if ($jsonResponse->Result->ResultValue == 'SUCCESS'){
+									$success = true;
+									$amountPaid = $jsonResponse->HostedTransaction->GrossAmt;
+									if ($amountPaid != (int)($userPayment->totalPaid * 100)){
+										$userPayment->message = "Payment amount did not match, was $userPayment->totalPaid, paid $amountPaid. ";
+										$userPayment->totalPaid = $amountPaid;
+									}
+									$user = new User();
+									$user->id = $userPayment->userId;
+									if ($user->find(true)){
+										$finePaymentCompleted = $user->completeFinePayment($userPayment);
+										if ($finePaymentCompleted['success']) {
+											$success = true;
+											$message = 'Your payment has been completed. ';
+											$authCode = $jsonResponse->HostedTransaction->AuthCode;
+											$netAmt = $jsonResponse->HostedTransaction->NetAmt;
+											$transactionId = $jsonResponse->HostedTransaction->TransactionId;
+											if (isset($jsonResponse->HostedTransaction->ObfuscatedAccountNumber)) {
+												$ccNumber = $jsonResponse->HostedTransaction->ObfuscatedAccountNumber;
+											}else{
+												$ccNumber = "Not provided";
+											}
+											$userPayment->message .= "Payment completed, TransactionId = $transactionId, AuthCode = $authCode, CC Number = $ccNumber, Net Amount = $netAmt. ";
+										} else {
+											$success = false;
+											$userPayment->error = true;
+											$userPayment->message .= $finePaymentCompleted['message'];
+										}
+									}else{
+										$userPayment->error = true;
+										$userPayment->message .= "Could not find user to mark the fine paid in the ILS. ";
+									}
+								}else{
+									$userPayment->error = true;
+									$userPayment->message .= "Payment processing failed. " . $jsonResponse->Result->ResultMessage;
+								}
+
+								$userPayment->completed = true;
+							}
+						}
+						if (empty($userPayment->message)) {
+							$error = 'Your payment has not been marked as complete within the system, please contact the library with your receipt to have the payment credited to your account.';
+						} else {
+							if ($userPayment->error){
+								$error = $userPayment->message;
+							}else{
+								if (empty($message)) {
+									$message = $userPayment->message;
+								}
+							}
+						}
+						$userPayment->update();
+					}else{
+						$error = 'Could not find settings for the user payment';
+					}
+				}else{
+					$error = 'Incorrect User for the payment';
+				}
+			}
+		}else{
+			$error = 'Incorrect Payment ID provided';
+		}
+		$result = [
+			'success' => $success,
+			'message' => $success ? $message : $error
+		];
+
+		return $result;
+	}
 }
