642931f9c1e3fbfbf7015db746a9899fb33756a6
Mark Noble <mdnoble73@gmail.com>
2021-08-13 11:34:12 -0600

    Updates to Aspen App backend to handle record and item information being stored in the database

--- code/web/app/aspenDiscover.php
+++ code/web/app/aspenDiscover.php
@@ -11,14 +11,16 @@
 # * include the helper file that holds the URL information by client
 # ****************************************************************************************************************************
 include_once 'config.php';
+require_once '../bootstrap.php';
+require_once '../bootstrap_aspen.php';
 
 # ****************************************************************************************************************************
 # * grab the passed location parameter, then find the path
 # ****************************************************************************************************************************
-$library      = $_GET['library'];
+$library = $_GET['library'];
 $locationInfo = urlPath($library);
-$urlPath      = $locationInfo[0];
-$shortname    = $locationInfo[1];
+$urlPath = $locationInfo[0];
+$shortname = $locationInfo[1];
 
 # ****************************************************************************************************************************
 # * give the number of results to return from the search - needed to accomodate for the culling of Hoopla and Kanopy
@@ -29,7 +31,9 @@ $searchLimit = 100;
 # * grab the parameters needed and clean it up ... need to default it to something too if there is nothing there
 # ****************************************************************************************************************************
 $browseCat = $_GET['limiter'];
-if (empty($browseCat)) { $browseCat = 'main_new_this_week'; }
+if (empty($browseCat)) {
+	$browseCat = 'main_new_this_week';
+}
 
 # ****************************************************************************************************************************
 # * search link to the catalogue
@@ -45,54 +49,64 @@ $jsonData = json_decode(file_get_contents($reportURL), true);
 # * loop over results and massage
 # * - help: https://stackoverflow.com/questions/6964403/parsing-json-with-php
 # ****************************************************************************************************************************
-foreach($jsonData['result']['records'] as $item) {
-  $author      = $item['author_display'];
-  
-# ****************************************************************************************************************************
-# * collection code may be empty - need to dummy it out just in case
-# ****************************************************************************************************************************
-  $ccode       = '';
-  if (isset($item['collection_' . $shortname][0])) { $ccode = $item['collection_' . $shortname][0]; }
+require_once ROOT_DIR . '/RecordDrivers/GroupedWorkDriver.php';
+$searchResults = [
+	'Items' => []
+];
 
-  $format      = $item['format_' . $shortname][0];
-  $iconName    = $urlPath . "/bookcover.php?id=" . $item['id'] . "&size=medium&type=grouped_work";
-  $id          = $item['id'];
-  
-# ****************************************************************************************************************************
-# * clean up the summary to remove some of the &# codes
-# ****************************************************************************************************************************
-  $summary     = utf8_encode(trim(strip_tags($item['display_description'])));
-  $summary     = str_replace('&#8211;', ' - ', $summary);
-  $summary     = str_replace('&#8212;', ' - ', $summary);
-  $summary     = str_replace('&#160;', ' ', $summary);
-  
-  $title       = ucwords($item['title_display']);
-  unset($itemList);
+if (isset($jsonData['result']['records'])) {
+	foreach ($jsonData['result']['records'] as $item) {
+		$groupedWork = new GroupedWorkDriver($item);
+		$author = $item['author_display'];
 
-# ****************************************************************************************************************************
-# * need to parse over the bib records
-# ****************************************************************************************************************************
-  foreach($item['record_details'] as $itemRecords) {
-    if (strpos($itemRecords, 'ils:') > -1 || strpos($itemRecords, 'overdrive:') > -1) {
-      $itemListing = explode('|', $itemRecords);
-	  
-	  //if (! is_array($itemList)) {
-	  if (! isset($itemList)) {
-	    $itemList[] = array('type' => $itemListing[0], 'name' => $itemListing[1]);
-	  } elseif (! in_array($itemListing[1], array_column($itemList, 'name'))) {
-        $itemList[] = array('type' => $itemListing[0], 'name' => $itemListing[1]);
-      }
-	} 
-  }
-  
-# ****************************************************************************************************************************
-# * Build out results array ... ensure we have at least one item available
-# ****************************************************************************************************************************
-  if (! empty($itemList)) {
-	if (count($itemList) > 0) {
-	  $searchResults['Items'][] = array('title' => trim($title), 'author' => $author, 'image' => $iconName, 'format' => $format . ' - ' . $ccode, 'itemList' => $itemList, 'key' => $id, 'summary' => $summary); 
+		# ****************************************************************************************************************************
+		# * collection code may be empty - need to dummy it out just in case
+		# ****************************************************************************************************************************
+		$ccode = '';
+		if (isset($item['collection_' . $shortname][0])) {
+			$ccode = $item['collection_' . $shortname][0];
+		}
+
+		$format = $item['format_' . $shortname][0];
+		$iconName = $urlPath . "/bookcover.php?id=" . $item['id'] . "&size=medium&type=grouped_work";
+		$id = $item['id'];
+
+		# ****************************************************************************************************************************
+		# * clean up the summary to remove some of the &# codes
+		# ****************************************************************************************************************************
+		$summary = utf8_encode(trim(strip_tags($item['display_description'])));
+		$summary = str_replace('&#8211;', ' - ', $summary);
+		$summary = str_replace('&#8212;', ' - ', $summary);
+		$summary = str_replace('&#160;', ' ', $summary);
+
+		$title = ucwords($item['title_display']);
+		unset($itemList);
+
+		# ****************************************************************************************************************************
+		# * need to parse over the bib records
+		# ****************************************************************************************************************************
+		$relatedRecords = $groupedWork->getRelatedRecords();
+		foreach ($relatedRecords as $relatedRecord) {
+			if (strpos($relatedRecord->id, 'ils:') > -1 || strpos($relatedRecord->id, 'overdrive:') > -1) {
+
+				//if (! is_array($itemList)) {
+				if (!isset($itemList)) {
+					$itemList[] = array('type' => $relatedRecord->id, 'name' => $relatedRecord->format);
+				} elseif (!in_array($relatedRecord->format, array_column($itemList, 'name'))) {
+					$itemList[] = array('type' => $relatedRecord->id, 'name' => $relatedRecord->format);
+				}
+			}
+		}
+
+		# ****************************************************************************************************************************
+		# * Build out results array ... ensure we have at least one item available
+		# ****************************************************************************************************************************
+		if (!empty($itemList)) {
+			if (count($itemList) > 0) {
+				$searchResults['Items'][] = array('title' => trim($title), 'author' => $author, 'image' => $iconName, 'format' => $format . ' - ' . $ccode, 'itemList' => $itemList, 'key' => $id, 'summary' => $summary);
+			}
+		}
 	}
-  } 
 }
 
 # ****************************************************************************************************************************
