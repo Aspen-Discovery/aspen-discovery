68fea84261e45d8d4a57214ebec5ae5e9872f426
Kirstien Kroeger <kirstien@bywatersolutions.com>
2021-08-19 21:38:28 -0500

    rename db update to clarity

--- code/web/sys/DBMaintenance/version_updates/21.10.01.php
+++ code/web/sys/DBMaintenance/version_updates/21.10.01.php
@@ -3,12 +3,12 @@
 function getUpdates21_10_01() : array
 {
 	return [
-		'appAccess' => [
+		'greenhouse_appAccess' => [
 			'title' => 'Add app access level options',
 			'description' => 'Create app access level for the greenhouse',
 			'sql' => [
 				'ALTER TABLE aspen_sites ADD COLUMN appAccess TINYINT(1) DEFAULT 0'
 			]
-		], //appAccess
+		], //greenhouse_appAccess
 	];
 }
