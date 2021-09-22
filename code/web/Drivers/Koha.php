6ed10117fe179a04e5fd5fa8d6cc5c706ca4c2e1
Kirstien Kroeger <kirstien@bywatersolutions.com>
2021-09-22 18:05:28 -0500

    Fix date formatting when sending dates to Koha API

--- code/web/Drivers/Koha.php
+++ code/web/Drivers/Koha.php
@@ -2712,13 +2712,15 @@ class Koha extends AbstractIlsDriver
 		if (strlen($date) == 0) {
 			return $date;
 		} else {
-			if (strpos($date, '-') !== false){
-				list($month, $day, $year) = explode('-', $date);
-				return "$year-$month-$day";
+			if (strpos($date, '/') !== false){
+				list($month, $day, $year) = explode('/', $date);
+				$formattedDate = "$year-$month-$day";
+				return $formattedDate;
 			}else{
 				return $date;
 			}
 		}
+
 	}
 
 	/**
