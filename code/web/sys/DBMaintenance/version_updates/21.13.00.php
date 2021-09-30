87579053cc16f64a15893cd58749842668e48040
Mark Noble <mdnoble73@gmail.com>
2021-09-30 14:08:59 -0600

    Updates to provide support for extended UTF8 characters

--- code/web/sys/DBMaintenance/version_updates/21.13.00.php
+++ code/web/sys/DBMaintenance/version_updates/21.13.00.php
@@ -2,6 +2,8 @@
 /** @noinspection PhpUnused */
 function getUpdates21_13_00() : array
 {
+	global $configArray;
+
 	return [
 		/*'name' => [
 			'title' => '',
@@ -31,5 +33,78 @@ function getUpdates21_13_00() : array
 				"UPDATE indexing_profiles set runFullUpdate = 1 WHERE indexingClass = 'Polaris'"
 			]
 		], //polaris_full_update_21_13
+		'utf8mb4support' => [
+			'title' => 'UTF-8 MB4 support',
+			'description' => 'Support emojis and other extended characters within the database',
+			'continueOnError' => true,
+			'sql' => [
+				"ALTER DATABASE " . $configArray['Database']['database_aspen_dbname'] . " DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;",
+				"ALTER TABLE cached_values CHANGE COLUMN value value VARCHAR(16000)",
+				"ALTER TABLE grouped_work_alternate_titles CHANGE COLUMN alternateTitle alternateTitle VARCHAR(709)",
+				"ALTER TABLE indexed_edition DROP INDEX edition",
+				"ALTER TABLE indexed_edition DROP INDEX edition_2",
+				"ALTER TABLE indexed_edition MODIFY COLUMN edition VARCHAR(1000) collate utf8mb4_bin",
+				"ALTER TABLE indexed_physicaldescription DROP INDEX physicalDescription",
+				"ALTER TABLE indexed_physicaldescription DROP INDEX physicalDescription_2",
+				"ALTER TABLE indexed_physicaldescription MODIFY COLUMN physicalDescription VARCHAR(1000) collate utf8mb4_bin",
+				"ALTER TABLE session MODIFY COLUMN created DATETIME DEFAULT CURRENT_TIMESTAMP",
+				"ALTER TABLE translation_terms DROP INDEX term",
+				"ALTER TABLE translation_terms MODIFY COLUMN term VARCHAR(1000) collate utf8mb4_bin",
+				"updateAllTablesToUtf8mb4",
+				"ALTER TABLE indexed_edition ADD INDEX edition(edition(500))",
+				"ALTER TABLE indexed_physicaldescription ADD INDEX physicalDescription (physicalDescription(500))",
+				"ALTER TABLE translation_terms ADD INDEX term (term(500))",
+			]
+		],
 	];
+}
+
+function updateAllTablesToUtf8mb4(&$update)
+{
+	global $configArray;
+	set_time_limit(0);
+
+	global $aspen_db;
+	$updateOk = true;
+	$result = $aspen_db->query("show tables", PDO::FETCH_ASSOC);
+	$allTables = $result->fetchAll();
+	foreach ($allTables as $tableInfo){
+		$tableName = reset($tableInfo);
+		try {
+			//Get a list of the collations for all tables to see if we need to preserve binary collations
+			$modifyClause = '';
+			$columnInfoResult = $aspen_db->query("SELECT * FROM information_schema.COLUMNS WHERE table_schema = '{$configArray['Database']['database_aspen_dbname']}' AND table_name = '{$tableName}'", PDO::FETCH_ASSOC);
+			$allColumnInfo = $columnInfoResult->fetchAll();
+			foreach ($allColumnInfo as $columnInfo){
+				if ($columnInfo['COLLATION_NAME'] == 'utf8_bin' || $columnInfo['COLLATION_NAME'] == 'utf8mb4_bin'){
+					$modifyClause .= ", MODIFY {$columnInfo['COLUMN_NAME']} {$columnInfo['COLUMN_TYPE']} CHARACTER SET utf8mb4 COLLATE utf8mb4_bin";
+					if ($columnInfo['IS_NULLABLE']){
+						$modifyClause .= " NULL";
+					}else{
+						$modifyClause .= " NOT NULL";
+					}
+					if ($columnInfo['COLUMN_DEFAULT'] != null){
+						$modifyClause .= " DEFAULT '{$columnInfo['COLUMN_DEFAULT']}'" ;
+					}
+				}
+			}
+			if (empty($modifyClause)) {
+				$fullSQL = "ALTER TABLE $tableName CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci";
+			}else{
+				$fullSQL = "ALTER TABLE $tableName DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci $modifyClause";
+			}
+			$aspen_db->exec($fullSQL);
+		}catch (Exception $e){
+			if (isset($update['continueOnError']) && $update['continueOnError']) {
+				if (!isset($update['status'])) {
+					$update['status'] = '';
+				}
+				$update['status'] .= '<br/><strong>' . $tableName . '</strong> failed to update to utf8mb4 <br/> - ' .$e->getMessage() . '<br/>';
+			} else {
+				$update['status'] = '<br/><strong>' . $tableName . '</strong> failed to update to utf8mb4 <br/> - '. $e->getMessage() . '<br/>';
+				$updateOk = false;
+			}
+		}
+	}
+	return $updateOk;
 }
