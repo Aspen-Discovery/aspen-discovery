8e85cf8f9512a057eb8c938ba6d6d3ebe66bc854
Kirstien Kroeger <kirstien@bywatersolutions.com>
2021-10-19 10:06:46 -0500

    Update 21.13.02.php
    Add in title/description (oops)

--- code/web/sys/DBMaintenance/version_updates/21.13.02.php
+++ code/web/sys/DBMaintenance/version_updates/21.13.02.php
@@ -13,8 +13,8 @@ function getUpdates21_13_02() : array
 			]
 		], //sample*/
 		'addContactEmail' => [
-			'title' => '',
-			'description' => '',
+			'title' => 'Add contact email',
+			'description' => 'Add contact email to location and library tables',
 			'sql' => [
 				"ALTER TABLE location ADD COLUMN contactEmail VARCHAR(250)",
 				"ALTER TABLE library ADD COLUMN contactEmail VARCHAR(250)",
