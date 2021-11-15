73c00965eb8149dacfafc53b5d3b2d6fe218a627
Kirstien Kroeger <kirstien@bywatersolutions.com>
2021-11-15 09:13:57 -0600

    Add option to show/hide Libby promo ad during fulfillment

--- code/web/sys/DBMaintenance/version_updates/21.15.00.php
+++ code/web/sys/DBMaintenance/version_updates/21.15.00.php
@@ -31,5 +31,24 @@ function getUpdates21_15_00() : array
 				'ALTER TABLE overdrive_settings ADD COLUMN useFulfillmentInterface TINYINT(1) DEFAULT 0',
 			]
 		], //overdrive_useFulfillmentInterface
+		'browseCategoryDismissal' => [
+			'title' => 'Add browse_category_dismissal table',
+			'description' => 'Enables the ability to hide browse categories by the user',
+			'sql' => [
+				'CREATE TABLE browse_category_dismissal (
+							id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+							browseCategoryId INT,
+							userId INT,
+							UNIQUE INDEX userBrowseCategory(userId, browseCategoryId)
+						) ENGINE = INNODB;',
+			]
+		], //browseCategoryDismissal
+		'overdrive_showLibbyPromo' => [
+			'title' => 'OverDrive - Enable show/hide Libby promo',
+			'description' => 'Enable show/hide option for Libby promo in OverDrive fulfillment interface',
+			'sql' => [
+				'ALTER TABLE overdrive_settings ADD COLUMN showLibbyPromo TINYINT(1) DEFAULT 1',
+			]
+		], //overdrive_showLibbyPromo
 	];
 }
