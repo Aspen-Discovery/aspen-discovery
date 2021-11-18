a5d749b2c9ceb51f9e3d7820e3400e20996ebbea
Mark Noble <mdnoble73@gmail.com>
2021-11-18 16:58:33 -0700

    Site Status page can now only show sites/checks with errors.

---
+++ code/web/sys/ECommerce/MSBSetting.php
@@ -0,0 +1,108 @@
+<?php
+
+
+class MSBSetting extends DataObject
+{
+	public $__table = 'msb_settings';
+	public $id;
+	public $name;
+	public $msbUrl;
+
+	private $_libraries;
+
+	static function getObjectStructure() : array {
+		$libraryList = Library::getLibraryList(!UserAccount::userHasPermission('Administer All Libraries'));
+
+		$structure = array(
+			'id' => array('property' => 'id', 'type' => 'label', 'label' => 'Id', 'description' => 'The unique id'),
+			'name' => array('property' => 'name', 'type' => 'text', 'label' => 'Name', 'description' => 'A name for the settings', 'maxLength' => 50),
+			'msbUrl' => array('property'=>'msbUrl', 'type'=>'url', 'label'=>'MSB URL', 'description'=>'URL for connection to MSB', 'hideInLists' => true,),
+
+			'libraries' => array(
+				'property' => 'libraries',
+				'type' => 'multiSelect',
+				'listStyle' => 'checkboxSimple',
+				'label' => 'Libraries',
+				'description' => 'Define libraries that use these settings',
+				'values' => $libraryList,
+				'hideInLists' => true,
+				'forcesReindex' => true
+			),
+		);
+
+		if (!UserAccount::userHasPermission('Library eCommerce Options')){
+			unset($structure['libraries']);
+		}
+		return $structure;
+	}
+
+	public function __get($name){
+		if ($name == "libraries") {
+			if (!isset($this->_libraries) && $this->id){
+				$this->_libraries = [];
+				$obj = new Library();
+				$obj->msbPalSettingId = $this->id;
+				$obj->find();
+				while($obj->fetch()){
+					$this->_libraries[$obj->libraryId] = $obj->libraryId;
+				}
+			}
+			return $this->_libraries;
+		} else {
+			return $this->_data[$name];
+		}
+	}
+
+	public function __set($name, $value){
+		if ($name == "libraries") {
+			$this->_libraries = $value;
+		}else {
+			$this->_data[$name] = $value;
+		}
+	}
+
+	public function update()
+	{
+		$ret = parent::update();
+		if ($ret !== FALSE) {
+			$this->saveLibraries();
+		}
+		return true;
+	}
+
+	public function insert()
+	{
+		$ret = parent::insert();
+		if ($ret !== FALSE) {
+			$this->saveLibraries();
+		}
+		return $ret;
+	}
+
+	public function saveLibraries(){
+		if (isset ($this->_libraries) && is_array($this->_libraries)){
+			$libraryList = Library::getLibraryList(!UserAccount::userHasPermission('Administer All Libraries'));
+			foreach ($libraryList as $libraryId => $displayName){
+				$library = new Library();
+				$library->libraryId = $libraryId;
+				$library->find(true);
+				if (in_array($libraryId, $this->_libraries)){
+					//We want to apply the scope to this library
+					if ($library->msbPalSettingId != $this->id){
+						$library->finePaymentType = 2;
+						$library->msbPalSettingId = $this->id;
+						$library->update();
+					}
+				}else{
+					//It should not be applied to this scope. Only change if it was applied to the scope
+					if ($library->msbPalSettingId == $this->id){
+						if ($library->finePaymentType == 2) {$library->finePaymentType = 0;}
+						$library->msbPalSettingId = -1;
+						$library->update();
+					}
+				}
+			}
+			unset($this->_libraries);
+		}
+	}
+}
