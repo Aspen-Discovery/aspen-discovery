e1f81dc3b86125914b86acb3b8c6fc6490ea9f8a
Mark Noble <mdnoble73@gmail.com>
2021-11-29 16:04:06 -0700

    Fix scoping for course reserves searching

--- code/web/sys/LibraryLocation/Library.php
+++ code/web/sys/LibraryLocation/Library.php
@@ -975,7 +975,7 @@ class Library extends DataObject
 			$scopingSetting = $searchSource;
 			if ($scopingSetting == null){
 				return null;
-			} else if ($scopingSetting == 'local' || $scopingSetting == 'econtent' || $scopingSetting == 'library' || $scopingSetting == 'location' || $scopingSetting == 'websites' || $scopingSetting == 'lists' || $scopingSetting == 'open_archives'){
+			} else if ($scopingSetting == 'local' || $scopingSetting == 'econtent' || $scopingSetting == 'library' || $scopingSetting == 'location' || $scopingSetting == 'websites' || $scopingSetting == 'lists' || $scopingSetting == 'open_archives' || $scopingSetting == 'course_reserves'){
 				Library::$searchLibrary[$searchSource] = Library::getActiveLibrary();
 			}else if ($scopingSetting == 'marmot' || $scopingSetting == 'unscoped'){
 				//Get the default library
