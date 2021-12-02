e29449dad47c4d165c6147bc3c52f46cb1407d97
Kirstien Kroeger <kirstien@bywatersolutions.com>
2021-12-02 03:21:49 -0600

    Updates to app config files

--- code/aspen_app/babel.config.js
+++ code/aspen_app/babel.config.js
@@ -1,6 +1,16 @@
-module.exports = function (api) {
-	api.cache(true);
-	return {
-		presets: ["babel-preset-expo"],
-	};
+// @generated: @expo/next-adapter@3.1.10
+// Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#shared-steps
+
+module.exports = {
+    presets: ['@expo/next-adapter/babel'],
+    plugins: [
+        ["module:react-native-dotenv", {
+            "moduleName": "@env",
+            "path": ".env",
+            "blacklist": null,
+            "whitelist": null,
+            "safe": false,
+            "allowUndefined": true
+        }]
+    ]
 };
