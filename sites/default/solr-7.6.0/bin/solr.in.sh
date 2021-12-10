1b4c66a10d679e8c955d221314f8eb088cb9f213
Mark Noble <mdnoble73@gmail.com>
2021-12-10 15:02:53 -0700

    Log4J updates

--- sites/default/solr-7.6.0/bin/solr.in.sh
+++ sites/default/solr-7.6.0/bin/solr.in.sh
@@ -80,6 +80,7 @@
 #SOLR_OPTS="$SOLR_OPTS -Dsolr.autoSoftCommit.maxTime=3000"
 #SOLR_OPTS="$SOLR_OPTS -Dsolr.autoCommit.maxTime=60000"
 #SOLR_OPTS="$SOLR_OPTS -Dsolr.clustering.enabled=true"
+SOLR_OPTS="$SOLR_OPTS -Dlog4j2.formatMsgNoLookups=true"
 
 # Location where the bin/solr script will save PID files for running instances
 # If not set, the script will create PID files in $SOLR_TIP/bin
