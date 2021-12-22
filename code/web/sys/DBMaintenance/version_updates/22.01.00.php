a00dbf893da463b8af9f0b5c15251783d7e21346
Kirstien Kroeger <kirstien@bywatersolutions.com>
2021-12-22 12:50:54 -0600

    Add 2fa tables

--- code/web/sys/DBMaintenance/version_updates/22.01.00.php
+++ code/web/sys/DBMaintenance/version_updates/22.01.00.php
@@ -74,5 +74,39 @@ function getUpdates22_01_00() : array
 				"ALTER TABLE layout_settings ADD COLUMN contrastRatio VARCHAR(5) DEFAULT 4.50"
 			]
 		], //layout_settings_contrast
+		'two_factor_auth' => [
+			'title' => 'Add tables for 2FA',
+			'description' => 'Add tables to store data related to 2FA',
+			'sql' => [
+				'CREATE TABLE IF NOT EXISTS two_factor_auth_codes (
+					id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+					userId INT(11),
+					sessionId VARCHAR(128),
+					code VARCHAR(7),
+					dateSent INT(12),
+					status VARCHAR(75) DEFAULT NULL /* sent, confirmed, expired, backup? */
+				) ENGINE INNODB',
+				'CREATE TABLE IF NOT EXISTS two_factor_auth_settings (
+					id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+					name VARCHAR(50) UNIQUE,
+					isEnabled VARCHAR(25) DEFAULT null,
+					authMethods VARCHAR(75) DEFAULT null,
+					deniedMessage LONGTEXT,
+					defaultRememberMe TINYINT(1) DEFAULT 0
+				) ENGINE INNODB',
+				"ALTER TABLE user ADD COLUMN twoFactorStatus int(1) DEFAULT 0", // 0 = not set, 1 = opted out, 2 = opted in
+				"ALTER TABLE user ADD COLUMN primaryTwoFactor varchar(25) DEFAULT null",
+				"ALTER TABLE user ADD COLUMN authLocked TINYINT(1) DEFAULT 0",
+				"ALTER TABLE ptype ADD COLUMN twoFactorAuthSettingId int(11) DEFAULT -1",
+				"ALTER TABLE library ADD COLUMN twoFactorAuthSettingId int(11) DEFAULT -1",
+			]
+		], //two_factor_auth
+		'two_factor_auth_permission' => [
+			'title' => 'Add permission setting for 2FA',
+			'description' => 'Add permission setting for managing two-factor authentication',
+			'sql' => [
+				"INSERT INTO permissions (sectionName, name, requiredModule, weight, description) VALUES ('Primary Configuration', 'Administer Two-Factor Authentication', '', 90, 'Controls if the user can change Two-Factor Authentication settings. <em>This has potential security and cost implications.</em>')",
+			]
+		], //two_factor_auth_permission
 	];
 }
