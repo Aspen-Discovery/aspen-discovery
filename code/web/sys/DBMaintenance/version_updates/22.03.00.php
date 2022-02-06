512148258e46b98140a5c7551c7019dc5a5bc475
Mark Noble <mdnoble73@gmail.com>
2022-02-06 14:43:43 -0700

    Increase length of library display name field

--- code/web/sys/DBMaintenance/version_updates/22.03.00.php
+++ code/web/sys/DBMaintenance/version_updates/22.03.00.php
@@ -18,5 +18,12 @@ function getUpdates22_03_00() : array
 				"INSERT INTO modules (name, indexName, backgroundProcess,logClassPath,logClassName) VALUES ('FOLIO', 'grouped_works', 'folio_export','/sys/ILS/IlsExtractLogEntry.php', 'IlsExtractLogEntry')",
 			]
 		], //evergreen_folio_modules
+		'library_displayName_length' => [
+			'title' => 'Increase Library Display Name Length',
+			'description' => 'Increase Library Display Name Length',
+			'sql' => [
+				'ALTER TABLE library change COLUMN displayName displayName VARCHAR(80) COLLATE utf8mb4_general_ci NOT NULL '
+			]
+		], //library_displayName_length
 	];
 }
