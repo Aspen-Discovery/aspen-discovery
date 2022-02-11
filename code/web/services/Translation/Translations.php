611aeabf9983914ffc5dacf191a1d89a50dafc57
Mark Noble <mdnoble73@gmail.com>
2022-02-11 08:48:58 -0700

    Do not timeout when exporting translations

--- code/web/services/Translation/Translations.php
+++ code/web/services/Translation/Translations.php
@@ -89,6 +89,7 @@ class Translation_Translations extends Admin_Admin
 
 	private function exportAllTranslations()
 	{
+		set_time_limit(0);
 		header('Content-type: application/csv');
 		header('Cache-Control: no-cache, must-revalidate'); // HTTP/1.1
 		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
