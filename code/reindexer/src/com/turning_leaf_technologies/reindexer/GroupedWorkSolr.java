c70299d8b0324e4b4f585335f53cff851aea6cbc
Mark Noble <mdnoble73@gmail.com>
2022-03-10 17:48:45 -0700

    Error logging while indexing

--- code/reindexer/src/com/turning_leaf_technologies/reindexer/GroupedWorkSolr.java
+++ code/reindexer/src/com/turning_leaf_technologies/reindexer/GroupedWorkSolr.java
@@ -216,225 +216,231 @@ public class GroupedWorkSolr implements Cloneable {
 		doc.addField("last_indexed", new Date());
 		doc.addField("alternate_ids", alternateIds);
 		doc.addField("recordtype", "grouped_work");
+		try {
+			//Title and variations
+			String fullTitle = title;
+			if (subTitle != null) {
+				fullTitle += " " + subTitle;
+			}
+			doc.addField("title", fullTitle);
+			doc.addField("title_display", displayTitle);
+			//This is set lower now with additional titles added with formats
+			//doc.addField("title_full", fullTitles);
 
-		//Title and variations
-		String fullTitle = title;
-		if (subTitle != null) {
-			fullTitle += " " + subTitle;
-		}
-		doc.addField("title", fullTitle);
-		doc.addField("title_display", displayTitle);
-		//This is set lower now with additional titles added with formats
-		//doc.addField("title_full", fullTitles);
+			doc.addField("subtitle_display", subTitle);
+			doc.addField("title_short", title);
+			doc.addField("title_sort", titleSort);
+			doc.addField("title_alt", titleAlt);
+			doc.addField("title_old", titleOld);
+			doc.addField("title_new", titleNew);
 
-		doc.addField("subtitle_display", subTitle);
-		doc.addField("title_short", title);
-		doc.addField("title_sort", titleSort);
-		doc.addField("title_alt", titleAlt);
-		doc.addField("title_old", titleOld);
-		doc.addField("title_new", titleNew);
+			//author and variations
+			doc.addField("auth_author", authAuthor);
+			doc.addField("author", getPrimaryAuthor());
 
-		//author and variations
-		doc.addField("auth_author", authAuthor);
-		doc.addField("author", getPrimaryAuthor());
+			doc.addField("auth_author2", authAuthor2);
+			doc.addField("author2", author2);
+			doc.addField("author2-role", author2Role);
+			doc.addField("author_additional", authorAdditional);
+			doc.addField("author_display", authorDisplay);
+			//format
+			doc.addField("grouping_category", groupingCategory);
 
-		doc.addField("auth_author2", authAuthor2);
-		doc.addField("author2", author2);
-		doc.addField("author2-role", author2Role);
-		doc.addField("author_additional", authorAdditional);
-		doc.addField("author_display", authorDisplay);
-		//format
-		doc.addField("grouping_category", groupingCategory);
+			doc.addField("format_boost", getTotalFormatBoost());
 
-		doc.addField("format_boost", getTotalFormatBoost());
+			//language related fields
+			//Check to see if we have Unknown plus a valid value
+			if (languages.size() > 1 || groupedWorkIndexer.getTreatUnknownLanguageAs().length() != 0) {
+				languages.remove("Unknown");
+			}
+			if (languages.size() == 0) {
+				languages.add(groupedWorkIndexer.getTreatUnknownLanguageAs());
+			}
+			doc.addField("language", languages);
+			doc.addField("translation", translations);
+			doc.addField("language_boost", languageBoost);
+			doc.addField("language_boost_es", languageBoostSpanish);
+			//Publication related fields
+			doc.addField("publisher", publishers);
+			doc.addField("publishDate", publicationDates);
+			//Sorting will use the earliest date published
+			doc.addField("publishDateSort", earliestPublicationDate);
 
-		//language related fields
-		//Check to see if we have Unknown plus a valid value
-		if (languages.size() > 1 || groupedWorkIndexer.getTreatUnknownLanguageAs().length() != 0) {
-			languages.remove("Unknown");
-		}
-		if (languages.size() == 0){
-			languages.add(groupedWorkIndexer.getTreatUnknownLanguageAs());
-		}
-		doc.addField("language", languages);
-		doc.addField("translation", translations);
-		doc.addField("language_boost", languageBoost);
-		doc.addField("language_boost_es", languageBoostSpanish);
-		//Publication related fields
-		doc.addField("publisher", publishers);
-		doc.addField("publishDate", publicationDates);
-		//Sorting will use the earliest date published
-		doc.addField("publishDateSort", earliestPublicationDate);
+			//faceting and refined searching
+			doc.addField("physical", physicals);
+			doc.addField("edition", editions);
+			doc.addField("dateSpan", dateSpans);
+			doc.addField("series", series.values());
+			doc.addField("series2", series2.values());
+			doc.addField("series_with_volume", seriesWithVolume.values());
+			doc.addField("topic", topics);
+			doc.addField("topic_facet", topicFacets);
+			doc.addField("subject_facet", subjects);
+			doc.addField("lc_subject", lcSubjects);
+			doc.addField("bisac_subject", bisacSubjects);
+			doc.addField("genre", genres);
+			doc.addField("genre_facet", genreFacets);
+			doc.addField("geographic", geographic);
+			doc.addField("geographic_facet", geographicFacets);
+			doc.addField("era", eras);
+			checkDefaultValue(literaryFormFull, "Not Coded");
+			checkDefaultValue(literaryFormFull, "Other");
+			checkDefaultValue(literaryFormFull, "Unknown");
+			checkInconsistentLiteraryFormsFull();
+			doc.addField("literary_form_full", literaryFormFull.keySet());
+			checkDefaultValue(literaryForm, "Not Coded");
+			checkDefaultValue(literaryForm, "Other");
+			checkDefaultValue(literaryForm, "Unknown");
+			checkInconsistentLiteraryForms();
+			doc.addField("literary_form", literaryForm.keySet());
+			if (targetAudienceFull.size() > 1 || !groupedWorkIndexer.isTreatUnknownAudienceAsUnknown()) {
+				targetAudienceFull.remove("Unknown");
+			}
+			if (targetAudienceFull.size() > 1) {
+				targetAudienceFull.remove("No Attempt To Code");
+				targetAudienceFull.remove("Other");
+			}
+			if (targetAudienceFull.size() == 0) {
+				targetAudienceFull.add(groupedWorkIndexer.getTreatUnknownAudienceAs());
+			}
+			doc.addField("target_audience_full", targetAudienceFull);
+			if (targetAudience.size() > 1 || !groupedWorkIndexer.isTreatUnknownAudienceAsUnknown()) {
+				targetAudience.remove("Unknown");
+			}
+			if (targetAudience.size() > 1) {
+				targetAudience.remove("Other");
+			}
+			if (targetAudience.size() == 0) {
+				targetAudience.add(groupedWorkIndexer.getTreatUnknownAudienceAs());
+			}
+			doc.addField("target_audience", targetAudience);
+			doc.addField("system_list", systemLists);
+			//Date added to catalog
+			Date dateAdded = getDateAdded();
+			doc.addField("date_added", dateAdded);
 
-		//faceting and refined searching
-		doc.addField("physical", physicals);
-		doc.addField("edition", editions);
-		doc.addField("dateSpan", dateSpans);
-		doc.addField("series", series.values());
-		doc.addField("series2", series2.values());
-		doc.addField("series_with_volume", seriesWithVolume.values());
-		doc.addField("topic", topics);
-		doc.addField("topic_facet", topicFacets);
-		doc.addField("subject_facet", subjects);
-		doc.addField("lc_subject", lcSubjects);
-		doc.addField("bisac_subject", bisacSubjects);
-		doc.addField("genre", genres);
-		doc.addField("genre_facet", genreFacets);
-		doc.addField("geographic", geographic);
-		doc.addField("geographic_facet", geographicFacets);
-		doc.addField("era", eras);
-		checkDefaultValue(literaryFormFull, "Not Coded");
-		checkDefaultValue(literaryFormFull, "Other");
-		checkDefaultValue(literaryFormFull, "Unknown");
-		checkInconsistentLiteraryFormsFull();
-		doc.addField("literary_form_full", literaryFormFull.keySet());
-		checkDefaultValue(literaryForm, "Not Coded");
-		checkDefaultValue(literaryForm, "Other");
-		checkDefaultValue(literaryForm, "Unknown");
-		checkInconsistentLiteraryForms();
-		doc.addField("literary_form", literaryForm.keySet());
-		if (targetAudienceFull.size() > 1 || !groupedWorkIndexer.isTreatUnknownAudienceAsUnknown()) {
-			targetAudienceFull.remove("Unknown");
-		}
-		if (targetAudienceFull.size() > 1) {
-			targetAudienceFull.remove("No Attempt To Code");
-			targetAudienceFull.remove("Other");
-		}
-		if (targetAudienceFull.size() == 0){
-			targetAudienceFull.add(groupedWorkIndexer.getTreatUnknownAudienceAs());
-		}
-		doc.addField("target_audience_full", targetAudienceFull);
-		if (targetAudience.size() > 1 || !groupedWorkIndexer.isTreatUnknownAudienceAsUnknown()) {
-			targetAudience.remove("Unknown");
-		}
-		if (targetAudience.size() > 1) {
-			targetAudience.remove("Other");
-		}
-		if (targetAudience.size() == 0){
-			targetAudience.add(groupedWorkIndexer.getTreatUnknownAudienceAs());
-		}
-		doc.addField("target_audience", targetAudience);
-		doc.addField("system_list", systemLists);
-		//Date added to catalog
-		Date dateAdded = getDateAdded();
-		doc.addField("date_added", dateAdded);
-
-		//Check to see if all items are on order.  If so, add on order keywords
-		boolean allItemsOnOrder = true;
-		int numItems = 0;
-		for (RecordInfo record : relatedRecords.values()) {
-			for (ItemInfo item : record.getRelatedItems()) {
-				numItems++;
-				if (!(item.isOrderItem() || (item.getStatusCode() != null && item.getStatusCode().equals("On Order")))) {
-					allItemsOnOrder = false;
+			//Check to see if all items are on order.  If so, add on order keywords
+			boolean allItemsOnOrder = true;
+			int numItems = 0;
+			for (RecordInfo record : relatedRecords.values()) {
+				for (ItemInfo item : record.getRelatedItems()) {
+					numItems++;
+					if (!(item.isOrderItem() || (item.getStatusCode() != null && item.getStatusCode().equals("On Order")))) {
+						allItemsOnOrder = false;
+					}
+				}
+				if (record.getFormatCategories().size() > 0) {
+					fullTitles.add(fullTitle + " " + record.getFormatCategories().toString());
 				}
 			}
-			if (record.getFormatCategories().size() > 0) {
-				fullTitles.add(fullTitle + " " + record.getFormatCategories().toString());
+			doc.addField("title_full", fullTitles);
+
+			if (numItems == 0) {
+				allItemsOnOrder = false;
 			}
-		}
-		doc.addField("title_full", fullTitles);
-
-		if (numItems == 0) {
-			allItemsOnOrder = false;
-		}
-		if (allItemsOnOrder) {
-			addKeywords("On Order");
-			addKeywords("Coming Soon");
-			doc.addField("days_since_added", -1);
-			doc.addField("time_since_added", "On Order");
-		} else {
-			//Check to see if all items are either on order or
-			if (dateAdded == null) {
-				//Determine date added based on publication date
-				if (earliestPublicationDate != null) {
-					//Return number of days since the given year
-					Calendar publicationDate = GregorianCalendar.getInstance();
-					publicationDate.set(earliestPublicationDate.intValue(), Calendar.JANUARY, 1);
-
-					long indexTime = new Date().getTime();
-					long publicationTime = publicationDate.getTime().getTime();
-					long bibDaysSinceAdded = (indexTime - publicationTime) / (long) (1000 * 60 * 60 * 24);
-					doc.addField("days_since_added", Long.toString(bibDaysSinceAdded));
-					doc.addField("time_since_added", DateUtils.getTimeSinceAddedForDate(publicationDate.getTime()));
-				} else {
-					doc.addField("days_since_added", Long.toString(Integer.MAX_VALUE));
-				}
+			if (allItemsOnOrder) {
+				addKeywords("On Order");
+				addKeywords("Coming Soon");
+				doc.addField("days_since_added", -1);
+				doc.addField("time_since_added", "On Order");
 			} else {
-				doc.addField("days_since_added", DateUtils.getDaysSinceAddedForDate(dateAdded));
-				doc.addField("time_since_added", DateUtils.getTimeSinceAddedForDate(dateAdded));
+				//Check to see if all items are either on order or
+				if (dateAdded == null) {
+					//Determine date added based on publication date
+					if (earliestPublicationDate != null) {
+						//Return number of days since the given year
+						Calendar publicationDate = GregorianCalendar.getInstance();
+						publicationDate.set(earliestPublicationDate.intValue(), Calendar.JANUARY, 1);
+
+						long indexTime = new Date().getTime();
+						long publicationTime = publicationDate.getTime().getTime();
+						long bibDaysSinceAdded = (indexTime - publicationTime) / (long) (1000 * 60 * 60 * 24);
+						doc.addField("days_since_added", Long.toString(bibDaysSinceAdded));
+						doc.addField("time_since_added", DateUtils.getTimeSinceAddedForDate(publicationDate.getTime()));
+					} else {
+						doc.addField("days_since_added", Long.toString(Integer.MAX_VALUE));
+					}
+				} else {
+					doc.addField("days_since_added", DateUtils.getDaysSinceAddedForDate(dateAdded));
+					doc.addField("time_since_added", DateUtils.getTimeSinceAddedForDate(dateAdded));
+				}
 			}
+
+			doc.addField("barcode", barcodes);
+			//Awards and ratings
+			doc.addField("mpaa_rating", mpaaRatings);
+			doc.addField("awards_facet", awards);
+			if (lexileScore.length() == 0) {
+				doc.addField("lexile_score", -1);
+			} else {
+				doc.addField("lexile_score", lexileScore);
+			}
+			if (lexileCode.length() > 0) {
+				doc.addField("lexile_code", StringUtils.trimTrailingPunctuation(lexileCode));
+			}
+			if (fountasPinnell.length() > 0) {
+				doc.addField("fountas_pinnell", fountasPinnell);
+			}
+			doc.addField("accelerated_reader_interest_level", StringUtils.trimTrailingPunctuation(acceleratedReaderInterestLevel));
+			if (StringUtils.isNumeric(acceleratedReaderReadingLevel)) {
+				doc.addField("accelerated_reader_reading_level", acceleratedReaderReadingLevel);
+			}
+			if (StringUtils.isNumeric(acceleratedReaderPointValue)) {
+				doc.addField("accelerated_reader_point_value", acceleratedReaderPointValue);
+			}
+			HashSet<String> eContentSources = getAllEContentSources();
+			keywords.addAll(eContentSources);
+
+			keywords.addAll(isbns.keySet());
+			keywords.addAll(oclcs);
+			keywords.addAll(barcodes);
+			keywords.addAll(issns);
+			keywords.addAll(lccns);
+			keywords.addAll(upcs.keySet());
+
+			HashSet<String> callNumbers = getAllCallNumbers();
+			keywords.addAll(callNumbers);
+			doc.addField("keywords", Util.getCRSeparatedStringFromSet(keywords));
+
+			doc.addField("table_of_contents", contents);
+			//broad search terms
+			//identifiers
+			doc.addField("lccn", lccns);
+			doc.addField("oclc", oclcs);
+			//Get the primary isbn
+			doc.addField("primary_isbn", primaryIsbn);
+			doc.addField("isbn", isbns.keySet());
+			doc.addField("issn", issns);
+			doc.addField("primary_upc", getPrimaryUpc());
+			doc.addField("upc", upcs.keySet());
+
+			//call numbers
+			doc.addField("callnumber-first", callNumberFirst);
+			doc.addField("callnumber-subject", callNumberSubject);
+			//relevance determiners
+			doc.addField("popularity", Long.toString((long) popularity));
+			doc.addField("total_holds", Long.toString(totalHolds));
+			doc.addField("num_holdings", numHoldings);
+			//aspen-discovery enrichment
+			doc.addField("rating", rating == -1f ? 2.5 : rating);
+			doc.addField("rating_facet", getRatingFacet(rating));
+
+			//Links to users
+			doc.addField("user_rating_link", userRatingLink);
+			doc.addField("user_not_interested_link", userNotInterestedLink);
+			doc.addField("user_reading_history_link", userReadingHistoryLink);
+
+			doc.addField("description", Util.getCRSeparatedString(description));
+			doc.addField("display_description", displayDescription);
+		}catch (Exception e){
+			logEntry.incErrors("Error creating solr document for grouped work " + id, e);
 		}
-
-		doc.addField("barcode", barcodes);
-		//Awards and ratings
-		doc.addField("mpaa_rating", mpaaRatings);
-		doc.addField("awards_facet", awards);
-		if (lexileScore.length() == 0) {
-			doc.addField("lexile_score", -1);
-		} else {
-			doc.addField("lexile_score", lexileScore);
+		try{
+			//Save information from scopes
+			addScopedFieldsToDocument(doc, logEntry);
+		}catch (Exception e){
+			logEntry.incErrors("Error adding scoped fields to grouped work " + id, e);
 		}
-		if (lexileCode.length() > 0) {
-			doc.addField("lexile_code", StringUtils.trimTrailingPunctuation(lexileCode));
-		}
-		if (fountasPinnell.length() > 0) {
-			doc.addField("fountas_pinnell", fountasPinnell);
-		}
-		doc.addField("accelerated_reader_interest_level", StringUtils.trimTrailingPunctuation(acceleratedReaderInterestLevel));
-		if (StringUtils.isNumeric(acceleratedReaderReadingLevel)) {
-			doc.addField("accelerated_reader_reading_level", acceleratedReaderReadingLevel);
-		}
-		if (StringUtils.isNumeric(acceleratedReaderPointValue)) {
-			doc.addField("accelerated_reader_point_value", acceleratedReaderPointValue);
-		}
-		HashSet<String> eContentSources = getAllEContentSources();
-		keywords.addAll(eContentSources);
-
-		keywords.addAll(isbns.keySet());
-		keywords.addAll(oclcs);
-		keywords.addAll(barcodes);
-		keywords.addAll(issns);
-		keywords.addAll(lccns);
-		keywords.addAll(upcs.keySet());
-
-		HashSet<String> callNumbers = getAllCallNumbers();
-		keywords.addAll(callNumbers);
-		doc.addField("keywords", Util.getCRSeparatedStringFromSet(keywords));
-
-		doc.addField("table_of_contents", contents);
-		//broad search terms
-		//identifiers
-		doc.addField("lccn", lccns);
-		doc.addField("oclc", oclcs);
-		//Get the primary isbn
-		doc.addField("primary_isbn", primaryIsbn);
-		doc.addField("isbn", isbns.keySet());
-		doc.addField("issn", issns);
-		doc.addField("primary_upc", getPrimaryUpc());
-		doc.addField("upc", upcs.keySet());
-
-		//call numbers
-		doc.addField("callnumber-first", callNumberFirst);
-		doc.addField("callnumber-subject", callNumberSubject);
-		//relevance determiners
-		doc.addField("popularity", Long.toString((long) popularity));
-		doc.addField("total_holds", Long.toString(totalHolds));
-		doc.addField("num_holdings", numHoldings);
-		//aspen-discovery enrichment
-		doc.addField("rating", rating == -1f ? 2.5 : rating);
-		doc.addField("rating_facet", getRatingFacet(rating));
-
-		//Links to users
-		doc.addField("user_rating_link", userRatingLink);
-		doc.addField("user_not_interested_link", userNotInterestedLink);
-		doc.addField("user_reading_history_link", userReadingHistoryLink);
-
-		doc.addField("description", Util.getCRSeparatedString(description));
-		doc.addField("display_description", displayDescription);
-
-		//Save information from scopes
-		addScopedFieldsToDocument(doc, logEntry);
 
 		return doc;
 	}
