22d55ad5c59b08abf623c2263bca865d188619f2
James Staub <tinlibrarian@gmail.com>
2022-03-17 14:26:27 -0500

    Adds Calendar link to Events breadcrumbs

--- code/web/services/Events/Results.php
+++ code/web/services/Events/Results.php
@@ -180,6 +180,24 @@ class Events_Results extends ResultsAction
 
 	function getBreadcrumbs() : array
 	{
-		return parent::getResultsBreadcrumbs('Events Search');
-	}
+        global $interface;
+        $breadcrumbs = [];
+        $breadcrumbs[] = new Breadcrumb('/Events/Calendar', 'Calendar');
+        $breadcrumbs[] = new Breadcrumb(null, 'Events Search');
+        $recordCount = $interface->getVariable('recordCount');
+        if (empty($recordCount)) {
+            $resultCountText = translate(['text'=>"No Results Found", "isPublicFacing" => true]);
+        }else{
+            if ($interface->getVariable('displayMode') == 'covers') {
+                $resultCountText = translate(['text' => "There are %1% total results.", 1=>number_format($recordCount), "isPublicFacing" => true]);
+            }else{
+                $recordStart = number_format($interface->getVariable('recordStart'));
+                $recordEnd = number_format($interface->getVariable('recordEnd'));
+                $recordCount = number_format($interface->getVariable('recordCount'));
+                $resultCountText = translate(['text'=>"Showing %1% - %2% of %3%", 1=>$recordStart, 2=>$recordEnd, 3=>$recordCount, "isPublicFacing" => true]);
+            }
+        }
+        $breadcrumbs[] = new Breadcrumb(null, $resultCountText, false);
+        return $breadcrumbs;
+    }
 }
