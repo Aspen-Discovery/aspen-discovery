bf0814c632f336a81e672776e50b4f7eb3e735d9
Mark Noble <mdnoble73@gmail.com>
2022-03-20 01:54:05 -0600

    Update ExportAspenData.php

--- code/web/services/Greenhouse/ExportAspenData.php
+++ code/web/services/Greenhouse/ExportAspenData.php
@@ -255,6 +255,8 @@ class Greenhouse_ExportAspenData extends Admin_Admin
 		];
 
 		if (isset($_REQUEST['submit'])){
+			set_time_limit(0);
+			ini_set('memory_limit','4G');
 			$submissionResults = [
 				'success' => false,
 				'message' => 'Nothing was exported'
