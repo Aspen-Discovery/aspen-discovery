65c8ff1a282c916746611aec1b9a32114c437341
Mark Noble <mdnoble73@gmail.com>
2022-04-26 13:42:33 -0600

    Add a default filter to tickets page to hide closed tickets

--- code/web/services/Admin/ObjectEditor.php
+++ code/web/services/Admin/ObjectEditor.php
@@ -398,7 +398,8 @@ abstract class ObjectEditor extends Admin_Admin
 	abstract function getDefaultSort() : string;
 
 	public function canFilter($objectStructure){
-		return ($this->getNumObjects() > 3) || (count($this->getAppliedFilters($objectStructure)) > 0);
+		$filterFields = $this->getFilterFields($objectStructure);
+		return ($this->getNumObjects() > 3) || (count($this->getAppliedFilters($filterFields)) > 0);
 	}
 
 	public function customListActions(){
@@ -595,13 +596,13 @@ abstract class ObjectEditor extends Admin_Admin
 	}
 
 	public function getFilterFields($structure){
-		$sortFields = [];
+		$filterFields = [];
 		$structure = $this->applyPermissionsToObjectStructure($structure);
 		foreach ($structure as $fieldName => $field){
-			$this->addFieldToFilterFieldsArray($sortFields, $field);
+			$this->addFieldToFilterFieldsArray($filterFields, $field);
 		}
-		ksort($sortFields);
-		return $sortFields;
+		ksort($filterFields);
+		return $filterFields;
 	}
 
 	private function addFieldToFilterFieldsArray(&$filterFields, $field){
@@ -630,37 +631,49 @@ abstract class ObjectEditor extends Admin_Admin
 				];
 			}
 		}
+		if (count($appliedFilters) == 0){
+			$appliedFilters = $this->getDefaultFilters($filterFields);
+		}
 		return $appliedFilters;
 	}
 
+	function getDefaultFilters(array $filterFields) : array{
+		return [];
+	}
+
 	function applyFilters(DataObject $object){
-		$appliedFilters = $this->getAppliedFilters($object::getObjectStructure());
+		$filterFields = $this->getFilterFields($object::getObjectStructure());
+		$appliedFilters = $this->getAppliedFilters($filterFields);
 		foreach ($appliedFilters as $fieldName => $filter){
-			if ($filter['filterType'] == 'matches'){
-				$object->$fieldName = $filter['filterValue'];
-			}elseif ($filter['filterType'] == 'contains'){
-				$object->whereAdd($fieldName . ' like ' . $object->escape('%' . $filter['filterValue'] . '%'));
-			}elseif ($filter['filterType'] == 'startsWith'){
-				$object->whereAdd($fieldName . ' like ' . $object->escape($filter['filterValue'] . '%'));
-			}elseif ($filter['filterType'] == 'beforeTime'){
-				$fieldValue = strtotime($filter['filterValue2']);
-				if ($fieldValue !== false) {
-					$object->whereAdd($fieldName . ' < ' . $fieldValue);
-				}
-			}elseif ($filter['filterType'] == 'afterTime'){
-				$fieldValue = strtotime($filter['filterValue']);
-				if ($fieldValue !== false) {
-					$object->whereAdd($fieldName . ' > ' . $fieldValue);
-				}
-			}elseif ($filter['filterType'] == 'betweenTimes'){
-				$fieldValue = strtotime($filter['filterValue']);
-				if ($fieldValue !== false) {
-					$object->whereAdd($fieldName . ' > ' . $fieldValue);
-				}
-				$fieldValue2 = strtotime($filter['filterValue2']);
-				if ($fieldValue2 !== false) {
-					$object->whereAdd($fieldName . ' < ' . $fieldValue2);
-				}
+			$this->applyFilter($object, $fieldName, $filter);
+		}
+	}
+
+	function applyFilter(DataObject $object, string $fieldName, array $filter){
+		if ($filter['filterType'] == 'matches'){
+			$object->$fieldName = $filter['filterValue'];
+		}elseif ($filter['filterType'] == 'contains'){
+			$object->whereAdd($fieldName . ' like ' . $object->escape('%' . $filter['filterValue'] . '%'));
+		}elseif ($filter['filterType'] == 'startsWith'){
+			$object->whereAdd($fieldName . ' like ' . $object->escape($filter['filterValue'] . '%'));
+		}elseif ($filter['filterType'] == 'beforeTime'){
+			$fieldValue = strtotime($filter['filterValue2']);
+			if ($fieldValue !== false) {
+				$object->whereAdd($fieldName . ' < ' . $fieldValue);
+			}
+		}elseif ($filter['filterType'] == 'afterTime'){
+			$fieldValue = strtotime($filter['filterValue']);
+			if ($fieldValue !== false) {
+				$object->whereAdd($fieldName . ' > ' . $fieldValue);
+			}
+		}elseif ($filter['filterType'] == 'betweenTimes'){
+			$fieldValue = strtotime($filter['filterValue']);
+			if ($fieldValue !== false) {
+				$object->whereAdd($fieldName . ' > ' . $fieldValue);
+			}
+			$fieldValue2 = strtotime($filter['filterValue2']);
+			if ($fieldValue2 !== false) {
+				$object->whereAdd($fieldName . ' < ' . $fieldValue2);
 			}
 		}
 	}
