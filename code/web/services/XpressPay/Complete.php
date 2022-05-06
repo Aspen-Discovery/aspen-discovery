64d33ff20d66376dd150bc00587941f8ed9863f6
Kirstien Kroeger <kirstien@bywatersolutions.com>
2022-05-06 16:11:37 -0500

    Check for GET from XpressPay

--- code/web/services/XpressPay/Complete.php
+++ code/web/services/XpressPay/Complete.php
@@ -7,17 +7,21 @@ class XpressPay_Complete extends Action
 		global $interface;
 		$error = '';
 		$message = '';
-		if (empty($_REQUEST['l1'])) {
-			$error = 'No Payment ID was provided, could not complete the payment';
-		}else{
-			require_once ROOT_DIR . '/sys/Account/UserPayment.php';
-			$result = UserPayment::completeXpressPayPayment($_REQUEST);
-			if ($result['success']){
-				$message = $result['message'];
-			}else {
-				$error = $result['message'];
+
+		if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+			if (empty($_REQUEST['l1'])) {
+				$error = 'No Payment ID was provided, could not complete the payment';
+			}else{
+				require_once ROOT_DIR . '/sys/Account/UserPayment.php';
+				$result = UserPayment::completeXpressPayPayment($_REQUEST);
+				if ($result['success']){
+					$message = $result['message'];
+				}else {
+					$error = $result['message'];
+				}
 			}
 		}
+
 		$interface->assign('error', $error);
 		$interface->assign('message', $message);
 		$this->display('paymentCompleted.tpl', 'Payment Completed');
