f026fd49ba44c19b731190c278f404d98bebb70c
Mark Noble <mdnoble73@gmail.com>
2022-06-10 10:44:33 -0600

    Misc updates
    - Remove times used from reading history
    - Update suggesters to use new solr core

--- code/web/cron/updateSuggesters.php
+++ code/web/cron/updateSuggesters.php
@@ -6,7 +6,13 @@ $solrBaseUrl = $configArray['Index']['url'];
 
 //Rebuilding can take quite awhile, give each 5 minutes to complete, other than grouped works which we will give 10
 set_time_limit(0);
-file_get_contents($solrBaseUrl . '/grouped_works/suggest?suggest.build=true');
+require_once ROOT_DIR . '/sys/SystemVariables.php';
+$systemVariables = SystemVariables::getSystemVariables();
+if ($systemVariables->searchVersion == 1) {
+	file_get_contents($solrBaseUrl . '/grouped_works/suggest?suggest.build=true');
+}else{
+	file_get_contents($solrBaseUrl . '/grouped_works_v2/suggest?suggest.build=true');
+}
 file_get_contents($solrBaseUrl . '/open_archives/suggest?suggest.build=true');
 file_get_contents($solrBaseUrl . '/genealogy/suggest?suggest.build=true');
 file_get_contents($solrBaseUrl . '/lists/suggest?suggest.build=true');
--- code/web/sys/DBMaintenance/version_updates/22.06.02.php
+++ code/web/sys/DBMaintenance/version_updates/22.06.02.php
@@ -18,5 +18,17 @@ function getUpdates22_06_02() : array
 				"INSERT INTO grouped_work_scheduled_index (permanent_id,processed, indexAfter) SELECT permanent_id, 0, $curTime FROM grouped_work where permanent_id like '%|||'",
 			]
 		], //force_reindex_of_records_with_pipe_language
+//		'fix_list_entries_for_grouped_works_with_language' =>[
+//			'title' => 'Fix List Entries for Grouped Works With Language',
+//			'description' => 'Fix List Entries for Grouped Works With Language',
+//			'sql' => [
+//				"fixListEntriesForGroupedWorksWithLanguage",
+//			]
+//		], //fix_list_entries_for_grouped_works_with_language
 	];
 }
+
+//function fixListEntriesForGroupedWorksWithLanguage(){
+//	require_once ROOT_DIR . '/sys/UserLists/UserListEntry.php';
+//	UserListEntry
+//}
