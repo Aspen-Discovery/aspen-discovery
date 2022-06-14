0f002f20f4ed7c8ad99caba90885ea390e68bed1
Mark Noble <mdnoble73@gmail.com>
2022-06-14 10:56:47 -0600

    Update regrouping logic for side loaded records
    Also remove ID from grouped_work_record_item_url

---
+++ code/web/sys/DBMaintenance/version_updates/22.06.04.php
@@ -0,0 +1,29 @@
+<?php
+/** @noinspection PhpUnused */
+function getUpdates22_06_04() : array
+{
+	$curTime = time();
+	return [
+		/*'name' => [
+			'title' => '',
+			'description' => '',
+			'sql' => [
+				''
+			]
+		], //sample*/
+		'remove_id_from_grouped_work_record_item_url' => [
+			'title' => 'Remove id from grouped_work_record_item_url',
+			'description' => 'Remove id for grouped_work_record_item_url since it is unused',
+			'sql' => [
+				"ALTER TABLE grouped_work_record_item_url DROP COLUMN id",
+			]
+		], //remove_id_from_grouped_work_record_item_url
+		'reprocess_all_sideloads_22_06_04' => [
+			'title' => 'Reprocess all sideloads',
+			'description' => 'Reprocess all sideloads to ensure they group properly',
+			'sql' => [
+				"UPDATE sideloads set runFullUpdate = 1",
+			]
+		], //reprocess_all_sideloads_22_06_04
+	];
+}
