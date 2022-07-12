4c761329b26110cde4ab4e0964dc4e58260f7631
Mark Noble <mdnoble73@gmail.com>
2022-07-12 17:26:45 -0600

    Increment css & JS counter

--- code/web/index.php
+++ code/web/index.php
@@ -34,7 +34,7 @@ $timer->logTime('Create interface');
 global $locationSingleton;
 getGitBranch();
 //Set a counter for CSS and JavaScript so we can have browsers clear their cache automatically
-$interface->assign('cssJsCacheCounter', 22);
+$interface->assign('cssJsCacheCounter', 23);
 
 // Setup Translator
 global $language;
