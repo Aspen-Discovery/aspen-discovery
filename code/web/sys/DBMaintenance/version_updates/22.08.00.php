9c64e2f377574644ec34fcec32e09f993bc4c3d6
Kirstien Kroeger <kirstien@bywatersolutions.com>
2022-08-03 11:48:42 -0500

    Revert "Update 22.08.00.php"
    This reverts commit d429393d8beeeaa5df76b74fa3cca428e77d3b94.

--- code/web/sys/DBMaintenance/version_updates/22.08.00.php
+++ code/web/sys/DBMaintenance/version_updates/22.08.00.php
@@ -88,6 +88,14 @@ function getUpdates22_08_00() : array
 			'sql' => [
 				'ALTER TABLE overdrive_settings CHANGE COLUMN allowLargeDeletes allowLargeDeletes TINYINT(1) DEFAULT 1'
 			]
-		],//overdrive_enable_allow_large_deletes
+		],
+		'aspen_site_lastOfflineTimeB' => [
+			'title' => 'More offline site monitoring in Greenhouse',
+			'description' => 'Add tracking of why Greenhouse is unable to connect to an Aspen site and when it reconnects',
+			'sql' => [
+				"ALTER TABLE aspen_sites ADD COLUMN lastOfflineNote VARCHAR(255)",
+				"ALTER TABLE aspen_sites ADD COLUMN lastOnlineTime INT",
+			]
+		],//aspen_site_lastOfflineTimeB
 	];
 }
