cccb71c46ee37758af8891d4ee18960f0c42be57
kendrabywater <109627335+kendralittle@users.noreply.github.com>
2022-08-23 16:04:51 -0400

    Update installer.sh

--- install/installer.sh
+++ install/installer.sh
@@ -62,10 +62,8 @@ read timezone
 timedatectl set-timezone $timezone
 
 #Setup LogRotate
-#cp install/logrotate.conf /etc/logrotate.d/aspen_discovery
-#Test Logrotate
-#logrotate -d /etc/logrotate.d/aspen-discovery
-#Restart logrotate
+cp install/logrotate.conf /etc/logrotate.d/aspen_discovery
+
 
 cd /usr/local/aspen-discovery/install
 bash ./setup_aspen_user.sh
