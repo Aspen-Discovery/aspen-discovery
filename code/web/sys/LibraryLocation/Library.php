416bd3a072b312f6d4e9f2410488c0bda6068162
Mark Noble <mdnoble73@gmail.com>
2022-09-23 08:27:32 -0600

    When using Restricting Pickup Location by Format, fix case sensitivity issue when checking location codes

--- code/web/sys/LibraryLocation/Library.php
+++ code/web/sys/LibraryLocation/Library.php
@@ -1859,7 +1859,7 @@ class Library extends DataObject
 
 		$notificationSettings = new NotificationSetting();
 		$notificationSettings->id = $this->lidaNotificationSettingId;
-		if($notificationSettings->find()) {
+		if($notificationSettings->find(true)) {
 			$lidaNotifications = clone $notificationSettings;
 		}
 
@@ -1874,9 +1874,11 @@ class Library extends DataObject
 			$filename = $this->fetchAndStoreSsoMetadata();
 			if (!$filename instanceof AspenError) {
 				// Update the ssoMetadataFilename in the DB
-				$this->ssoMetadataFilename = $fileName;
+				$this->ssoMetadataFilename = $filename;
 			}
 			return $filename;
+		}else{
+			return false;
 		}
 	}
 
