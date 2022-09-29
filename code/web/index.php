67b7eddde3ee1404b826aaad48afecc4dfc41f43
Mark Noble <mdnoble73@gmail.com>
2022-09-29 16:50:51 -0600

    Clear CSS and JS cache

--- code/web/index.php
+++ code/web/index.php
@@ -34,7 +34,7 @@ $timer->logTime('Create interface');
 global $locationSingleton;
 getGitBranch();
 //Set a counter for CSS and JavaScript so we can have browsers clear their cache automatically
-$interface->assign('cssJsCacheCounter', 26);
+$interface->assign('cssJsCacheCounter', 27);
 
 // Setup Translator
 global $language;
