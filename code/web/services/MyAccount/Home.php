58be02a6718495a2cd9428bb254fd79a8b1a41ca
Mark Noble <mdnoble73@gmail.com>
2022-10-06 12:32:30 -0600

    Updates when circulation modules are on

--- code/web/services/MyAccount/Home.php
+++ code/web/services/MyAccount/Home.php
@@ -12,42 +12,7 @@ class MyAccount_Home extends MyAccount{
 			// Check to see if the user has rated any titles
 			$interface->assign('hasRatings', $user->hasRatings());
 
-			// Check to see what sidebar sections to display, if any
-			$showUserCirculationModules = $interface->getVariable('showUserCirculationModules');
-			$showCurbsidePickups = $interface->getVariable('showCurbsidePickups');
-			$showFines = $interface->getVariable('showFines');
-			$showRatings = $interface->getVariable('showRatings');
-			$showFavorites = $interface->getVariable('showFavorites');
-			$enableSavedSearches = $interface->getVariable('enableSavedSearches');
-			$displayMaterialsRequest = $interface->getVariable('displayMaterialsRequest');
-			$enableReadingHistory = $interface->getVariable('enableReadingHistory');
-			$allowAccountLinking = $interface->getVariable('allowAccountLinking');
-			$showUserPreferences = $interface->getVariable('showUserPreferences');
-			$showUserContactInformation = $interface->getVariable('showUserContactInformation');
-			$twoFactorEnabled = $interface->getVariable('twoFactorEnabled');
-			$allowPinReset = $interface->getVariable('allowPinReset');
-			$userIsStaff = $interface->getVariable('userIsStaff');
-
-
-			$showMyAccount = false;
-			if ($showUserCirculationModules || $showCurbsidePickups || $showFines || $showRatings || $showFavorites || $enableSavedSearches || $displayMaterialsRequest || $enableReadingHistory) {
-				$showMyAccount = true;
-			}
-
-			$showAccountSettings = false;
-			if ($allowAccountLinking || $showUserPreferences || $showUserContactInformation || $user->showMessagingSettings() || $twoFactorEnabled || $allowPinReset || $userIsStaff || $showUserCirculationModules) {
-				$showAccountSettings = true;
-			}
-
-			$interface->assign('showMyAccount', $showMyAccount);
-			$interface->assign('showAccountSettings', $showAccountSettings);
-
-			// If neither sidebar sections are show, don't display the sidebar
-			if ($showMyAccount || $showAccountSettings) {
-				$this->display('home.tpl');
-			} else {
-				$this->display('home.tpl', '', false);
-			}
+			parent::display('home.tpl');
 		}
 	}
 
