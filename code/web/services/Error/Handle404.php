6b479b6a38084ca2a4a181cff7ee6f7170cfc662
Mark Noble <mdnoble73@gmail.com>
2022-10-27 15:06:41 -0600

    Update Handle404.php

--- code/web/services/Error/Handle404.php
+++ code/web/services/Error/Handle404.php
@@ -6,7 +6,7 @@ class Error_Handle404 extends Action {
 		global $interface;
 		$interface->assign('showBreadcrumbs', false);
 		http_response_code(404);
-		$this->display('404.tpl', 'Page Not Found');
+		$this->display('404.tpl', 'Page Not Found', false);
 	}
 
 	function getBreadcrumbs() : array
