23515b1099a358934c1207988fe45d3eda190c9c
Kirstien Kroeger <kirstien@bywatersolutions.com>
2022-10-27 15:12:29 -0500

    clean up variables when logging out

--- code/aspen_app/src/util/logout.js
+++ code/aspen_app/src/util/logout.js
@@ -13,55 +13,53 @@ import {PATRON} from "./loadPatron";
  * Logout the user from Aspen LiDA and remove all saved data
  **/
 export async function removeData() {
-	const keys = ['@libraryHomeLink', '@libraryAddress', '@libraryPhone',
-		'@libraryEmail', '@libraryShowHours', '@libraryHoursMessage',
-		'@libraryHours', '@libraryLatitude', '@libraryLongitude',
-		'@patronProfile', '@patronLibrary', '@libraryInfo', '@locationInfo',
-		'@appSettings', '@pickupLocations', '@browseCategories', '@ILSMessages',
-		'@patronCheckouts', '@patronHolds', '@patronHoldsNotReady', '@patronHoldsReady',
-		'@linkedAccounts', '@viewerAccounts', '@pathUrl', '@userToken'];
-
-	SecureStore.deleteItemAsync("patronName");
-	SecureStore.deleteItemAsync("library");
-	SecureStore.deleteItemAsync("libraryName");
-	SecureStore.deleteItemAsync("locationId");
-	SecureStore.deleteItemAsync("solrScope");
-	SecureStore.deleteItemAsync("pathUrl");
-	SecureStore.deleteItemAsync("version");
-	SecureStore.deleteItemAsync("userKey");
-	SecureStore.deleteItemAsync("secretKey");
-	SecureStore.deleteItemAsync("userToken");
-	SecureStore.deleteItemAsync("logo");
-	SecureStore.deleteItemAsync("favicon");
-	await AsyncStorage.removeItem('@userToken');
-	await AsyncStorage.removeItem('@patronProfile');
-	await AsyncStorage.removeItem('@libraryInfo');
-	await AsyncStorage.removeItem('@locationInfo');
-	await AsyncStorage.removeItem('@pathUrl');
-	//await AsyncStorage.clear();
-	// global.promptForOverdriveEmail = "";
-	// global.overdriveEmail = "";
-	// global.patronId = "";
-	// global.barcode = "";
-	// global.rememberHoldPickupLocation = "";
-	// global.pickupLocationId = "";
-	// global.homeLocationId = "";
-	// global.interfaceLanguage = "";
-	// global.numCheckedOut = "";
-	// global.numOverdue = "";
-	// global.numHolds = "";
-	// global.numHoldsAvailable = "";
-	// global.userKey = "";
-	// global.secretKey = "";
-	// global.allHolds = "";
-	// global.unavailableHolds = "";
-	// global.availableHolds = "";
-	// global.allUserHolds = "";
-	// global.checkedOutItems = "";
-
-	LIBRARY = GLOBALS_LIBRARY;
-	PATRON = GLOBALS_PATRON;
-	console.log("Storage data cleansed.")
+	try {
+		SecureStore.deleteItemAsync("patronName");
+		SecureStore.deleteItemAsync("library");
+		SecureStore.deleteItemAsync("libraryName");
+		SecureStore.deleteItemAsync("locationId");
+		SecureStore.deleteItemAsync("solrScope");
+		SecureStore.deleteItemAsync("pathUrl");
+		SecureStore.deleteItemAsync("version");
+		SecureStore.deleteItemAsync("userKey");
+		SecureStore.deleteItemAsync("secretKey");
+		SecureStore.deleteItemAsync("userToken");
+		SecureStore.deleteItemAsync("logo");
+		SecureStore.deleteItemAsync("favicon");
+		await AsyncStorage.removeItem('@userToken');
+		await AsyncStorage.removeItem('@patronProfile');
+		await AsyncStorage.removeItem('@libraryInfo');
+		await AsyncStorage.removeItem('@locationInfo');
+		await AsyncStorage.removeItem('@pathUrl');
+	} catch (e) {
+		console.log(e);
+	} finally {
+		LIBRARY.url = null;
+		LIBRARY.name = null;
+		LIBRARY.favicon = null;
+		LIBRARY.version = "22.10.00";
+		LIBRARY.languages = [];
+		LIBRARY.vdx = [];
+		PATRON.userToken = null;
+		PATRON.scope = null;
+		PATRON.library = null;
+		PATRON.location = null;
+		PATRON.listLastUsed = null;
+		PATRON.fines = 0;
+		PATRON.messages = [];
+		PATRON.num.checkedOut = 0;
+		PATRON.num.holds = 0;
+		PATRON.num.lists = 0;
+		PATRON.num.overdue = 0;
+		PATRON.num.ready = 0;
+		PATRON.num.savedSearches = 0;
+		PATRON.num.updatedSearches = 0;
+		PATRON.promptForOverdriveEmail = 1;
+		PATRON.rememberHoldPickupLocation = 0;
+		PATRON.pickupLocations = [];
+		PATRON.language = "en";
+		console.log("Storage data cleansed.")
+	}
 }
 
 /**
