a492fa6e3dc36e7ef12ff905a16910cc505ce04c
Kirstien Kroeger <kirstien@bywatersolutions.com>
2022-10-27 12:08:09 -0500

    Update MarcRecordDriver.php

--- code/web/RecordDrivers/MarcRecordDriver.php
+++ code/web/RecordDrivers/MarcRecordDriver.php
@@ -2338,27 +2338,25 @@ class MarcRecordDriver extends GroupedWorkSubDriver
 	public function getValidPickupLocations($pickupAtRule): array
 	{
 		$locations = [];
-		$groupedWorkDriver = $this->getGroupedWorkDriver();
-		$relatedRecords = $groupedWorkDriver->getRelatedRecords();
-		foreach($relatedRecords as $record) {
-			$items = $record->getItems();
-			foreach($items as $item) {
-				if($pickupAtRule == 2) {
-					if(!isset($locations[$item->locationCode])) {
-						$location = new Location();
-						$location->code = $item->locationCode;
-						if($location->find(true)) {
-							$library = $location->getParentLibrary();
-							foreach($library->getLocations() as $libraryBranch) {
-								$locations[strtolower($libraryBranch->code)] = strtolower($libraryBranch->code);
-							}
+		$relatedRecord = $this->getGroupedWorkDriver()->getRelatedRecord($this->getIdWithSource());
+		$items = $relatedRecord->getItems();
+		foreach($items as $item) {
+			if($pickupAtRule == 2) {
+				if(!isset($locations[$item->locationCode])) {
+					$location = new Location();
+					$location->code = $item->locationCode;
+					if($location->find(true)) {
+						$library = $location->getParentLibrary();
+						foreach($library->getLocations() as $libraryBranch) {
+							$locations[strtolower($libraryBranch->code)] = strtolower($libraryBranch->code);
 						}
 					}
-				} else {
-					$locations[strtolower($item->locationCode)] = strtolower($item->locationCode);
 				}
+			} else {
+				$locations[strtolower($item->locationCode)] = strtolower($item->locationCode);
 			}
 		}
+
 		return $locations;
 	}
 }
