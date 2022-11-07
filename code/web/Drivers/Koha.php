3763fee142775735ce4a9f1d1162c1a0b3c878b7
Mark Noble <mdnoble73@gmail.com>
2022-11-07 16:46:42 -0700

    Revert change so the REST API is used for renewals.

--- code/web/Drivers/Koha.php
+++ code/web/Drivers/Koha.php
@@ -1824,7 +1824,7 @@ class Koha extends AbstractIlsDriver
 			return $result;
 		}
 
-		if (false && $this->getKohaVersion() >= 19.11) {
+		if ($this->getKohaVersion() >= 19.11) {
 			$sourceId = null;
 			require_once ROOT_DIR . '/sys/User/Checkout.php';
 			$checkout = new Checkout();
