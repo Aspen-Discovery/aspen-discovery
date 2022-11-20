3043501619ee022fa11a5ffa2b4af8a9d9cd0625
Kirstien Kroeger <kirstien@bywatersolutions.com>
2022-11-20 02:48:15 -0600

    Update updater.sh

--- code/aspen_app/updater.sh
+++ code/aspen_app/updater.sh
@@ -66,7 +66,7 @@ then
           sed -i'.bak' "s/{{APP_ENV}}/$site/g" eas.json
           if [[ $otaUpdate == 'yes' ]]
           then
-            APP_ENV=$site eas update --branch $branchName --message $comment --platform $osPlatform
+            APP_ENV=$site eas update --branch $branchName --message "$comment" --platform $osPlatform
           else
             APP_ENV=$site eas build --platform $osPlatform --profile $channel
           fi
@@ -87,7 +87,7 @@ else
   sed -i'.bak' "s/{{APP_ENV}}/$slug/g" eas.json
   if [[ $otaUpdate == 'yes' ]]
   then
-    APP_ENV=$site eas update --branch $branchName --message $comment --platform $osPlatform
+    APP_ENV=$site eas update --branch $branchName --message "$comment" --platform $osPlatform
   else
     APP_ENV=$slug eas build --platform $osPlatform --profile $channel
   fi
