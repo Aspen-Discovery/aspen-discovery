666271bccc0c91c79eb47082df5247c8bc704212
Kirstien Kroeger <kirstien@bywatersolutions.com>
2022-11-19 20:54:24 -0600

    updating to eas build

---
+++ code/aspen_app/updater.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+printf "\n******************************\n"
+printf "Starting Aspen LiDA Updater...\n"
+printf "******************************\n"
+printf "Select release channel:\n"
+PS3="> "
+channels=("production" "beta" "development")
+select item in "${channels[@]}"
+do
+    case $REPLY in
+        *) channel=$item; break;;
+    esac
+done
+
+readarray -t instances < <(jq -c 'keys' 'app-configs/apps.json' | jq -r '.[]')
+declare -a instances
+printf "Select instance:\n"
+PS3="> "
+select item in "${instances[@]}" all
+do
+  eval item=$item
+    case $REPLY in
+        *) slug=$item; break;;
+    esac
+done
+
+printf "Select platform(s):\n"
+PS3="> "
+platforms=("ios" "android" "all")
+select item in "${platforms[@]}"
+do
+    case $REPLY in
+        *) osPlatform=$item; break;;
+    esac
+done
+
+printf "******************************\n"
+if [[ $slug == 'all' ]]
+then
+  readarray -t sites < <(jq -c 'keys' 'app-configs/apps.json' | jq @sh | jq -r)
+  declare -a sites
+  for site in ${sites[@]}
+      do
+        eval site=$site
+         printf "\nRpdating %s in channel %s for %s platform(s)... \n" "$site" "$channel" "$osPlatform"
+          node /usr/local/aspen-discovery/code/aspen_app/app-configs/copyConfig.js
+          node /usr/local/aspen-discovery/code/aspen_app/app-configs/updateConfig.js --instance=$site --env=$channel
+          sed -i'.bak' "s/{{APP_ENV}}/$site/g" eas.json
+          APP_ENV=$site eas build --platform $osPlatform --profile $channel
+          node /usr/local/aspen-discovery/code/aspen_app/app-configs/restoreConfig.js
+          sed -i'.bak' "s/$site/{{APP_ENV}}/g" eas.json
+      done
+else
+  printf "\nUpdating %s in channel %s for %s platform(s)... \n" "$slug" "$channel" "$osPlatform"
+  node /usr/local/aspen-discovery/code/aspen_app/app-configs/copyConfig.js
+  node /usr/local/aspen-discovery/code/aspen_app/app-configs/updateConfig.js --instance=$slug --env=$channel
+  sed -i'.bak' "s/{{APP_ENV}}/$slug/g" eas.json
+  APP_ENV=$slug eas build --platform $osPlatform --profile $channel
+  node /usr/local/aspen-discovery/code/aspen_app/app-configs/restoreConfig.js
+  sed -i'.bak' "s/$slug/{{APP_ENV}}/g" eas.json
+fi
+
+rm -f "eas.json.bak"
+printf "******************************\n"
+printf " 👌 Finished. Bye! \n"
+exit
