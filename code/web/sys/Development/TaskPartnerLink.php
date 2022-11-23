38b83d1e35e416fbed2a40bdf232515145fb6ae7
Mark Noble <mdnoble73@gmail.com>
2022-11-23 08:49:00 -0700

    Development Planning in Aspen
    - Partners

---
+++ code/web/sys/Development/TaskPartnerLink.php
@@ -0,0 +1,33 @@
+<?php
+
+class TaskPartnerLink extends DataObject {
+	public $__table = 'development_task_partner_link';
+	public $partnerId;
+	public $taskId;
+
+	static function getObjectStructure() : array{
+		$taskList = [];
+		require_once ROOT_DIR . '/sys/Development/DevelopmentTask.php';
+		$task = new DevelopmentTask();
+		$task->find();
+		while ($task->fetch()){
+			$taskList[$task->id] = $task->name;
+		}
+
+		$partnerList = [];
+		require_once ROOT_DIR . '/sys/Greenhouse/AspenSite.php';
+		$partner = new AspenSite();
+		$partner->siteType = 0;
+		$partner->orderBy('name asc');
+		$partner->find();
+		while ($partner->fetch()){
+			$partnerList[$partner->id] = $partner->name;
+		}
+
+		return array(
+			'id' => array('property'=>'id', 'type'=>'label', 'label'=>'Id', 'description'=>'The unique id'),
+			'partnerId' => array('property' => 'partnerId', 'type' => 'enum', 'values' => $partnerList, 'label' => 'Partner', 'description' => 'The partner who requested the task', 'required' => true),
+			'taskId' => array('property'=>'taskId', 'type'=>'enum', 'values'=>$taskList, 'label'=>'Task', 'description'=>'The task related to the partner', 'required' => true),
+		);
+	}
+}
