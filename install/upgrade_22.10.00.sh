58d59c68cc70139928b68e5a15bb3fc989cf19fc
kendrabywater <109627335+kendralittle@users.noreply.github.com>
2022-11-29 16:23:57 -0500

    Update upgrade_22.10.00.sh

--- install/upgrade_22.10.00.sh
+++ install/upgrade_22.10.00.sh
@@ -6,27 +6,3 @@ if [ -z "$1" ]
 fi
 
 php /usr/local/aspen-discovery/install/updateCron_22_10.php $1
-
-
-#upgrade openssh to 9.0
-yum groupinstall "Development Tools" -y
-yum install zlib-devel openssl-devel -y
-
-cp /etc/ssh/sshd_config  /etc/ssh/sshd_config
-
-wget -c  https://cdn.openbsd.org/pub/OpenBSD/OpenSSH/portable/openssh-9.0p1.tar.gz
-
-tar -xzf  openssh-9.0p1.tar.gz
-
-cd openssh-9.0p1/
-
-yum install pam-devel libselinux-devel -y
-
-./configure  --with-pam --with-selinux --with-privsep-path=/var/lib/sshd/ --sysconfdir=/etc/ssh
-
-make
-make install
-
-
-# Disable Apache serversignature
-echo -e "ServerSignature Off \nServerTokens Prod" >> /etc/httpd/conf/httpd.conf
