5ce7893ad047e0dc2ee3ca7963710f6be39abc69
Mark Noble <mdnoble73@gmail.com>
2022-12-06 16:36:45 -0700

    Break javascript cache

--- code/web/index.php
+++ code/web/index.php
@@ -34,7 +34,7 @@ $timer->logTime('Create interface');
 global $locationSingleton;
 getGitBranch();
 //Set a counter for CSS and JavaScript so we can have browsers clear their cache automatically
-$interface->assign('cssJsCacheCounter', 27);
+$interface->assign('cssJsCacheCounter', 28);
 
 // Setup Translator
 global $language;
