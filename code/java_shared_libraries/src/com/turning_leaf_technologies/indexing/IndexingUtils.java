099f1d452e480e9b8c36a18b4e920c308bb9f560
Mark Noble <mdnoble73@gmail.com>
2022-12-06 08:47:10 -0700

    Open archives checks

--- code/java_shared_libraries/src/com/turning_leaf_technologies/indexing/IndexingUtils.java
+++ code/java_shared_libraries/src/com/turning_leaf_technologies/indexing/IndexingUtils.java
@@ -679,7 +679,7 @@ public class IndexingUtils {
 				BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
 				while ((line = input.readLine()) != null) {
 					//logger.info(line);
-					if (line.matches(".*" + indexerName + "\\.jar " + serverName) && !line.contains("/bin/sh -c")){
+					if (line.matches(".*" + indexerName + "\\.jar " + serverName) && !line.contains("/bin/sh -c") && !line.contains("runuser")){
 						logger.warn(line);
 						numInstancesRunning++;
 					}
