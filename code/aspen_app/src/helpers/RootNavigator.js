d56fa09d9f03a54e70cc6bd6924c7a7df38c030b
Kirstien Kroeger <kirstien@bywatersolutions.com>
2022-12-07 11:36:41 -0600

    fix user drawer context

--- code/aspen_app/src/helpers/RootNavigator.js
+++ code/aspen_app/src/helpers/RootNavigator.js
@@ -1,23 +1,29 @@
-import {
-  CommonActions,
-  createNavigationContainerRef,
-} from "@react-navigation/native";
+import { CommonActions, createNavigationContainerRef } from '@react-navigation/native';
 
 export const navigationRef = createNavigationContainerRef();
 
 export const navigate = (name, params) => {
-  if (navigationRef.current) {
-    navigationRef.current.navigate(name, params);
-  }
+     if (navigationRef.current) {
+          navigationRef.current.navigate(name, params);
+     }
+};
+
+export const navigateStack = (stack, screen, params) => {
+     if (navigationRef.current) {
+          navigationRef.current.navigate(stack, {
+               screen: screen,
+               params: params,
+          });
+     }
 };
 
 export const navigateAndSimpleReset = (name, index = 0) => {
-  if (navigationRef.isReady()) {
-    navigationRef.dispatch(
-      CommonActions.reset({
-        index,
-        routes: [{ name }],
-      })
-    );
-  }
+     if (navigationRef.isReady()) {
+          navigationRef.dispatch(
+               CommonActions.reset({
+                    index,
+                    routes: [{ name }],
+               })
+          );
+     }
 };
