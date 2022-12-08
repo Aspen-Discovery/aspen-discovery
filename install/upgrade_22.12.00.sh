97107d450f71589c3f726542cd1cbf4ee53ca12c
Mark Noble <mdnoble73@gmail.com>
2022-12-08 10:43:39 -0700

    Fix removing modules

--- install/upgrade_22.12.00.sh
+++ install/upgrade_22.12.00.sh
@@ -2,9 +2,9 @@
 # configure mod evasive
 #yum install mod_evasive -y
 #cp mod_evasive.conf /etc/httpd/conf.d/mod_evasive.conf
-yum remove mod_evasive
+yum remove mod_evasive -y
 
 # mod security is causing issues with file uploads.  Not installing.
 #configure mod security
 #yum install mod_security -y
-yum remove mod_security
+yum remove mod_security -y
