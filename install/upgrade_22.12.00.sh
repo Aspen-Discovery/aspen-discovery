1b641452a3b7fc12f7b139bd67036071129bdb03
Mark Noble <mdnoble73@gmail.com>
2022-12-08 09:59:22 -0700

    Remove mod_evasive and mod_security

--- install/upgrade_22.12.00.sh
+++ install/upgrade_22.12.00.sh
@@ -1,6 +1,10 @@
+# mod evasive is causing issues with sites that have lots of book covers on one page. Not installing.
 # configure mod evasive
-yum install mod_evasive -y
-cp mod_evasive.conf /etc/httpd/conf.d/mod_evasive.conf
+#yum install mod_evasive -y
+#cp mod_evasive.conf /etc/httpd/conf.d/mod_evasive.conf
+yum remove mod_evasive
 
+# mod security is causing issues with file uploads.  Not installing.
 #configure mod security
-yum install mod_security -y
+#yum install mod_security -y
+yum remove mod_security
