0990015fe816258df3aebfafbe9c04d553ad295d
Mark Noble <mdnoble73@gmail.com>
2023-01-19 10:07:59 -0700

    Fix showing error messages from OverDrive when errors are encountered with download links, read online, etc.

--- code/web/services/Help/OverDriveError.php
+++ code/web/services/Help/OverDriveError.php
@@ -4,7 +4,7 @@ require_once ROOT_DIR . '/Action.php';
 
 class Help_OverDriveError extends Action {
 	function launch() {
-		$this->display('Help/overdriveError.tpl', 'Error in OverDrive');
+		$this->display('overdriveError.tpl', 'Error in OverDrive');
 	}
 
 	function getBreadcrumbs(): array {
