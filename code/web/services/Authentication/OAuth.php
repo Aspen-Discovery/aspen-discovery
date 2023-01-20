1f694367ce5f0cf10b7c8696ff5b79d844af8c43
Mark Noble <mdnoble73@gmail.com>
2023-01-20 14:10:53 -0700

    Additional error trapping for OAuth

--- code/web/services/Authentication/OAuth.php
+++ code/web/services/Authentication/OAuth.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'bootstrap.php';
+require_once 'bootstrap_aspen.php';
 require_once ROOT_DIR . '/sys/Authentication/OAuthAuthentication.php';
 
 class Authentication_OAuth extends Action {
