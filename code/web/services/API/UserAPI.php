62fc796ff878399c2f79902b9ac3b967e21c92c2
Kirstien Kroeger <kirstien@bywatersolutions.com>
2023-02-14 10:09:08 -0600

    Update UserAPI.php

--- code/web/services/API/UserAPI.php
+++ code/web/services/API/UserAPI.php
@@ -242,26 +242,6 @@ class UserAPI extends Action {
 
 		require_once ROOT_DIR . '/CatalogFactory.php';
 		$driversToTest = UserAccount::getAccountProfiles();
-/*		if (strlen($library->casHost) > 0 && $username == null && $password == null) {
-			//Check CAS first
-			require_once ROOT_DIR . '/sys/Authentication/CASAuthentication.php';
-			$casAuthentication = new CASAuthentication(null);
-			$logger->log('Checking CAS Authentication from UserAccount::validateAccount', Logger::LOG_DEBUG);
-			$casUsername = $casAuthentication->validateAccount(null, null, $parentAccount, false);
-			if ($casUsername == false || $casUsername instanceof AspenError) {
-				//The user could not be authenticated in CAS
-				$logger->log('User could not be authenticated in CAS', Logger::LOG_DEBUG);
-				self::$validatedAccounts[$username . $password] = false;
-				return false;
-			} else {
-				$logger->log('User was authenticated in CAS', Logger::LOG_DEBUG);
-				//Set both username and password since authentication methods could use either.
-				//Each authentication method will need to deal with the possibility that it gets a barcode for both user and password
-				$username = $casUsername;
-				$password = $casUsername;
-				$validatedViaSSO = true;
-			}
-		}*/
 
 		foreach ($driversToTest as $driverName => $additionalInfo) {
 			if ($accountSource == null || $accountSource == $additionalInfo['accountProfile']->name) {
@@ -281,13 +261,13 @@ class UserAPI extends Action {
 					];
 				} else {
 					$invalidUser = (array) $validatedUser;
-					if(isset($invalidUser['resetToken'])) {
+					if(isset($invalidUser['message'])) {
 						return [
 							'success' => false,
-							'id' => $invalidUser['id'],
+							'id' => $invalidUser['id'] ?? null,
 							'message' => $invalidUser['message'],
 							'resetToken' => $invalidUser['resetToken'] ?? null,
-							'userId' => $invalidUser['userId']
+							'userId' => $invalidUser['userId'] ?? null
 						];
 					}
 				}
