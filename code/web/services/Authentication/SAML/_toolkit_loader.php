183a68db630d6a99be4353f6b1019c2b0a918352
Kirstien Kroeger <kirstien@bywatersolutions.com>
2023-02-14 16:37:25 -0600

    add OneLogin SAML library

---
+++ code/web/services/Authentication/SAML/_toolkit_loader.php
@@ -0,0 +1,20 @@
+<?php
+
+// Create an __autoload function
+// (can conflicts other autoloaders)
+// http://php.net/manual/en/language.oop5.autoload.php
+
+$libDir = __DIR__ . '/lib/Saml2/';
+$extlibDir = __DIR__ . '/extlib/';
+
+// Load external libs
+require_once $extlibDir . 'xmlseclibs/xmlseclibs.php';
+
+$folderInfo = scandir($libDir);
+
+foreach ($folderInfo as $element) {
+    if (is_file($libDir.$element) && (substr($element, -4) === '.php')) {
+        include_once $libDir.$element;
+        //break;
+    }
+}
