281a82617fd264dd776f5428dff84ef54f994208
Kirstien Kroeger <kirstien@bywatersolutions.com>
2023-02-15 12:15:37 -0600

    Update Evergreen.php

--- code/web/Drivers/Evergreen.php
+++ code/web/Drivers/Evergreen.php
@@ -1684,10 +1684,8 @@ class Evergreen extends AbstractIlsDriver {
 				}
 			}
 
-			if(UserAccount::getActiveUserObj()) {
-				$userObj = UserAccount::getActiveUserObj();
-				$interface->assign('primaryEmail', $userObj->email);
-			}
+			$interface->assign('primaryEmail', $user->email);
+
 			return [
 				'preferences' => $notificationPreferences,
 				'primaryEmail' => $user->email,
