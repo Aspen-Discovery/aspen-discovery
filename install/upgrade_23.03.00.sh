68a2539d392152201d92c453d052ad4e4fc24e7b
Mark Noble <mdnoble73@gmail.com>
2023-03-07 12:22:07 -0700

    Update upgrade_23.03.00.sh

--- install/upgrade_23.03.00.sh
+++ install/upgrade_23.03.00.sh
@@ -4,8 +4,8 @@ yum -y install php php-imagick
 
 file="/usr/local/aspen-discovery/sites/$1/conf/crontab_settings.txt "
 
-search="10 0 * * *    root    /usr/local/aspen-discovery/code/cron/nightly_mysql_dump.sh $1 aspen 2>&1 >/dev/null"
+search="10 0 * * *    root    \/usr\/local\/aspen-discovery\/code\/cron\/nightly_mysql_dump.sh $1 aspen 2>&1 >\/dev\/null"
 
-replace="10 0 * * *    root    php /usr/local/aspen-discovery/code/web/cron/backupAspen.php $1"
+replace="10 0 * * *    root    php \/usr\/local\/aspen-discovery\/code\/web\/cron\/backupAspen.php $1"
 
 sed -i "s/$search/$replace/g" "$file"
