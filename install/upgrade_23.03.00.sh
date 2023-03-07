18a62070fa65a85fd3d387d6573ea4a1eba3abe0
Mark Noble <mdnoble73@gmail.com>
2023-03-07 14:14:05 -0700

    Move backup to use new scripts

--- install/upgrade_23.03.00.sh
+++ install/upgrade_23.03.00.sh
@@ -4,10 +4,10 @@ yum -y install php php-imagick
 
 file="/usr/local/aspen-discovery/sites/$1/conf/crontab_settings.txt"
 
-search="10 0 \* \* \*\s+root\s+\/usr\/local\/aspen-discovery\/code\/cron\/nightly_mysql_dump\.sh $1 aspen 2>&1 >\s*\/dev\/null"
+search="/usr/local/aspen-discovery/code/cron/nightly_mysql_dump\.sh $1 aspen 2>&1 >\s*/dev/null"
 
-replace="10 0 \* \* \*    root    php \/usr\/local\/aspen-discovery\/code\/web\/cron\/backupAspen\.php $1"
+replace="/usr/local/aspen-discovery/code/web/cron/backupAspen\.php $1"
 
 cp "$file" "$file.bak"
-echo "sed -i \"s/$search/$replace/g\" \"$file\""
-sed -i "s/$search/$replace/g" "$file"
+#echo "sed -i \"s#$search#$replace#g\" \"$file\""
+sed -i "s#$search#$replace#g" "$file"
