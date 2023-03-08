4bb11c24793ee6526dba213eb9bb0006e748a90a
Kodi <77032146+K-Alette@users.noreply.github.com>
2023-03-08 10:52:35 -0800

    Create 23.04.00.php
    db maintenance file for 23.04.00

---
+++ code/web/sys/DBMaintenance/version_updates/23.04.00.php
@@ -0,0 +1,25 @@
+<?php
+/** @noinspection PhpUnused */
+function getUpdates23_04_00(): array {
+	$curTime = time();
+	return [
+		/*'name' => [
+			'title' => '',
+			'description' => '',
+			'continueOnError' => false,
+			'sql' => [
+				''
+			]
+		], //sample*/
+
+		//mark
+
+		//kirstien
+
+		//kodi
+
+		//google_bucket
+
+		//other
+	];
+}
