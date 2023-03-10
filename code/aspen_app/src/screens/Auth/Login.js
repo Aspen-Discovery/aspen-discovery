10addf08a89393364ab4b0d976fd117918d8c718
Kirstien Kroeger <kirstien@bywatersolutions.com>
2023-03-10 09:24:00 -0600

    translation prep

--- code/aspen_app/src/screens/Auth/Login.js
+++ code/aspen_app/src/screens/Auth/Login.js
@@ -3,7 +3,8 @@ import { create } from 'apisauce';
 import Constants from 'expo-constants';
 import * as Updates from 'expo-updates';
 import _ from 'lodash';
-import { Badge, Box, Button, Center, HStack, Icon, Image, Input, KeyboardAvoidingView, Pressable, Text, VStack } from 'native-base';
+import { MaterialIcons } from "@expo/vector-icons";
+import { Badge, Box, Button, Center, HStack, Icon, Image, Input, KeyboardAvoidingView, Pressable, Text, VStack, Modal, FlatList } from 'native-base';
 import React, { Component } from 'react';
 import { Platform } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -17,7 +18,171 @@ import { GetLoginForm } from './LoginForm';
 import { SelectYourLibrary } from './SelectYourLibrary';
 import {LIBRARY} from '../../util/loadLibrary';
 import {getLibraryLoginLabels} from '../../util/api/library';
-import {getTranslation} from '../../translations/TranslationService';
+import {getTermFromDictionary, getTranslation} from '../../translations/TranslationService';
+import {LanguageContext} from '../../context/initialContext';
+import { useQuery } from '@tanstack/react-query';
+import {getFirstRecord, getVariations} from '../../util/api/item';
+import {loadingSpinner} from '../../components/loadingSpinner';
+import {loadError} from '../../components/loadError';
+
+export const LoginScreen = () => {
+     const { language } = React.useContext(LanguageContext);
+     const [isLoading, setIsLoading] = React.useState(false);
+     const [selectedLibrary, setSelectedLibrary] = React.useState([]);
+     const [usernameLabel, setUsernameLabel] = React.useState('Library Barcode');
+     const [passwordLabel, setPasswordLabel] = React.useState('Password/PIN');
+     const [showModal, setShowModal] = React.useState(false);
+     const [query, setQuery] = React.useState('');
+     const isCommunity = GLOBALS.slug === 'aspen-lida';
+     const logoImage = Constants.manifest2?.extra?.expoClient?.extra?.loginLogo ?? Constants.manifest.extra.loginLogo;
+
+     const updateSelectedLibrary = async (data) => {
+          setSelectedLibrary(data);
+          LIBRARY.url = data.baseUrl; // used in some cases before library context is set
+          await getLibraryLoginLabels(data.libraryId, data.baseUrl).then(async labels => {
+               try {
+                    const username = await getTranslation(labels.username, 'en', data.baseUrl);
+                    const password = await getTranslation(labels.password, 'en', data.baseUrl);
+                    setUsernameLabel(username);
+                    setPasswordLabel(password);
+               } catch (e) {
+                    // couldn't fetch translated login terms for some reason, just use the default as backup
+               }
+          });
+          setShowModal(false);
+     }
+
+     return (
+         <Box flex={1} alignItems="center" justifyContent="center" safeArea={5}>
+              <Image source={{ uri: logoImage }} rounded={25} size="xl" alt="" fallbackSource={require('../../themes/default/aspenLogo.png')} />
+              {isCommunity ? (
+                  <SelectYourLibraryModal updateSelectedLibrary={updateSelectedLibrary} selectedLibrary={selectedLibrary} query={query} setQuery={query} showModal={showModal} setShowModal={setShowModal} isCommunity={isCommunity}/>
+              ) : null}
+              <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'padding'} width="100%">
+                   {selectedLibrary ? (
+                       <GetLoginForm />
+                   ) : null}
+                   {isCommunity && Platform.OS !== 'android' ? (
+                       <Button mt={8} size="xs" variant="ghost" colorScheme="secondary" startIcon={<Icon as={Ionicons} name="navigate-circle-outline" size={5} />}>{translate('login.reset_geolocation')}</Button>
+                   ) : null}
+                   <Center>
+                        <Text mt={5} fontSize="xs" color="coolGray.600">
+                             {GLOBALS.appVersion} b[{GLOBALS.appBuild}] p[{GLOBALS.appPatch}] c[{GLOBALS.releaseChannel ?? 'Development'}]
+                        </Text>
+                   </Center>
+              </KeyboardAvoidingView>
+         </Box>
+     )
+
+}
+
+const SelectYourLibraryModal = (data) => {
+     const locations = [];
+     const {query, setQuery, isCommunity, showModal, setShowModal, updateSelectedLibrary, selectedLibrary} = data;
+     const releaseChannel = GLOBALS.releaseChannel ?? Updates.releaseChannel;
+     const url = Constants.manifest2?.extra?.expoClient?.extra?.greenhouseUrl ?? Constants.manifest.extra.greenhouseUrl;
+
+     const { status, data: nearbyLibraries, error, isFetching, isPreviousData } = useQuery({
+          queryKey: ['nearbyLibrariesFromGreenhouse', url, releaseChannel],
+          queryFn: () => fetchNearbyLibrariesFromGreenhouse(releaseChannel, url)
+     });
+
+     const { data: allLibraries } = useQuery({
+          queryKey: ['allLibrariesFromGreenhouse', url, releaseChannel],
+          queryFn: () => fetchAllLibrariesFromGreenhouse(releaseChannel, url),
+          enabled: !!nearbyLibraries
+     });
+
+     const Header = () => {
+          return (
+              <Box bg="white" _dark={{ bg: 'coolGray.800' }}>
+                   <Input variant="filled" size="lg" autoCorrect={false} status="info" placeholder={translate('search.title')} clearButtonMode="always" value={query} onChangeText={(text) => setQuery(text)}/>
+              </Box>
+          )
+     }
+
+     function Filter() {
+          const query = "";
+          let haystack = locations;
+          if(!_.isEmpty(query) && !_.isEmpty(haystack)) {
+               haystack = '';
+          }
+
+          if(!isCommunity) {
+               return _.filter(haystack, function (branch) {
+                    return branch.name.toLowerCase().indexOf(query.toLowerCase()) > -1;
+               });
+          }
+
+          return _.filter(haystack, function (branch) {
+               return branch.name.toLowerCase().indexOf(query.toLowerCase()) > -1 || branch.librarySystem.toLowerCase().indexOf(query.toLowerCase()) > -1;
+          })
+     }
+
+     return (
+         <Center>
+              <Button onPress={() => setShowModal(true)} colorScheme="primary" m={5} size="md"
+                      startIcon={<Icon as={MaterialIcons} name="place" size={5} />}>{selectedLibrary?.name ? selectedLibrary.name : getTermFromDictionary('en', 'select_your_library')}</Button>
+              <Modal isOpen={showModal} size="lg" avoidKeyboard onClose={() => setShowModal(false)}>
+                   <Modal.Content bg="white" _dark={{ bg: "coolGray.800" }} maxH="350">
+                        <Modal.CloseButton />
+                        <Modal.Header>{getTermFromDictionary('en', 'find_your_library')}</Modal.Header>
+                        {status === 'loading' || isFetching ? loadingSpinner() : status === 'error' ? loadError(error, '') : (
+                            <FlatList
+                                stickyHeaderIndices={[0]}
+                                keyboardShouldPersistTaps="handled"
+                                keyExtractor={(item, index) => index.toString()}
+                                renderItem={({ item }) => <Item data={item} isCommunity={isCommunity} setShowModal={setShowModal} updateSelectedLibrary={updateSelectedLibrary}/>}
+                                ListHeaderComponent={Header}
+                            />
+                        )}
+                   </Modal.Content>
+              </Modal>
+         </Center>
+     )
+}
+
+const Item = (data) => {
+     const library = data.data;
+     const libraryIcon = library.favicon;
+     const {isCommunity, setShowModal, updateSelectedLibrary} = data;
+     return (
+         <Pressable borderBottomWidth="1" _dark={{ borderColor: 'gray.600' }} borderColor="coolGray.200" onPress={() => updateSelectedLibrary(library)} pl="4" pr="5" py="2">
+              <HStack space={3} alignItems="center">
+                   <Image
+                       key={library.name}
+                       borderRadius={100}
+                       source={{ uri: libraryIcon }}
+                       fallbackSource={require('../../themes/default/aspenLogo.png')}
+                       bg="warmGray.200"
+                       _dark={{bgColor: 'coolGray.800'}}
+                       size={{
+                            base: '25px',
+                       }}
+                       alt={library.name} />
+                   <VStack>
+                        <Text
+                            bold
+                            fontSize={{
+                                 base: 'sm',
+                                 lg: 'md',
+                            }}>
+                             {library.name}
+                        </Text>
+                        {isCommunity ? (
+                            <Text
+                                fontSize={{
+                                     base: 'xs',
+                                     lg: 'sm',
+                                }}>
+                                 {library.librarySystem}
+                            </Text>
+                        ) : null}
+                   </VStack>
+              </HStack>
+         </Pressable>
+     )
+}
 
 export default class Login extends Component {
      constructor(props) {
