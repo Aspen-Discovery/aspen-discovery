ac14a7503b8fa2cd159ae59042cfc25b5a7f220d
Mark Noble <mdnoble73@gmail.com>
2023-03-10 16:14:01 -0700

    Fix backup upgrade

--- install/upgrade_23.03.00.sh
+++ install/upgrade_23.03.00.sh
@@ -6,7 +6,7 @@ file="/usr/local/aspen-discovery/sites/$1/conf/crontab_settings.txt"
 
 search="/usr/local/aspen-discovery/code/cron/nightly_mysql_dump\.sh $1 aspen 2>&1 >\s*/dev/null"
 
-replace="/usr/local/aspen-discovery/code/web/cron/backupAspen\.php $1"
+replace="php /usr/local/aspen-discovery/code/web/cron/backupAspen\.php $1"
 
 cp "$file" "$file.bak"
 #echo "sed -i \"s#$search#$replace#g\" \"$file\""
