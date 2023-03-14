7ca2df2bc2f99004cb2c791afb9c00ab85d4f6a3
Kirstien Kroeger <kirstien@bywatersolutions.com>
2023-03-14 11:04:42 -0500

    move to context, translation updates

--- code/aspen_app/src/navigations/stack/MoreStackNavigator.js
+++ code/aspen_app/src/navigations/stack/MoreStackNavigator.js
@@ -2,9 +2,9 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
 
 import { ContactLibrary } from '../../screens/Library/Contact';
-import More from '../../screens/More/More';
-import {LanguageContext, LibraryBranchContext, LibrarySystemContext, UserContext} from '../../context/initialContext';
+import {LanguageContext} from '../../context/initialContext';
 import {getTermFromDictionary} from '../../translations/TranslationService';
+import {MoreScreen} from '../../screens/More/More';
 
 const MoreStackNavigator = () => {
      const { language } = React.useContext(LanguageContext);
@@ -18,13 +18,8 @@ const MoreStackNavigator = () => {
                }}>
                <Stack.Screen
                     name="More"
-                    component={More}
+                    component={MoreScreen}
                     options={{ title: getTermFromDictionary(language, 'nav_more') }}
-                    initialParams={{
-                         libraryContext: JSON.stringify(React.useContext(LibrarySystemContext)),
-                         locationContext: JSON.stringify(React.useContext(LibraryBranchContext)),
-                         userContext: React.useContext(UserContext),
-                    }}
                />
                <Stack.Screen name="Contact" component={ContactLibrary} options={{ title: getTermFromDictionary(language, 'contact') }} />
           </Stack.Navigator>
