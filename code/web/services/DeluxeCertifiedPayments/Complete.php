b238d8398a23b4292dedd6a5b67f0b6ad3c48fb9
Kirstien Kroeger <kirstien@bywatersolutions.com>
2023-03-23 09:58:45 -0500

    add post endpoints

---
+++ code/web/services/DeluxeCertifiedPayments/Complete.php
@@ -0,0 +1,28 @@
+<?php
+
+class DeluxeCertifiedPayments_Complete extends Action {
+	public function launch() {
+		global $logger;
+		$success = false;
+		$message = '';
+		$error = '';
+		$logger->log('Completing Deluxe Certified Payments Session Notification Request...', Logger::LOG_ERROR);
+		$logger->log(print_r($_POST, true), Logger::LOG_ERROR);
+
+		$result = [
+			'success' => $success,
+			'message' => $success ? $message : $error,
+		];
+
+		header('Content-type: application/json');
+		header('Cache-Control: no-cache, must-revalidate'); // HTTP/1.1
+		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+
+		echo json_encode($result);
+		die();
+	}
+
+	function getBreadcrumbs(): array {
+		return [];
+	}
+}
