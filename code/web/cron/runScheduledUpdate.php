f8324c778f2279de469b2dec0418ac670469c054
Kirstien Kroeger <kirstien@bywatersolutions.com>
2023-04-28 09:30:48 -0500

    Create runScheduledUpdate.php

---
+++ code/web/cron/runScheduledUpdate.php
@@ -0,0 +1,110 @@
+<?php
+require_once __DIR__ . '/../bootstrap.php';
+require_once __DIR__ . '/../bootstrap_aspen.php';
+
+require_once ROOT_DIR . '/sys/Updates/ScheduledUpdate.php';
+require_once ROOT_DIR . '/sys/Greenhouse/AspenSite.php';
+
+$pendingUpdates = new ScheduledUpdate();
+$pendingUpdates->status = 'pending';
+$updatesToRun = $pendingUpdates->fetchAll('id');
+
+global $configArray;
+global $serverName;
+
+foreach($updatesToRun as $id) {
+	$scheduledUpdate = new ScheduledUpdate();
+	$scheduledUpdate->id = $id;
+	if($scheduledUpdate->find(true)) {
+		$siteName = "";
+		$currentVersion = "";
+		if($scheduledUpdate->siteId) {
+			$site = new AspenSite();
+			$site->id = $scheduledUpdate->siteId;
+			if($site->find((true))) {
+				$currentVersion = $site->version;
+				$siteName = $site->name;
+			}
+		} else {
+			global $interface;
+			global $library;
+			$currentVersion = $interface->getVariable('gitBranchWithCommit');
+			$siteName = $library->displayName;
+		}
+
+		if($scheduledUpdate->updateType === 'complete') {
+			exec("cd /usr/local/aspen-discovery; git fetch origin; git reset --hard origin/$currentVersion", $resetGitResult);
+			$scheduledUpdate->notes .= $resetGitResult;
+			if(str_contains($resetGitResult, 'fatal') || str_contains($resetGitResult, 'failed') || str_contains($resetGitResult, 'rejected')) {
+				$scheduledUpdate->status = 'failed';
+			} else {
+				$scheduledUpdate->status = 'complete';
+			}
+
+			exec("cd /usr/local/aspen-discovery; sudo git pull origin $scheduledUpdate->updateToVersion", $gitResult);
+			$scheduledUpdate->notes .= $gitResult;
+			if(str_contains($gitResult, 'fatal') || str_contains($gitResult, 'failed') || str_contains($gitResult, 'rejected')) {
+				$scheduledUpdate->status = 'failed';
+			} else {
+				$scheduledUpdate->status = 'complete';
+			}
+		}else if($scheduledUpdate->updateType === 'patch') {
+			exec("cd /usr/local/aspen-discovery; sudo git pull origin $scheduledUpdate->updateToVersion", $gitResult);
+			$scheduledUpdate->notes .= $gitResult;
+			if(str_contains($gitResult, 'fatal') || str_contains($gitResult, 'failed') || str_contains($gitResult, 'rejected')) {
+				$scheduledUpdate->status = 'failed';
+			} else {
+				$scheduledUpdate->status = 'complete';
+			}
+		} else {
+			// invalid updateType
+		}
+
+		// run db maintenance ???
+
+		$scheduledUpdate->update();
+
+		// send slack notification
+		require_once ROOT_DIR . '/sys/Greenhouse/GreenhouseSettings.php';
+		$greenhouseSettings = new GreenhouseSettings();
+		$greenhouseAlertSlackHook = null;
+		$shouldSendBuildAlert = false;
+		if ($greenhouseSettings->find(true)) {
+			$greenhouseAlertSlackHook = $greenhouseSettings->greenhouseAlertSlackHook;
+			$shouldSendBuildAlert = $greenhouseSettings->sendBuildTrackerAlert;
+		}
+
+		if ($greenhouseAlertSlackHook && $shouldSendBuildAlert) {
+			$scheduledUpdateUrl = $configArray['Site']['url'] . '/Admin/ScheduledUpdates/';
+			if($scheduledUpdate->siteId) {
+				// update scheduled from the greenhouse
+				require_once ROOT_DIR . '/sys/SystemVariables.php';
+				$systemVariables = SystemVariables::getSystemVariables();
+				if(!empty($systemVariables)) {
+					$scheduledUpdateUrl = $systemVariables->greenhouseUrl . '/Greenhouse/UpdateCenter/';
+				}
+			}
+			if($scheduledUpdate->status === 'failed') {
+				$notification = "- :fire: <$scheduledUpdateUrl|Update failed> for $siteName while updating to $scheduledUpdate->updateToVersion ($scheduledUpdate->updateType)";
+				$notification .= '<!here>';
+			} else if($scheduledUpdate->status === 'complete') {
+				$notification = "- <$scheduledUpdateUrl|Update completed> for $siteName to $scheduledUpdate->updateToVersion ($scheduledUpdate->updateType)";
+			} else {
+				// something weird has happened
+				$notification = null;
+			}
+			$alertText = "*$siteName* $notification\n";
+			if($notification) {
+				$curlWrapper = new CurlWrapper();
+				$headers = [
+					'Accept: application/json',
+					'Content-Type: application/json',
+				];
+				$curlWrapper->addCustomHeaders($headers, false);
+				$body = new stdClass();
+				$body->text = $alertText;
+				$curlWrapper->curlPostPage($greenhouseAlertSlackHook, json_encode($body));
+			}
+		}
+	}
+}
