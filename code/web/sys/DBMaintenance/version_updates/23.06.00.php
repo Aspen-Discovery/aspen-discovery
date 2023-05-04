3034497cfa7ecdff303e014ff7a84025775b257e
Kirstien Kroeger <kirstien@bywatersolutions.com>
2023-05-04 15:54:25 -0500

    Create 23.06.00.php

---
+++ code/web/sys/DBMaintenance/version_updates/23.06.00.php
@@ -0,0 +1,20 @@
+<?php
+/** @noinspection PhpUnused */
+function getUpdates23_06_00(): array {
+	$curTime = time();
+	return [
+		/*'name' => [
+			'title' => '',
+			'description' => '',
+			'continueOnError' => false,
+			'sql' => [
+				''
+			]
+		], //sample*/
+
+		//mark
+		//kirstien
+		//kodi
+		//other
+	];
+}
