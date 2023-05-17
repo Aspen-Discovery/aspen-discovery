49694eb0d5758519eaddc49b4ac0ba3f168cc9e0
Kirstien Kroeger <kirstien@bywatersolutions.com>
2023-05-17 11:19:33 -0500

    Update GroupedWorkDisplaySetting.php

--- code/web/sys/Grouping/GroupedWorkDisplaySetting.php
+++ code/web/sys/Grouping/GroupedWorkDisplaySetting.php
@@ -243,7 +243,6 @@ class GroupedWorkDisplaySetting extends DataObject {
 								'label' => 'Show Covers for Editions',
 								'description' => 'Turn on to show individual covers for each edition',
 								'default' => false,
-								'note' => 'Covers will only display if showing related record labels is unchecked',
 								'hideInLists' => true,
 							],
 						],
