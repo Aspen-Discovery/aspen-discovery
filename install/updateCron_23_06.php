288c81a8b7fd7e1b1a724f005d6ac67e4f878c79
Kirstien Kroeger <kirstien@bywatersolutions.com>
2023-05-22 13:33:41 -0500

    Create updateCron_23_06.php

---
+++ install/updateCron_23_06.php
@@ -0,0 +1,38 @@
+<?php
+
+if (count($_SERVER['argv']) > 1) {
+	$serverName = $_SERVER['argv'][1];
+	//Check to see if the update already exists properly.
+	$fhnd = fopen("/usr/local/aspen-discovery/sites/$serverName/conf/crontab_settings.txt", 'r');
+	if ($fhnd) {
+		$lines = [];
+		$insertUpdate = true;
+		$changeToRoot = false;
+		while (($line = fgets($fhnd)) !== false) {
+			if (strpos($line, 'updateCommunityTranslations') > 0) {
+				$insertUpdate = false;
+				if (strpos($line, 'aspen ') > 0) {
+					$changeToRoot = true;
+				}
+			}
+		}
+		fclose($fhnd);
+		if ($insertUpdate) {
+			$lines[] = "#########################\n";
+			$lines[] = "# Update Translations from Community #\n";
+			$lines[] = "#########################\n";
+			$lines[] = "15 1 * * * root php /usr/local/aspen-discovery/code/web/cron/updateCommunityTranslations.php $serverName\n";
+		}
+		if ($changeToRoot || $insertUpdate) {
+			//echo("- Writing new cron\n");
+			$newContent = implode('', $lines);
+			file_put_contents("/usr/local/aspen-discovery/sites/$serverName/conf/crontab_settings.txt", $newContent);
+		}
+	} else {
+		echo("- Could not find cron settings file\n");
+	}
+
+} else {
+	echo 'Must provide servername as first argument';
+	exit();
+}
