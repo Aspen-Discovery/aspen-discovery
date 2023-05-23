fbdc6b54868827943a783585d75d40a202b41e92
Kirstien Kroeger <kirstien@bywatersolutions.com>
2023-05-23 18:19:33 -0500

    temp drop in browse category image caching

--- code/aspen_app/src/screens/BrowseCategory/Home.js
+++ code/aspen_app/src/screens/BrowseCategory/Home.js
@@ -214,7 +214,7 @@ export const DiscoverHomeScreen = () => {
                          alt={item.title_display}
                          source={{
                               uri: `${imageUrl}`,
-                              expiresIn: 3600,
+                              expiresIn: 1,
                          }}
                          style={{
                               width: '100%',
