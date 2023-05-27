c6faa5101a1d9b0da16fbb8e5f29358054002694
James Staub <james.staub@nashville.gov>
2023-05-27 08:24:24 -0500

    Fix for Nashville Nonresident, continued

--- code/web/Drivers/Nashville.php
+++ code/web/Drivers/Nashville.php
@@ -24,83 +24,75 @@ class Nashville extends CarlX {
         'NR' => '01', // NR = Non Resident, a juke to identify Nashville non resident account fees
     ];
 
-	public function completeFinePayment(User $patron, UserPayment $payment): array {
+	public function completeFinePayment(User $user, UserPayment $payment): array {
 		global $logger;
 		global $serverName;
 		require_once ROOT_DIR . '/sys/Email/Mailer.php';
 		$mailer = new Mailer();
 		require_once ROOT_DIR . '/sys/SystemVariables.php';
 		$systemVariables = SystemVariables::getSystemVariables();
-
 		$accountLinesPaid = explode(',', $payment->finesPaid);
-		$user = new User();
 		$user->id = $payment->userId;
-		if ($user->find(true)) {
-			$patronId = $user->cat_username;
-			$allPaymentsSucceed = true;
-			foreach ($accountLinesPaid as $line) {
-				// MSB Payments are in the form of fineId|paymentAmount
-				[
-					$feeId,
-					$pmtAmount,
-				] = explode('|', $line);
-				[
-					$feeId,
-					$feeType,
-				] = explode('-', $feeId);
-				$feeTypeSIP = Nashville::$fineTypeSIP2Translations[$feeType];
-				if (strlen($feeId) == 13 && strpos($feeId, '1700') === 0) { // we stripped out leading octothorpes (#) from CarlX manual fines in CarlX.php getFines() which take the form "#".INSTBIT (Institution; Nashville = 1700) in order to sidestep CSS/javascript selector "#" problems; need to add them back for updating CarlX via SIP2 Fee Paid
-					$feeId = '#' . $feeId;
-				}
-				$response = $this->feePaidViaSIP($feeTypeSIP, '02', $pmtAmount, 'USD', $feeId, '', $patronId); // As of CarlX 9.6, SIP2 37/38 BK transaction id is written by CarlX as a receipt number; CarlX will not keep information passed through 37 BK; hence transId should be empty instead of, e.g., MSB's Transaction ID at $payment->orderId
-				// If failed with 'Invalid patron ID', check for changed patron ID
-				if ($response['success'] === false && $response['message'] == 'Invalid patron ID.') {
-					$newPatronIds = $this->getPatronIDChanges($patronId);
-					if ($newPatronIds) {
-						foreach ($newPatronIds as $newPatronId) {
-							$logger->log("MSB Payment CarlX update failed on Payment Reference ID $payment->id on FeeID $feeId : " . $response['message'] . ". Trying patron id change lookup on $patronId, found " . $newPatronId['NEWPATRONID'], Logger::LOG_ERROR);
-							$response = $this->feePaidViaSIP($feeTypeSIP, '02', $pmtAmount, 'USD', $feeId, '', $newPatronId['NEWPATRONID']);
-							if ($response['success'] === false) {
-								$logger->log("MSB Payment CarlX update failed on Payment Reference ID $payment->id on FeeID $feeId : " . $response['message'], Logger::LOG_ERROR);
-								$allPaymentsSucceed = false;
-							} else {
-								$logger->log("MSB Payment CarlX update succeeded on Payment Reference ID $payment->id on FeeID $feeId : " . $response['message'], Logger::LOG_DEBUG);
-								$allPaymentsSucceed = true;
-								break;
-							}
+		$patronId = $user->cat_username;
+		$allPaymentsSucceed = true;
+		foreach ($accountLinesPaid as $line) {
+			// MSB Payments are in the form of fineId|paymentAmount
+			[
+				$feeId,
+				$pmtAmount,
+			] = explode('|', $line);
+			[
+				$feeId,
+				$feeType,
+			] = explode('-', $feeId);
+			$feeTypeSIP = Nashville::$fineTypeSIP2Translations[$feeType];
+			if (strlen($feeId) == 13 && strpos($feeId, '1700') === 0) { // we stripped out leading octothorpes (#) from CarlX manual fines in CarlX.php getFines() which take the form "#".INSTBIT (Institution; Nashville = 1700) in order to sidestep CSS/javascript selector "#" problems; need to add them back for updating CarlX via SIP2 Fee Paid
+				$feeId = '#' . $feeId;
+			}
+			$response = $this->feePaidViaSIP($feeTypeSIP, '02', $pmtAmount, 'USD', $feeId, '', $patronId); // As of CarlX 9.6, SIP2 37/38 BK transaction id is written by CarlX as a receipt number; CarlX will not keep information passed through 37 BK; hence transId should be empty instead of, e.g., MSB's Transaction ID at $payment->orderId
+			// If failed with 'Invalid patron ID', check for changed patron ID
+			if ($response['success'] === false && $response['message'] == 'Invalid patron ID.') {
+				$newPatronIds = $this->getPatronIDChanges($patronId);
+				if ($newPatronIds) {
+					foreach ($newPatronIds as $newPatronId) {
+						$logger->log("MSB Payment CarlX update failed on Payment Reference ID $payment->id on FeeID $feeId : " . $response['message'] . ". Trying patron id change lookup on $patronId, found " . $newPatronId['NEWPATRONID'], Logger::LOG_ERROR);
+						$response = $this->feePaidViaSIP($feeTypeSIP, '02', $pmtAmount, 'USD', $feeId, '', $newPatronId['NEWPATRONID']);
+						if ($response['success'] === false) {
+							$logger->log("MSB Payment CarlX update failed on Payment Reference ID $payment->id on FeeID $feeId : " . $response['message'], Logger::LOG_ERROR);
+							$allPaymentsSucceed = false;
+						} else {
+							$logger->log("MSB Payment CarlX update succeeded on Payment Reference ID $payment->id on FeeID $feeId : " . $response['message'], Logger::LOG_DEBUG);
+							$allPaymentsSucceed = true;
+							break;
 						}
-					} else {
-						$logger->log("MSB Payment CarlX update failed on Payment Reference ID $payment->id on FeeID $feeId : " . $response['message'] . ". Trying patron id change lookup... failed to find patron id change for $patronId", Logger::LOG_ERROR);
 					}
-				}
-				if ($response['success'] === false) {
-					$logger->log("MSB Payment CarlX update failed on Payment Reference ID $payment->id on FeeID $feeId : " . $response['message'], Logger::LOG_ERROR);
-					$allPaymentsSucceed = false;
 				} else {
-					$logger->log("MSB Payment CarlX update succeeded on Payment Reference ID $payment->id on FeeID $feeId : " . $response['message'], Logger::LOG_DEBUG);
-                    if ($feeType == 'NR') {
-                        $this->updateNonResident($user);
-                    }
+					$logger->log("MSB Payment CarlX update failed on Payment Reference ID $payment->id on FeeID $feeId : " . $response['message'] . ". Trying patron id change lookup... failed to find patron id change for $patronId", Logger::LOG_ERROR);
 				}
 			}
-			if ($allPaymentsSucceed === false) {
-				$success = false;
-				$message = "MSB Payment CarlX update failed for Payment Reference ID $payment->id . See messages.log for details on individual items.";
-				$level = Logger::LOG_ERROR;
-				$payment->completed = 9;
+			if ($response['success'] === false) {
+				$logger->log("MSB Payment CarlX update failed on Payment Reference ID $payment->id on FeeID $feeId : " . $response['message'], Logger::LOG_ERROR);
+				$allPaymentsSucceed = false;
 			} else {
-				$success = true;
-				$message = "MSB payment successfully recorded in CarlX for Payment Reference ID $payment->id .";
-				$level = Logger::LOG_NOTICE;
-				$payment->completed = 1;
+				$logger->log("MSB Payment CarlX update succeeded on Payment Reference ID $payment->id on FeeID $feeId : " . $response['message'], Logger::LOG_DEBUG);
+				if ($feeType == 'NR') {
+					$this->updateNonResident($user);
+				}
 			}
-			$payment->update();
-			$this->createPatronPaymentNote($patronId, $payment->id);
-		} else {
-			$success = false;
-			$message = 'User Payment ' . $payment->id . 'failed with Invalid Patron';
-			$level = Logger::LOG_ERROR;
 		}
+		if ($allPaymentsSucceed === false) {
+			$success = false;
+			$message = "MSB Payment CarlX update failed for Payment Reference ID $payment->id . See messages.log for details on individual items.";
+			$level = Logger::LOG_ERROR;
+			$payment->completed = 9;
+		} else {
+			$success = true;
+			$message = "MSB payment successfully recorded in CarlX for Payment Reference ID $payment->id .";
+			$level = Logger::LOG_NOTICE;
+			$payment->completed = 1;
+		}
+		$payment->update();
+		$this->createPatronPaymentNote($patronId, $payment->id);
 		$logger->log($message, $level);
 		if ($level == Logger::LOG_ERROR) {
 			if (!empty($systemVariables->errorEmail)) {
@@ -124,7 +116,7 @@ class Nashville extends CarlX {
     // Following successful online payment, update Patron with new Expiration Date
     protected function updateNonResident(User $user): array {
         global $logger;
-	$patronId = $user->cat_username;
+		$patronId = $user->cat_username;
         $request = $this->getSearchbyPatronIdRequest($user);
         $request->Patron = new stdClass();
         $request->Patron->ExpirationDate = date('c', strtotime('+1 year'));
