c434fd2d7fd0b280c8f8e549cca40a89cc892a3a
Mark Noble <mdnoble73@gmail.com>
2023-06-04 12:49:22 -0600

    Allow scheduling full updates of Aspen Discovery

--- code/web/cron/runScheduledUpdate.php
+++ code/web/cron/runScheduledUpdate.php
@@ -31,61 +31,42 @@ if (count($updatesToRun) == 0) {
 				$scheduledUpdate->notes = "FAILED: Bad version to update to $versionToUpdateTo \n";
 			}else{
 				if (str_replace('.', '', $versionToUpdateTo) >= str_replace('.', '', $currentVersion)) {
-					if ($scheduledUpdate->updateType === 'complete') {
-						$scheduledUpdate->notes .= "FAILED: Complete updates are not supported yet";
-					} elseif ($scheduledUpdate->updateType === 'patch') {
-						//assume it works and update to false if there are issues.
-						$updateSucceeded = true;
-						if (strcasecmp($configArray['System']['operatingSystem'], 'windows') == 0) {
-							exec("cd c:\web\aspen-discovery; git fetch origin; git reset --hard origin/$versionToUpdateTo 2>&1", $resetGitResult);
-							$scheduledUpdate->notes .= "Resetting git to branch $versionToUpdateTo\n";
-							foreach ($resetGitResult as $result) {
-								$scheduledUpdate->notes .= $result . "\n";
-							}
+					$operatingSystem = $configArray['System']['operatingSystem'];
+					$linuxDistribution = '';
+					if (strcasecmp($operatingSystem, 'windows') == 0) {
+						$installDir = 'c:\web\aspen-discovery';
+					} else {
+						$installDir = '/usr/local/aspen-discovery';
+						$osInformation = getOSInformation();
+						if ($osInformation != null) {
+							$linuxDistribution = $osInformation['id'];
+							$scheduledUpdate->notes .= "Linux distribution is $linuxDistribution\n";
 						} else {
-							$scheduledUpdate->notes .= "Fetching all changes from git\n";
-							exec("cd /usr/local/aspen-discovery; git fetch origin", $resetGitResult, $resultCode);
-							foreach ($resetGitResult as $result) {
-								$scheduledUpdate->notes .= $result . "\n";
-							}
-							if (!hasErrors($scheduledUpdate->notes)) {
-								$scheduledUpdate->notes .= "Resetting git to branch $versionToUpdateTo\n";
-								exec("cd /usr/local/aspen-discovery; git reset --hard origin/$versionToUpdateTo 2>&1", $resetGitResult, $resultCode);
-								foreach ($resetGitResult as $result) {
-									$scheduledUpdate->notes .= $result . "\n";
-								}
-							}
+							$scheduledUpdate->notes .= "Could not determine Linux distribution\n";
 						}
+					}
 
-						if (!hasErrors($scheduledUpdate->notes)) {
-							if (strcasecmp($configArray['System']['operatingSystem'], 'windows') == 0) {
-								exec("cd c:\web\aspen-discovery; git pull origin $versionToUpdateTo", $gitResult);
-							} else {
-								exec("cd /usr/local/aspen-discovery; git pull origin $versionToUpdateTo 2>&1", $gitResult);
-							}
-							$scheduledUpdate->notes .= "Pulling branch $currentVersion\n";
-							foreach ($gitResult as $result) {
-								$scheduledUpdate->notes .= $result . "\n";
-							}
-						}
-
-						if (!hasErrors($scheduledUpdate->notes)) {
-							// run db maintenance
-							$scheduledUpdate->notes .= "Running database maintenance $currentVersion\n";
-							require_once ROOT_DIR . '/services/API/SystemAPI.php';
-							$systemAPI = new SystemAPI();
-							$dbMaintenance = $systemAPI->runPendingDatabaseUpdates();
-							if (!isset($dbMaintenance['success']) || $dbMaintenance['success'] == false) {
-								$scheduledUpdate->status = 'failed';
-							}
-							if (isset($dbMaintenance['message'])) {
-								$message = $dbMaintenance['message'] ?? '';
-								$scheduledUpdate->notes .= $message;
-							}
+					//Check to see if this is a secondary site or a full site
+					$systemVariables = SystemVariables::getSystemVariables();
+					$isSecondarySite = false;
+					if (!empty($systemVariables)) {
+						$isSecondarySite = $systemVariables->doQuickUpdates;
+					}
+					if ($isSecondarySite) {
+						if ($scheduledUpdate->updateType === 'complete') {
+							doFullSecondaryUpgrade($operatingSystem, $linuxDistribution, $serverName, $versionToUpdateTo, $installDir, $scheduledUpdate);
+						} elseif ($scheduledUpdate->updateType === 'patch') {
+							doSecondaryUpdate($operatingSystem, $linuxDistribution, $versionToUpdateTo, $scheduledUpdate);
 						}
 					} else {
-						// invalid updateType
-						$scheduledUpdate->notes = "FAILED: Invalid update type\n";
+						if ($scheduledUpdate->updateType === 'complete') {
+							doFullUpgrade($operatingSystem, $linuxDistribution, $serverName, $versionToUpdateTo, $installDir, $scheduledUpdate);
+						} elseif ($scheduledUpdate->updateType === 'patch') {
+							doPatchUpgrade($operatingSystem, $versionToUpdateTo, $scheduledUpdate);
+						} else {
+							// invalid updateType
+							$scheduledUpdate->notes = "FAILED: Invalid update type\n";
+						}
 					}
 				} else {
 					$scheduledUpdate->notes = "FAILED: Must update to a version that is the same or newer than the current version of $currentVersion\n";
@@ -132,6 +113,232 @@ if (count($updatesToRun) == 0) {
 	console_log("Finished running " . count($updatesToRun) . " updates\n");
 }
 
+/**
+ * @param $operatingSystem
+ * @param $versionToUpdateTo
+ * @param ScheduledUpdate $scheduledUpdate
+ * @return void
+ */
+function doPatchUpgrade($operatingSystem, $versionToUpdateTo, ScheduledUpdate $scheduledUpdate): void{
+	if ($operatingSystem == 'linux') {
+		executeCommand("Stopping java", "pkill java", $scheduledUpdate);
+	}
+	updateGitAndRunDatabaseUpdates($operatingSystem, $versionToUpdateTo, $scheduledUpdate);
+}
+
+/**
+ * @param $operatingSystem
+ * @param $versionToUpdateTo
+ * @param ScheduledUpdate $scheduledUpdate
+ * @return void
+ */
+function updateGitAndRunDatabaseUpdates($operatingSystem, $versionToUpdateTo, ScheduledUpdate $scheduledUpdate): void {
+	if (strcasecmp($operatingSystem, 'windows') == 0) {
+		$installDir = 'c:\web\aspen-discovery';
+	} else {
+		$installDir = '/usr/local/aspen-discovery';
+	}
+	executeCommand("Fetching all changes from git", "cd $installDir; git fetch origin", $scheduledUpdate);
+	if (!hasErrors($scheduledUpdate->notes)) {
+		executeCommand("Resetting git to branch $versionToUpdateTo", "cd $installDir; git reset --hard origin/$versionToUpdateTo 2>&1", $scheduledUpdate);
+	}
+
+	if (!hasErrors($scheduledUpdate->notes)) {
+		executeCommand("Pulling branch $versionToUpdateTo", "cd $installDir; git pull origin $versionToUpdateTo", $scheduledUpdate);
+	}
+
+	if (!hasErrors($scheduledUpdate->notes)) {
+		runDatabaseMaintenance($versionToUpdateTo, $scheduledUpdate);
+	}
+}
+
+function runDatabaseMaintenance($versionToUpdateTo, $scheduledUpdate) {
+	// run db maintenance
+	$scheduledUpdate->notes .= "Running database maintenance $versionToUpdateTo\n";
+	require_once ROOT_DIR . '/services/API/SystemAPI.php';
+	$systemAPI = new SystemAPI();
+	$dbMaintenance = $systemAPI->runPendingDatabaseUpdates();
+	if (!isset($dbMaintenance['success']) || $dbMaintenance['success'] == false) {
+		$scheduledUpdate->status = 'failed';
+	}
+	if (isset($dbMaintenance['message'])) {
+		$message = $dbMaintenance['message'] ?? '';
+		$scheduledUpdate->notes .= $message;
+	}
+}
+
+/**
+ * @param $operatingSystem
+ * @param $linuxDistribution
+ * @param $serverName
+ * @param $versionToUpdateTo
+ * @param $installDir
+ * @param ScheduledUpdate $scheduledUpdate
+ * @return void
+ */
+function doFullSecondaryUpgrade($operatingSystem, $linuxDistribution, $serverName, $versionToUpdateTo, $installDir, ScheduledUpdate &$scheduledUpdate): void {
+	if ($operatingSystem == 'linux') {
+		if ($linuxDistribution == 'debian') {
+			executeCommand("Stopping cron", "service cron stop", $scheduledUpdate);
+		} else {
+			executeCommand("Stopping cron", "service crond stop", $scheduledUpdate);
+			executeCommand("Running system updates", "yum -y update", $scheduledUpdate);
+		}
+	}
+	if ($operatingSystem == 'linux') {
+		executeCommand("Stopping java", "pkill java", $scheduledUpdate);
+	}
+
+	// Run database updates
+	runDatabaseMaintenance($versionToUpdateTo, $scheduledUpdate);
+
+	//Run version specific upgrade script
+	if ($operatingSystem == 'linux') {
+		if ($linuxDistribution == 'debian') {
+			if (file_exists("$installDir/install/upgrade_debian_$versionToUpdateTo.sh")) {
+				executeCommand("Running version upgrade script", "cd $installDir/install; ./upgrade_debian_$versionToUpdateTo.sh $serverName", $scheduledUpdate);
+			}
+		} else {
+			if (file_exists("$installDir/install/upgrade_$versionToUpdateTo.sh")) {
+				executeCommand("Running version upgrade script", "cd $installDir/install; ./upgrade_$versionToUpdateTo.sh $serverName", $scheduledUpdate);
+			}
+		}
+	}
+
+	//Update Solr files
+	if ($operatingSystem == 'linux') {
+		if ($linuxDistribution == 'debian') {
+			executeCommand("Updating Solr files", "cd $installDir/data_dir_setup; ./update_solr_files_debian.sh $serverName", $scheduledUpdate);
+		} else {
+			executeCommand("Updating Solr files", "cd $installDir/data_dir_setup; ./update_solr_files.sh $serverName", $scheduledUpdate);
+		}
+	} else {
+		executeCommand("Updating Solr files", "cd $installDir/data_dir_setup; ./update_solr_files.bat $serverName", $scheduledUpdate);
+	}
+
+	//Restart services
+	if ($operatingSystem == 'linux') {
+		//Start cron
+		if ($linuxDistribution == 'debian') {
+			executeCommand("Starting cron", "service cron start", $scheduledUpdate);
+		} else {
+			executeCommand("Starting cron", "service crond start", $scheduledUpdate);
+		}
+	}
+}
+
+/**
+ * @param $operatingSystem
+ * @param $linuxDistribution
+ * @param $versionToUpdateTo
+ * @param ScheduledUpdate $scheduledUpdate
+ * @return void
+ */
+function doSecondaryUpdate($operatingSystem, $linuxDistribution, $versionToUpdateTo, ScheduledUpdate &$scheduledUpdate): void {
+	if ($operatingSystem == 'linux') {
+		if ($linuxDistribution == 'debian') {
+			executeCommand("Stopping cron", "service cron stop", $scheduledUpdate);
+		} else {
+			executeCommand("Stopping cron", "service crond stop", $scheduledUpdate);
+			executeCommand("Running system updates", "yum -y update", $scheduledUpdate);
+		}
+	}
+
+	if ($operatingSystem == 'linux') {
+		executeCommand("Stopping java", "pkill java", $scheduledUpdate);
+	}
+
+	// Run database updates
+	runDatabaseMaintenance($versionToUpdateTo, $scheduledUpdate);
+
+	//Restart services
+	if ($operatingSystem == 'linux') {
+		//Start cron
+		if ($linuxDistribution == 'debian') {
+			executeCommand("Starting cron", "service cron start", $scheduledUpdate);
+		} else {
+			executeCommand("Starting cron", "service crond start", $scheduledUpdate);
+		}
+	}
+}
+
+/**
+ * @param $operatingSystem
+ * @param $linuxDistribution
+ * @param $serverName
+ * @param $versionToUpdateTo
+ * @param $installDir
+ * @param ScheduledUpdate $scheduledUpdate
+ * @return void
+ */
+function doFullUpgrade($operatingSystem, $linuxDistribution, $serverName, $versionToUpdateTo, $installDir, ScheduledUpdate &$scheduledUpdate): void {
+
+	//Prepare the system to be updated
+	if ($operatingSystem == 'linux') {
+		if ($linuxDistribution == 'debian') {
+			executeCommand("Stopping cron", "service cron stop", $scheduledUpdate);
+		} else {
+			executeCommand("Stopping cron", "service crond stop", $scheduledUpdate);
+			executeCommand("Running system updates", "yum -y update", $scheduledUpdate);
+		}
+	}
+	if ($operatingSystem == 'linux') {
+		executeCommand("Stopping java", "pkill java", $scheduledUpdate);
+	}
+
+	//Update the system
+	updateGitAndRunDatabaseUpdates($operatingSystem, $versionToUpdateTo, $scheduledUpdate);
+
+	//Run version specific upgrade script
+	if ($operatingSystem == 'linux') {
+		if ($linuxDistribution == 'debian') {
+			if (file_exists("$installDir/install/upgrade_debian_$versionToUpdateTo.sh")) {
+				executeCommand("Running version upgrade script", "cd $installDir/install; ./upgrade_debian_$versionToUpdateTo.sh $serverName", $scheduledUpdate);
+			}
+		} else {
+			if (file_exists("$installDir/install/upgrade_$versionToUpdateTo.sh")) {
+				executeCommand("Running version upgrade script", "cd $installDir/install; ./upgrade_$versionToUpdateTo.sh $serverName", $scheduledUpdate);
+			}
+		}
+	}
+
+	//Update Solr files
+	if ($operatingSystem == 'linux') {
+		if ($linuxDistribution == 'debian') {
+			executeCommand("Updating Solr files", "cd $installDir/data_dir_setup; ./update_solr_files_debian.sh $serverName", $scheduledUpdate);
+		} else {
+			executeCommand("Updating Solr files", "cd $installDir/data_dir_setup; ./update_solr_files.sh $serverName", $scheduledUpdate);
+		}
+	} else {
+		executeCommand("Updating Solr files", "cd $installDir/data_dir_setup; ./update_solr_files.bat $serverName", $scheduledUpdate);
+	}
+
+	//Restart services
+	if ($operatingSystem == 'linux') {
+		//Restart mysql
+		executeCommand("Restarting MySQL", "service mysqld restart", $scheduledUpdate);
+		//Restart apache
+		executeCommand("Restarting apache", "apachectl graceful", $scheduledUpdate);
+		//Start cron
+		if ($linuxDistribution == 'debian') {
+			executeCommand("Starting cron", "service cron start", $scheduledUpdate);
+		} else {
+			executeCommand("Starting cron", "service crond start", $scheduledUpdate);
+		}
+	}
+
+	//Run git cleanup
+	executeCommand("Cleaning up git", "cd $installDir; git gc", $scheduledUpdate);
+}
+
+function executeCommand(string $commandNote, string $commandToExecute, ScheduledUpdate $scheduledUpdate) {
+	$scheduledUpdate->notes .= $commandNote . "\n";
+	exec($commandToExecute, $execResult);
+	foreach ($execResult as $result) {
+		$scheduledUpdate->notes .= $result . "\n";
+	}
+}
+
 function hasErrors($notes) : bool {
 	$lowerNotes = strtolower($notes);
 	if ((strpos($lowerNotes, 'fatal') !== false) || (strpos($lowerNotes, 'failed') !== false) || (strpos($lowerNotes, 'rejected') !== false)) {
@@ -139,6 +346,35 @@ function hasErrors($notes) : bool {
 	} else {
 		return false;
 	}
+}
+
+function getOSInformation() {
+	if (false == function_exists("shell_exec") || false == is_readable("/etc/os-release")) {
+		return null;
+	}
+
+	$os         = shell_exec('cat /etc/os-release');
+	if (preg_match_all('/.*=/', $os, $matchListIds)) {
+		$listIds    = $matchListIds[0];
+	} else {
+		$listIds = [];
+	}
+
+	if (preg_match_all('/=.*/', $os, $matchListVal)) {
+		$listVal = $matchListVal[0];
+	} else {
+		$listVal = [];
+	}
+
+	array_walk($listIds, function(&$v, $k){
+		$v = strtolower(str_replace('=', '', $v));
+	});
+
+	array_walk($listVal, function(&$v, $k){
+		$v = preg_replace('/[="]/', '', $v);
+	});
+
+	return array_combine($listIds, $listVal);
 }
 
 function console_log($message, $prefix = '') {
