5d5948b40f2c1bbc1e4d546a170c7c115317501a
James Staub <james.staub@nashville.gov>
2023-06-27 14:07:50 -0500

    Update setup_server.bat

--- setup_server.bat
+++ setup_server.bat
@@ -1,41 +1,45 @@
 @echo off
-rem Setup data and logs for a new server by copying the appropriate files from default. 
-set SERVERNAME=%1
+rem Setup data and logs for a new aspen-discovery server by copying the appropriate files from default.
+set HOST=%1
 shift
-
-if not "!%SERVERNAME%!"=="!!" goto serverset
+if not "!%HOST%!"=="!!" goto serverset
 goto usage
+
 :serverset
-
-rem Do the bulk of the copy work
-
-rem get the directory we started with 
-set WORKINGDIR=%CD%
-echo WORKINGDIR is %WORKINGDIR%
+set WD=%CD%
+echo Working directory is %WD%
+echo Server name is %HOST%
 
 echo setting up data directory
 cd c:\data
 mkdir aspen-discovery
 cd c:\data\aspen-discovery
-mkdir %SERVERNAME%
-cd %WORKINGDIR%
-cp -rp data_dir_setup/* c:\data\aspen-discovery\%SERVERNAME%
+echo creating accelerated reader data folder
+mkdir accelerated_reader
+mkdir %HOST%
+cd %HOST%
+copy %WD%/data_dir_setup/* .
 
 echo setting up logs directory
 cd c:\var\log
 mkdir aspen-discovery
 cd c:\var\log\aspen-discovery
-mkdir %SERVERNAME%
-cd c:\var\log\aspen-discovery\%SERVERNAME%
-mkdir jetty
-cd %WORKINGDIR%
+mkdir %HOST%
+cd %HOST%
+
+echo Installing Solr Files for HOST
+cd %WD%/data_dir_setup/
+update_solr_files.bat %HOST%
+
+echo In lieu of "Creating symbolic link in /etc/httpd/conf.d to apache config file"
+echo Windows apache config file (httpd.conf) needs an Include pointing to %HOST% file, e.g.,
+echo Include "c:/web/aspen-discovery/sites/nashville.aspenlocal/httpd-nashville.aspenlocal.conf"
+
 goto end
 
 :usage
-echo Usage: setup_server {servername}
+echo Usage: setup_server {host}
 echo.
 goto end
 
-:end
-rem We're all done -- close down the local environment.
-endlocal
+:done
