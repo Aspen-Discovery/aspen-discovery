78a566132eaa236d396bdbc5a8a20b60d41c96ad
Mark Noble <mdnoble73@gmail.com>
2023-07-07 14:51:41 -0600

    Add basic release notes and database updates

---
+++ code/web/sys/DBMaintenance/version_updates/23.08.00.php
@@ -0,0 +1,28 @@
+<?php
+/** @noinspection PhpUnused */
+function getUpdates23_08_00(): array {
+	$curTime = time();
+	return [
+		/*'name' => [
+			'title' => '',
+			'description' => '',
+			'continueOnError' => false,
+			'sql' => [
+				''
+			]
+		], //sample*/
+
+
+		//mark - ByWater
+
+
+		//kirstien - ByWater
+
+
+		//kodi - ByWater
+
+
+		//other organizations
+
+	];
+}
