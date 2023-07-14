57fe21bd1497d6bd34df02b58498e7f57dbeeda3
Kirstien Kroeger <kirstien@bywatersolutions.com>
2023-07-14 10:53:08 -0500

    notification onboarding fixes

--- code/aspen_app/src/screens/BrowseCategory/Home.js
+++ code/aspen_app/src/screens/BrowseCategory/Home.js
@@ -18,7 +18,6 @@ import { navigateStack } from '../../helpers/RootNavigator';
 import { fetchReadingHistory, fetchSavedSearches, getLinkedAccounts, getPatronCheckedOutItems, getPatronHolds, getViewerAccounts, reloadProfile, updateNotificationOnboardingStatus } from '../../util/api/user';
 import { getTermFromDictionary } from '../../translations/TranslationService';
 import { NotificationsOnboard } from '../../components/NotificationsOnboard';
-import { getNotificationPreference } from '../../components/Notifications';
 
 let maxCategories = 5;
 
@@ -27,7 +26,7 @@ export const DiscoverHomeScreen = () => {
      const navigation = useNavigation();
      const [loading, setLoading] = React.useState(false);
      const [showNotificationsOnboarding, setShowNotificationsOnboarding] = React.useState(false);
-     const [alreadyCheckedNotifications, setAlreadyCheckedNotifications] = React.useState(false);
+     const [alreadyCheckedNotifications, setAlreadyCheckedNotifications] = React.useState(true);
      const { user, locations, accounts, cards, lists, updateUser, updateLanguage, updatePickupLocations, updateLinkedAccounts, updateLists, updateLibraryCards, updateLinkedViewerAccounts, updateReadingHistory, notificationSettings, expoToken, updateNotificationOnboard, notificationOnboard } = React.useContext(UserContext);
      const { library } = React.useContext(LibrarySystemContext);
      const { category, updateBrowseCategories, updateBrowseCategoryList, updateMaxCategories } = React.useContext(BrowseCategoryContext);
@@ -160,13 +159,17 @@ export const DiscoverHomeScreen = () => {
      useFocusEffect(
           React.useCallback(() => {
                const checkSettings = async () => {
-                    console.log('notificationOnboard: ' + notificationOnboard);
                     if (!_.isUndefined(notificationOnboard)) {
                          if (notificationOnboard === 1 || notificationOnboard === 2 || notificationOnboard === '1' || notificationOnboard === '2') {
                               setShowNotificationsOnboarding(true);
+                              setAlreadyCheckedNotifications(false);
                          } else {
                               setShowNotificationsOnboarding(false);
                          }
+                    } else {
+                         updateNotificationOnboard(1);
+                         setShowNotificationsOnboarding(true);
+                         setAlreadyCheckedNotifications(false);
                     }
                };
                checkSettings().then(() => {
@@ -177,7 +180,7 @@ export const DiscoverHomeScreen = () => {
 
      // load notification onboarding prompt
      //console.log('showNotificationsOnboarding: ' + showNotificationsOnboarding);
-     if (showNotificationsOnboarding && !alreadyCheckedNotifications && Device.isDevice) {
+     if (showNotificationsOnboarding && !alreadyCheckedNotifications && Device.isDevice && notificationOnboard !== '0' && notificationOnboard !== '0') {
           return <NotificationsOnboard setAlreadyCheckedNotifications={setAlreadyCheckedNotifications} />;
      }
 
