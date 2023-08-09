0db7524f4d4d007220b0c867e36cb6af4eac8a84
Jacob O'Mara <jacobomara901@gmail.com>
2023-08-09 16:54:10 +0100

    feat: Allow users to set cookie bar enable/disable on a per library basis
    This patch moves the Cookie Consent settings from system variables to
    Library systems settings to allow you to set whether the cookie consent
    bar is activated on a per Library system basis. This will also allow you to set a different cookie policy for different libraries.
    
    - Amend index.php to look in library system settings for if cookie consent is enabled and the cookie policy HTML.
    - DB update to move columns for cookie consent to the correct Library
      table instead of system variables.
    - Add the menu items for Cookie Consent to Library.php
    - Remove the same menu items from SystemVariables.php
    - Move the Data protection section to Library.php but leave IP tracking in SystemVariable, no longer in a section.

--- code/web/sys/DBMaintenance/version_updates/23.08.00.php
+++ code/web/sys/DBMaintenance/version_updates/23.08.00.php
@@ -183,5 +183,17 @@ function getUpdates23_08_00(): array {
 			]
 		],
 		//add theming to cookieConsent banner
+		'move_cookieConsent_to_library_settings' => [
+			'title' => 'Move Cookie Consent toggle to library settings',
+			'description' => 'Moves the cookie consent toggle from system settings to library settings',
+			'continueOnError' => true,
+			'sql' => [
+				"ALTER TABLE system_variables DROP COLUMN cookieStorageConsent",
+				"ALTER TABLE system_variables DROP COLUMN cookiePolicyHTML",
+				"ALTER TABLE library ADD COLUMN cookieStorageConsent tinyint(1) DEFAULT 0",
+				"ALTER TABLE library ADD COLUMN cookiePolicyHTML TEXT",
+				"UPDATE library set cookiePolicyHTML = 'This body has not yet set a cookie storage policy, please check back later.'",
+			]
+		]
 	];
 }
