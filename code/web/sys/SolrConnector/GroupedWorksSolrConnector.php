8747adca2534ebd12aaea9ae29bce1822ed9517d
Mark Noble <mdnoble73@gmail.com>
2023-08-21 17:50:21 -0600

    Updates for boosting by branch location (do not do boosting by library, only location)

--- code/web/sys/SolrConnector/GroupedWorksSolrConnector.php
+++ code/web/sys/SolrConnector/GroupedWorksSolrConnector.php
@@ -200,7 +200,7 @@ class GroupedWorksSolrConnector extends Solr {
 			$options['fq'][] = '-user_reading_history_link:' . UserAccount::getActiveUserId();
 		}
 
-		$boostFactors = $this->getBoostFactors($searchLibrary);
+		$boostFactors = $this->getBoostFactors($searchLibrary, $searchLocation);
 		if (!empty($boostFactors)) {
 			$options['bf'] = $boostFactors;
 		}
@@ -268,7 +268,7 @@ class GroupedWorksSolrConnector extends Solr {
 		foreach ($scopingFilters as $filter) {
 			$options['fq'][] = $filter;
 		}
-		$boostFactors = $this->getBoostFactors($searchLibrary);
+		$boostFactors = $this->getBoostFactors($searchLibrary, $searchLocation);
 		if (!empty($boostFactors)) {
 			$options['bf'] = $boostFactors;
 		}
@@ -340,7 +340,7 @@ class GroupedWorksSolrConnector extends Solr {
 	 * @param Library $searchLibrary
 	 * @return array
 	 */
-	public function getBoostFactors($searchLibrary) {
+	public function getBoostFactors($searchLibrary, $searchLocation) {
 		global $activeLanguage;
 
 		$boostFactors = [];
