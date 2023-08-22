f619e08480ff785f98bd18271197e7fdfc196640
Mark Noble <mdnoble73@gmail.com>
2023-08-22 11:22:35 -0600

    Formatting updates

--- code/web/sys/Events/LibraryEventsSetting.php
+++ code/web/sys/Events/LibraryEventsSetting.php
@@ -6,32 +6,36 @@ class LibraryEventsSetting extends DataObject {
 	public $id;
 	public $settingSource;
 	public $settingId;
-    public $eventsFacetSettingsId;
+	public $eventsFacetSettingsId;
 	public $libraryId;
 
-    private $_facetGroup = false;
+	private $_facetGroup = false;
 
-    /** @return EventsFacet[] */
-    public function getFacets() {
-        try {
-            return $this->getFacetGroup()->getFacets();
-        } catch (Exception $e) {
-            return [];
-        }
-    }
+	/** @return EventsFacet[] */
+	public function getFacets() {
+		try {
+			if (!is_null($this->getFacetGroup())) {
+				return $this->getFacetGroup()->getFacets();
+			} else {
+				return [];
+			}
+		} catch (Exception $e) {
+			return [];
+		}
+	}
 
-    public function getFacetGroup(): ?EventsFacetGroup {
-        try {
-            if ($this->_facetGroup === false) {
-                $this->_facetGroup = new EventsFacetGroup();
-                $this->_facetGroup->id = $this->facetGroupId;
-                if (!$this->_facetGroup->find(true)) {
-                    $this->_facetGroup = null;
-                }
-            }
-            return $this->_facetGroup;
-        } catch (Exception $e) {
-            return null;
-        }
-    }
+	public function getFacetGroup(): ?EventsFacetGroup {
+		try {
+			if ($this->_facetGroup === false) {
+				$this->_facetGroup = new EventsFacetGroup();
+				$this->_facetGroup->id = $this->facetGroupId;
+				if (!$this->_facetGroup->find(true)) {
+					$this->_facetGroup = null;
+				}
+			}
+			return $this->_facetGroup;
+		} catch (Exception $e) {
+			return null;
+		}
+	}
 }
