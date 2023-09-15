c58faa2e4301b59049b5596d9338db0ed9436ef3
Jason Boyer <JBoyer@equinoxOLI.org>
2023-09-15 13:16:35 -0400

    Use supplied db host when creating new database
    Signed-off-by: Jason Boyer <JBoyer@equinoxOLI.org>

--- install/createSite.php
+++ install/createSite.php
@@ -304,7 +304,7 @@ exec("$mysqlConnectionCommand {$variables['aspenDBName']} < $installDir/install/
 
 
 //Connect to the database
-$aspen_db = new PDO("mysql:dbname={$variables['aspenDBName']};host=localhost",$variables['aspenDBUser'],$variables['aspenDBPwd']);
+$aspen_db = new PDO("mysql:dbname={$variables['aspenDBName']};host={$variables['aspenDBHost']}",$variables['aspenDBUser'],$variables['aspenDBPwd']);
 $updateUserStmt = $aspen_db->prepare("UPDATE user set cat_password=" . $aspen_db->quote($variables['aspenAdminPwd']) . ", password=" . $aspen_db->quote($variables['aspenAdminPwd']) . " where username = 'aspen_admin'");
 $updateUserStmt->execute();
 
