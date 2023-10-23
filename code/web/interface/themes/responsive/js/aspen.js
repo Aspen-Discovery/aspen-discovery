3b73382468cb1757d5c1c912f003c7d7f05e9198
Jacob O'Mara <jacob.omara@ptfs-europe.com>
2023-10-23 17:40:38 +0100

    feat: Implement ajax call to save Cookie Preferences
    Also, change cookie to session based cookie.

--- code/web/interface/themes/responsive/js/aspen.js
+++ code/web/interface/themes/responsive/js/aspen.js
@@ -15106,8 +15106,30 @@ AspenDiscovery.CookieConsent = (function() {
             aDate.setMonth(aDate.getMonth() + 3);
             $('.stripPopup').hide();
             $('.modal').modal('hide');
-            document.cookie = 'cookieConsent' + '=' + encodeURIComponent(aDate) + '; expires=' + aDate.toUTCString() + '; path=/';
-            return;
+            //set cookie and update db (if logged in) with AJAX
+			var url = Globals.path + "/AJAX/JSON";
+			var params =  {
+				method : 'saveCookiePreference',
+                cookieEssential: cookieString['Essential'],
+                cookieAnalytics: cookieString['Analytics'],
+			};
+			$.getJSON(url, params,
+				function(data) {
+					if (data.success) {
+						if (data.message.length > 0){
+							//User was logged in, show a message about how to update
+							AspenDiscovery.showMessage('Success', data.message, true, true);
+						}else{
+							//Refresh the page
+							// noinspection SillyAssignmentJS
+							window.location.href = window.location.href;
+						}
+					} else {
+						AspenDiscovery.showMessage("Error", data.message);
+					}
+				}
+			).fail(AspenDiscovery.ajaxFail);
+			return false;
         },
         cookieDisagree: function() {
             console.log('cookieDisagree');  
