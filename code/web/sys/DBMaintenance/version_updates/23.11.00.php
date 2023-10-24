93dfedae4cb44ab36344fd07c1abde1e9493b91c
Mark Noble <mdnoble73@gmail.com>
2023-10-24 05:04:33 -0600

    Update 23.11.00.php

--- code/web/sys/DBMaintenance/version_updates/23.11.00.php
+++ code/web/sys/DBMaintenance/version_updates/23.11.00.php
@@ -3,16 +3,21 @@
 function getUpdates23_11_00(): array {
 	$curTime = time();
 	return [
-        		//Alexander - PTFS
-			'display_list_author_control' => [
-				'title' => 'User List Author Control',
-				'description' => 'Add a setting to allow users to control whether their name appears on public lists they have created.',
-				'continueOnError' => true,
-				'sql' => [
-					'ALTER TABLE  user_list ADD COLUMN displayListAuthor TINYINT(1) DEFAULT 1',
-					'ALTER TABLE user ADD COLUMN displayListAuthor TINYINT(1) DEFAULT 1',
-				],
+		//mark - ByWater
+		//kirstien - ByWater
+		//kodi - ByWater
+		//Alexander - PTFS
+		'display_list_author_control' => [
+			'title' => 'User List Author Control',
+			'description' => 'Add a setting to allow users to control whether their name appears on public lists they have created.',
+			'continueOnError' => true,
+			'sql' => [
+				'ALTER TABLE  user_list ADD COLUMN displayListAuthor TINYINT(1) DEFAULT 1',
+				'ALTER TABLE user ADD COLUMN displayListAuthor TINYINT(1) DEFAULT 1',
+			],
 		],
+		//Jacob - PTFS
+		//other
     ];
 }
 
