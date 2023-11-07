04ee415fecdf1b7da5cbaac47f3e2d934682e721
Kodi <kodilein@gmail.com>
2023-11-07 08:49:49 -0800

    Update SirsiDynixROA.php

--- code/web/Drivers/SirsiDynixROA.php
+++ code/web/Drivers/SirsiDynixROA.php
@@ -3361,17 +3361,19 @@ class SirsiDynixROA extends HorizonAPI {
 			foreach ($customFields as $customField) {
 				if ($customField->symphonyName == 'library') {
 					$fields[$customField->symphonyName] = $pickupLocationField;
-				} elseif(($customField->symphonyName == 'parentname' || $customField->symphonyName == 'care_of' || $customField->symphonyName == 'careof') && $library->promptForParentInSelfReg) {
-					$fields[] = [
-						'property' => 'cardType',
-						'type' => 'enum',
-						'values' => [
-							'adult' => 'Adult (18 and Over)',
-							'minor' => 'Minor (Under 18)',
-						],
-						'label' => 'Type of Card',
-						'onchange' => 'AspenDiscovery.Account.updateSelfRegistrationFields()',
-					];
+				} elseif(($customField->symphonyName == 'parentname' || $customField->symphonyName == 'care_of' || $customField->symphonyName == 'careof')) {
+					if ($library->promptForParentInSelfReg){
+						$fields[] = [
+							'property' => 'cardType',
+							'type' => 'enum',
+							'values' => [
+								'adult' => 'Adult (18 and Over)',
+								'minor' => 'Minor (Under 18)',
+							],
+							'label' => 'Type of Card',
+							'onchange' => 'AspenDiscovery.Account.updateSelfRegistrationFields()',
+						];
+					}
 					$fields[$customField->symphonyName] = [
 						'property' => $customField->symphonyName,
 						'type' => $customField->fieldType,
