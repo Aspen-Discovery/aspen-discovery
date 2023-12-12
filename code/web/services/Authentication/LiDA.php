cedd6268c65921f19be8e131b5099edc15ea114c
Kirstien Kroeger <kirstien@bywatersolutions.com>
2023-12-11 18:11:29 -0600

    if session expired, create new

--- code/web/services/Authentication/LiDA.php
+++ code/web/services/Authentication/LiDA.php
@@ -25,13 +25,35 @@ class Authentication_LiDA extends Action {
 								$session->redirectUser($tmpUser, $returnTo);
 							}
 						}
+					} else {
+						$session->delete();
+
+						// create a new shared session
+						$data = random_bytes(16);
+						assert(strlen($data) == 16);
+						$data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+						$data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+						$uuid = vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+						require_once ROOT_DIR . '/sys/Session/SharedSession.php';
+						$sharedSession = new SharedSession();
+						$sharedSession->setSessionId($uuid);
+						$sharedSession->setUserId($_REQUEST['user']);
+						$sharedSession->setCreated(strtotime('now'));
+
+						if (UserAccount::findNewAspenUser('id', $_REQUEST['user'])) {
+							$tmpUser = new User();
+							$tmpUser->id = $_REQUEST['user'];
+							if ($tmpUser->find(true)) {
+								$session->redirectUser($tmpUser, $returnTo);
+							}
+						}
 					}
 				} else {
-					// ask the user to log into Discovery
+					// no matching shared session found, ask the user to log into Discovery
 					header('Location: /MyAccount/' . $returnTo . '?minimalInterface=true');
 				}
 			} else {
-				// ask the user to log into Discovery
+				// not enough data provided, ask the user to log into Discovery
 				header('Location: /MyAccount/' . $returnTo . '?minimalInterface=true');
 			}
 		} else {
