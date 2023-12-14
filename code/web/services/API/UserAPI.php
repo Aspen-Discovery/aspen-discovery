021bf14c31e4d348f061289e48b44c906dd077e0
Kirstien Kroeger <kirstien@bywatersolutions.com>
2023-12-14 17:40:53 -0600

    set the SSO session variables again if needed

--- code/web/services/API/UserAPI.php
+++ code/web/services/API/UserAPI.php
@@ -5352,6 +5352,11 @@ class UserAPI extends Action {
 					$session->init();
 					$_SESSION['activeUserId'] = $user->id;
 
+					if($user->isLoggedInViaSSO) {
+						$_SESSION['rememberMe'] = false;
+						$_SESSION['loggedInViaSSO'] = true;
+					}
+
 					return ['success' => true];
 				} else {
 					UserAccount::softLogout();
