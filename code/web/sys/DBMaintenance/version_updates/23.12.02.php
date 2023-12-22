930ee8694290fbc85bfaa81557c496ba0c93bf4a
Mark Noble <mdnoble73@gmail.com>
2023-12-21 22:17:10 -0700

    Remove resetting last index time

--- code/web/sys/DBMaintenance/version_updates/23.12.02.php
+++ code/web/sys/DBMaintenance/version_updates/23.12.02.php
@@ -12,16 +12,16 @@ function getUpdates23_12_02(): array {
             ]
 		], //name*/
 
-		'reset_last_index_time_23_12_02' => [
-			'title'=> 'Reset Last Index Time 23.12.02',
-			'description' => 'Reset Last Index Time 23.12.02',
-			'sql' => [
-				'UPDATE indexing_profiles SET lastUpdateOfChangedRecords = 1703034000',
-				'UPDATE overdrive_settings SET lastUpdateOfChangedRecords = 1703034000',
-				'UPDATE hoopla_settings SET lastUpdateOfChangedRecords = 1703034000',
-				'UPDATE axis360_settings SET lastUpdateOfChangedRecords = 1703034000',
-				'UPDATE cloud_library_settings SET lastUpdateOfChangedRecords = 1703034000',
-			]
-		], //reset_last_index_time_23_12_02
+//		'reset_last_index_time_23_12_02' => [
+//			'title'=> 'Reset Last Index Time 23.12.02',
+//			'description' => 'Reset Last Index Time 23.12.02',
+//			'sql' => [
+//				'UPDATE indexing_profiles SET lastUpdateOfChangedRecords = 1703034000',
+//				'UPDATE overdrive_settings SET lastUpdateOfChangedRecords = 1703034000',
+//				'UPDATE hoopla_settings SET lastUpdateOfChangedRecords = 1703034000',
+//				'UPDATE axis360_settings SET lastUpdateOfChangedRecords = 1703034000',
+//				'UPDATE cloud_library_settings SET lastUpdateOfChangedRecords = 1703034000',
+//			]
+//		], //reset_last_index_time_23_12_02
     ];
 }
