24326793ed39fa009b8e22302abc3b1b4a482182
Kirstien Kroeger <kirstien@bywatersolutions.com>
2023-12-22 12:05:03 -0600

    add shouldAskBrightness

--- code/web/sys/DBMaintenance/version_updates/24.01.00.php
+++ code/web/sys/DBMaintenance/version_updates/24.01.00.php
@@ -60,6 +60,14 @@ function getUpdates24_01_00(): array {
 				'ALTER TABLE user MODIFY COLUMN onboardAppNotifications TINYINT(1) DEFAULT 1 NOT NULL'
 			]
 		], //update_user_notification_onboard
+		'add_user_brightness_permission' => [
+			'title' => 'Add column to determine if we should prompt the user for screen brightness permissions or not',
+			'description' => 'Add column to determine if we should prompt the user for screen brightness permissions or not',
+			'continueOnError' => false,
+			'sql' => [
+				'ALTER TABLE user ADD COLUMN shouldAskBrightness TINYINT(1) DEFAULT 1 NOT NULL'
+			]
+		], //add_user_brightness_permission
 
 		//kodi - ByWater
 		'add_ecommerce_stripe_settings' => [
