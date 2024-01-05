c723629dc94d2932977b7a30a8b2a7f345cfff12
Mark Noble <mdnoble73@gmail.com>
2024-01-05 09:34:36 -0700

    Updates for bulk export of translations

--- code/web/sys/Translation/TranslationTerm.php
+++ code/web/sys/Translation/TranslationTerm.php
@@ -50,4 +50,137 @@ class TranslationTerm extends DataObject {
 		}
 		return $defaultText;
 	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getId() {
+		return $this->id;
+	}
+
+	/**
+	 * @param mixed $id
+	 */
+	public function setId($id): void {
+		$this->id = $id;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getTerm() {
+		return $this->term;
+	}
+
+	/**
+	 * @param mixed $term
+	 */
+	public function setTerm($term): void {
+		$this->term = $term;
+	}
+
+	/**
+	 * @param mixed $defaultText
+	 */
+	public function setDefaultText($defaultText): void {
+		$this->defaultText = $defaultText;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getParameterNotes() {
+		return $this->parameterNotes;
+	}
+
+	/**
+	 * @param mixed $parameterNotes
+	 */
+	public function setParameterNotes($parameterNotes): void {
+		$this->parameterNotes = $parameterNotes;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getSamplePageUrl() {
+		return $this->samplePageUrl;
+	}
+
+	/**
+	 * @param mixed $samplePageUrl
+	 */
+	public function setSamplePageUrl($samplePageUrl): void {
+		$this->samplePageUrl = $samplePageUrl;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getIsPublicFacing() {
+		return $this->isPublicFacing;
+	}
+
+	/**
+	 * @param mixed $isPublicFacing
+	 */
+	public function setIsPublicFacing($isPublicFacing): void {
+		$this->isPublicFacing = $isPublicFacing;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getIsAdminFacing() {
+		return $this->isAdminFacing;
+	}
+
+	/**
+	 * @param mixed $isAdminFacing
+	 */
+	public function setIsAdminFacing($isAdminFacing): void {
+		$this->isAdminFacing = $isAdminFacing;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getIsMetadata() {
+		return $this->isMetadata;
+	}
+
+	/**
+	 * @param mixed $isMetadata
+	 */
+	public function setIsMetadata($isMetadata): void {
+		$this->isMetadata = $isMetadata;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getIsAdminEnteredData() {
+		return $this->isAdminEnteredData;
+	}
+
+	/**
+	 * @param mixed $isAdminEnteredData
+	 */
+	public function setIsAdminEnteredData($isAdminEnteredData): void {
+		$this->isAdminEnteredData = $isAdminEnteredData;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getLastUpdate() {
+		return $this->lastUpdate;
+	}
+
+	/**
+	 * @param mixed $lastUpdate
+	 */
+	public function setLastUpdate($lastUpdate): void {
+		$this->lastUpdate = $lastUpdate;
+	}
 }
