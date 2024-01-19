71670dd4a5beeaa0bb62f74623e8083105b4f179
Mark Noble <mdnoble73@gmail.com>
2024-01-18 17:17:46 -0700

    Update upgrade_24.02.00.sh

--- install/upgrade_24.02.00.sh
+++ install/upgrade_24.02.00.sh
@@ -10,4 +10,4 @@ yum install -y java-11-openjdk
 update-alternatives --set java /usr/lib/jvm/java-11-openjdk-11.0.21.0.9-1.el7_9.x86_64/bin/java
 
 #Update the startup script to call solr 8 rather than solr 7
-sed -i -e "s/7.6.0/8.11.2/" /usr/local/aspen-discovery/sites/$1/$1.sh
+sed -i -e "s/7.6.0/8.11.2/g" /usr/local/aspen-discovery/sites/$1/$1.sh
