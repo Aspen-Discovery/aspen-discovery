da19dfdfbdbb5063a932ea5d3f4198f91d424b99
Kirstien Kroeger <kirstien@bywatersolutions.com>
2024-01-24 15:02:22 -0600

    add new results page for materials request

--- code/web/services/MaterialsRequest/Submit.php
+++ code/web/services/MaterialsRequest/Submit.php
@@ -169,18 +169,7 @@ class MaterialsRequest_Submit extends Action {
 								$materialsRequest->dateUpdated = time();
 
 								if ($materialsRequest->insert()) {
-									$interface->assign('success', true);
-									$interface->assign('materialsRequest', $materialsRequest);
-									// Update Request Counts on success
-									$interface->assign('requestsThisYear', ++$requestsThisYear);
-									$interface->assign('openRequests', ++$openRequests);
-
-									require_once ROOT_DIR . '/sys/MaterialsRequestUsage.php';
-									MaterialsRequestUsage::incrementStat($materialsRequest->status, $homeLibrary->libraryId);
-
-									$materialsRequest->sendStatusChangeEmail();
-									$materialsRequest->sendStaffNewMaterialsRequestEmail();
-									unset($_REQUEST);
+									header('Location: /MaterialsRequest/Results?success=true&id=' . $materialsRequest->id);
 								} else {
 									$interface->assign('success', false);
 									$interface->assign('error', translate([
