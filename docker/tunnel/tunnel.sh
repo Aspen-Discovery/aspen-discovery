adae043ee2e9146232487928fe3c4c2010be608c
Lucas Montoya <lucasmontoya13@mi.unc.edu.ar>
2024-01-30 12:37:17 -0300

    Tunnel directory has been created
    - Dockerfile was included for build purposes
    - Modified script for ssh tunnel

--- docker/tunnel/tunnel.sh
+++ docker/tunnel/tunnel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-chmod -R 400 /root/.ssh/id_rsa*
+chmod -R 400 /home/${TUNNEL_User}/.ssh/id_rsa*
 
-ssh -N -L ${TUNNEL_LocalPort}:${TUNNEL_RemoteHost}:${TUNNEL_RemotePort} ${TUNNEL_JumpServer} & 
+ssh -N -L ${TUNNEL_LocalPort}:${TUNNEL_RemoteHost}:${TUNNEL_RemotePort} ${TUNNEL_JumpServer} &
 #Espera infinita
 /bin/bash -c "trap : TERM INT; sleep infinity & wait"
 
