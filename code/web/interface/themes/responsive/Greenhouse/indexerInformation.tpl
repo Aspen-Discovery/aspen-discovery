fa981d9de70f4c375677edd0286d2c7bb67817de
Mark Noble <mdnoble73@gmail.com>
2024-02-16 16:06:51 -0700

    Correct 404 page after submitting a ticket.
    Also add a page to monitor indexers

---
+++ code/web/interface/themes/responsive/Greenhouse/indexerInformation.tpl
@@ -0,0 +1,59 @@
+{strip}
+	<div class="row">
+		<div class="col-xs-12">
+			<h1 id="pageTitle">{$pageTitleShort}</h1>
+		</div>
+	</div>
+	{if !empty($killResults)}
+		<div class="row">
+			<div class="col-xs-12">
+				<div class="alert alert-info">{$killResults}</div>
+			</div>
+		</div>
+	{/if}
+	{if isset($runningProcesses)}
+		<form action="" method="get" id='indexerInformationForm' class="form-inline">
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="alert alert-info">{translate text="This tool can be used to view information about the indexers running on the system." isAdminFacing=true}</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-xs-1">
+					<strong>{translate text='Select' isAdminFacing=true}</strong>
+				</div>
+				<div class="col-xs-2">
+					<strong>{translate text="PID" isAdminFacing=true}</strong>
+				</div>
+				<div class="col-xs-4">
+					<strong>{translate text="Name" isAdminFacing=true}</strong>
+				</div>
+				<div class="col-xs-3">
+					<strong>{translate text="Start Time" isAdminFacing=true}</strong>
+				</div>
+			</div>
+			{foreach from=$runningProcesses item=process}
+				<div class="row processRow striped">
+					<div class="col-xs-1">
+						<input type="checkbox" class="selectedObject" name="selectedProcesses[{$process.pid}]" aria-label="Select Process {$process.pid}">
+					</div>
+					<div class="col-xs-2">
+						{$process.pid}
+					</div>
+					<div class="col-xs-4">
+						{$process.name}
+					</div>
+					<div class="col-xs-3">
+						{$process.startTime}
+					</div>
+				</div>
+			{/foreach}
+			<div class="btn-group btn-group-sm">
+				<button name="stopProcesses" type='submit' value='stopProcess' class="btn btn-sm btn-danger">{translate text='Stop Selected Processes' isAdminFacing=true}</button>
+			</div>
+		</form>
+		<br/>
+	{else}
+		<p>{translate text="There are no running indexers" isAdminFacing=true}</p>
+	{/if}
+{/strip}
---
+++ code/web/services/Greenhouse/IndexerInformation.php
@@ -0,0 +1,107 @@
+<?php
+require_once ROOT_DIR . '/services/Admin/Admin.php';
+
+class IndexerInformation extends Admin_Admin{
+	function launch() {
+		global $interface;
+
+		$runningProcesses = $this->loadRunningProcesses();
+		$killResults = '';
+		if (!empty($_REQUEST['selectedProcesses'])) {
+			$processesToStop = $_REQUEST['selectedProcesses'];
+			global $configArray;
+			if ($configArray['System']['operatingSystem'] != 'windows') {
+				foreach ($processesToStop as $processId => $value){
+					if (in_array($processId, $runningProcesses)) {
+						exec("kill $processId", $stopResults);
+						$killResults .= $stopResults;
+					}else{
+						exec("kill $processId", $stopResults);
+					}
+				}
+			}else{
+				$killResults = 'Unable to stop processes on Windows';
+			}
+			$interface->assign('killResults', $killResults);
+
+		}
+
+		$interface->assign('runningProcesses', $runningProcesses);
+
+		$this->display('indexerInformation.tpl', 'Indexer Information', false);
+	}
+
+	function loadRunningProcesses() {
+		global $configArray;
+		global $serverName;
+		$runningProcesses = [];
+		if ($configArray['System']['operatingSystem'] == 'windows') {
+			/** @noinspection SpellCheckingInspection */
+			exec("WMIC PROCESS get Processid,Commandline", $processes);
+			$processRegEx = '/.*?java(?:.exe\")?\s+-jar\s(.*?)\.jar.*?\s+(\d+)/ix';
+			$processIdIndex = 2;
+			$processNameIndex = 1;
+			$processStartIndex = -1;
+			$solrRegex = "/$serverName\\\\solr7/ix";
+		} else {
+			exec("ps -ef | grep java", $processes);
+			$processRegEx = '/(\d+)\s+.*?([a-zA-Z0-9:]{5}).*?(\d{2}:\d{2}:\d{2})\sjava\s-jar\s(.*?)\.jar\s' . $serverName . '/ix';
+			$processIdIndex = 1;
+			$processNameIndex = 3;
+			$processStartIndex = 2;
+			$solrRegex = "/(\d+)\s+.*?([a-zA-Z0-9:]{5}).*?(\d{2}:\d{2}:\d{2})\s.*?$serverName\/solr7/ix";
+		}
+
+		$results = "";
+
+		$solrRunning = false;
+		foreach ($processes as $processInfo) {
+			if (preg_match($processRegEx, $processInfo, $matches)) {
+				$processId = $matches[$processIdIndex];
+				if ($processStartIndex > 0) {
+					$startDayTime = $matches[$processStartIndex];
+				}else{
+					$startDayTime = translate(['text'=>'Not Available', 'isPublicFacing'=>true]);
+				}
+
+				$process = $matches[$processNameIndex];
+				if (array_key_exists($process, $runningProcesses)) {
+					$results .= "There is more than one process for $process PID: {$runningProcesses[$process]['pid']} and $processId\r\n";
+				} else {
+					$runningProcesses[$processId] = [
+						'name' => $process,
+						'pid' => $processId,
+						'startTime' => $startDayTime,
+					];
+				}
+
+				//echo("Process: $process ($processId)\r\n");
+			} elseif (preg_match($solrRegex, $processInfo)) {
+				$solrRunning = true;
+			}
+		}
+
+		return $runningProcesses;
+	}
+
+	function getActiveAdminSection(): string {
+		return 'greenhouse';
+	}
+
+	function canView(): bool {
+		if (UserAccount::isLoggedIn()) {
+			if (UserAccount::getActiveUserObj()->isAspenAdminUser()) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function getBreadcrumbs(): array {
+		$breadcrumbs = [];
+		$breadcrumbs[] = new Breadcrumb('/Greenhouse/Home', 'Greenhouse Home');
+		$breadcrumbs[] = new Breadcrumb('', 'Indexer Information');
+
+		return $breadcrumbs;
+	}
+}
