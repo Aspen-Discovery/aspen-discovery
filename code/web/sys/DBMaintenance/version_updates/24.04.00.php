1dc4c1984f02ab36ba2cb3f3dc811c3da84a7aba
Mark Noble <mdnoble73@gmail.com>
2024-03-04 14:33:55 -0700

    Create default files

---
+++ code/web/sys/DBMaintenance/version_updates/24.04.00.php
@@ -0,0 +1,31 @@
+<?php
+
+function getUpdates24_04_00(): array {
+	$curTime = time();
+	return [
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+
+		//mark - ByWater
+
+		//kirstien - ByWater
+
+		//kodi - ByWater
+
+		//lucas - Theke
+
+		//alexander - PTFS Europe
+
+		//jacob - PTFS Europe
+
+		// James Staub
+
+
+	];
+}
