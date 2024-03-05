3b213a02fcfb237223696b957bc4c46bc6c45828
Kirstien Kroeger <kirstien@bywatersolutions.com>
2024-03-05 10:10:02 -0600

    Update LoginForm.js

--- code/aspen_app/src/screens/Auth/LoginForm.js
+++ code/aspen_app/src/screens/Auth/LoginForm.js
@@ -85,8 +85,8 @@ export const GetLoginForm = (props) => {
                               } else {
                                    getTermFromDictionary('en', 'catalog_offline_message');
                               }
+                              return;
                          }
-                         return;
                     }
                }
 
