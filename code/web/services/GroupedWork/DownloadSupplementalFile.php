2e22ebcc91031ff32edbc83fcfff52cf179da047
Mark Noble <mdnoble73@gmail.com>
2024-03-21 15:48:51 -0600

    Track User ILS Usage based on the instance of the active site

--- code/web/services/GroupedWork/DownloadSupplementalFile.php
+++ code/web/services/GroupedWork/DownloadSupplementalFile.php
@@ -32,6 +32,8 @@ class GroupedWork_DownloadSupplementalFile {
 							if (UserAccount::isLoggedIn()) {
 								require_once ROOT_DIR . '/sys/ILS/UserILSUsage.php';
 								$userUsage = new UserILSUsage();
+								global $aspenUsage;
+								$userUsage->instance = $aspenUsage->getInstance();
 								$userUsage->userId = UserAccount::getActiveUserId();
 								$userUsage->indexingProfileId = $this->recordDriver->getIndexingProfile()->id;
 								$userUsage->year = date('Y');
