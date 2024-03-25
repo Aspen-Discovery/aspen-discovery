ff1086096b1b7c348f3e2de3ffa5bd787d7cf71b
Mark Noble <mdnoble73@gmail.com>
2024-03-25 17:06:02 -0600

    Update UserAPITests.php

--- tests/phpunit/tests/services/API/UserAPITests.php
+++ tests/phpunit/tests/services/API/UserAPITests.php
@@ -3,7 +3,41 @@
 namespace services\API;
 
 use PHPUnit\Framework\TestCase;
+use UserAPI;
 
 class UserAPITests extends TestCase {
+	public function testIsLoggedIn() {
+		require_once __DIR__ . '/../../../../../code/web/services/API/UserAPI.php';
+		$userAPI = new UserAPI();
 
+		$isLoggedIn = $userAPI->isLoggedIn();
+		$this->assertFalse($isLoggedIn);
+	}
+
+	public function testLoginFailure() {
+		require_once __DIR__ . '/../../../../../code/web/services/API/UserAPI.php';
+		$userAPI = new UserAPI();
+
+		$result = $userAPI->login();
+		$this->assertFalse($result['success']);
+		$this->assertEquals('This method must be called via POST.', $result['message']);
+
+		$isLoggedIn = $userAPI->isLoggedIn();
+		$this->assertFalse($isLoggedIn);
+	}
+
+	public function testLogin() {
+		require_once __DIR__ . '/../../../../../code/web/services/API/UserAPI.php';
+		$userAPI = new UserAPI();
+
+		$_POST['username'] = 'test_user';
+		$_POST['username'] = 'password';
+		$result = $userAPI->login();
+		$this->assertTrue($result['success']);
+		$this->assertEquals('Test User', $result['name']);
+		$this->assertNotNull($result['session']);
+
+		$isLoggedIn = $userAPI->isLoggedIn();
+		$this->assertTrue($isLoggedIn);
+	}
 }
