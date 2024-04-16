b659e4ae567f24b38ad7d3305528c99bc1f3f9a4
Jacob O'Mara <jacob.omara@ptfs-europe.com>
2024-04-16 15:15:58 +0100

    fix: ensure summon author names point to correct author
    Previously on records written by more than one author, the record could sometimes display the sub-author in favour of the primary author
    this patch changes the getAuthor function in summon record driver to another field that is always populated with the primary author.

--- code/web/RecordDrivers/SummonRecordDriver.php
+++ code/web/RecordDrivers/SummonRecordDriver.php
@@ -342,8 +342,8 @@ class SummonRecordDriver extends RecordInterface {
 	}
 
 	public function getAuthor() {
-		if(isset($this->record['Author'][0])) {
-			$author=$this->record['Author'][0];
+		if(isset($this->record['Author_xml'][0]['fullname'])) {
+			$author=$this->record['Author_xml'][0]['fullname'];
 		} else {
 			$author='Unknown Title';
 		}
