2c28f0c9ffb3e982d0bfb8dd6b83fbfec5dddbaf
Alexander Blanchard <alexander.blanchard@ptfs-europe.com>
2024-05-14 09:43:44 +0100

    chore: updating to 24.06.00
    This commit moves database changes to 24.06.00 and updates the correct MD file.

--- code/web/sys/DBMaintenance/version_updates/24.06.00.php
+++ code/web/sys/DBMaintenance/version_updates/24.06.00.php
@@ -81,6 +81,23 @@ function getUpdates24_06_00(): array {
 
 		//other
 
+		//alexander - PTFS Europe
+		'summon_ip_addresses' => [
+			'title' => 'Summon IP address configuration',
+			'description' => 'Allow configuration of which IP addresses should automatically authenticate with Summon',
+			'continueOnError' => true,
+			'sql' => [
+				'ALTER TABLE ip_lookup ADD COLUMN authenticatedForSummon TINYINT DEFAULT 0',
+			]
+		], //summon authentication
+		'explore_more_section_control' => [
+			'title' => 'Explore More Section Control',
+			'description' => 'Allow control over whether the Explore More Section is displayed',
+			'sql' => [
+				"ALTER TABLE layout_settings ADD COLUMN showExploreMoreOptions TINYINT DEFAULT '1'",
+			]
+		],//control_whether_the_explore_more_box_is_displayed
+
 
 	];
 }
