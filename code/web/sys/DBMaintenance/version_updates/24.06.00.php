17ba1d4f6c893b83f4b9004ce9626bdb00eb05f1
Alexander Blanchard <alexander.blanchard@ptfs-europe.com>
2024-05-30 11:53:22 +0100

    refactor: remove unrealted code

--- code/web/sys/DBMaintenance/version_updates/24.06.00.php
+++ code/web/sys/DBMaintenance/version_updates/24.06.00.php
@@ -82,14 +82,6 @@ function getUpdates24_06_00(): array {
 		//other
 
 		//alexander - PTFS Europe
-		'summon_ip_addresses' => [
-			'title' => 'Summon IP address configuration',
-			'description' => 'Allow configuration of which IP addresses should automatically authenticate with Summon',
-			'continueOnError' => true,
-			'sql' => [
-				'ALTER TABLE ip_lookup ADD COLUMN authenticatedForSummon TINYINT DEFAULT 0',
-			]
-		], //summon authentication
 		'explore_more_section_control' => [
 			'title' => 'Explore More Section Control',
 			'description' => 'Allow control over whether the Explore More Section is displayed',
