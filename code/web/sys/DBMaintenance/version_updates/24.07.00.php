f8223de44c50627fdb92a26e386a18d04891214c
Mark Noble <mdnoble73@gmail.com>
2024-06-05 16:31:20 -0600

    Create new branch

---
+++ code/web/sys/DBMaintenance/version_updates/24.07.00.php
@@ -0,0 +1,30 @@
+<?php
+
+function getUpdates24_07_00(): array {
+	$curTime = time();
+	return [
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+
+		//mark - ByWater
+
+
+		//kirstien - ByWater
+
+
+		//kodi - ByWater
+
+		//katherine - ByWater
+
+		//other
+
+
+
+	];
+}
