48e6331bee22c394a5fc7e8055cd645afd2f7fc9
Lucas Montoya <lucasmontoya13@mi.unc.edu.ar>
2024-06-10 12:57:12 -0300

    Tunnel service : - The names of the environment variables have been changed to more suitable names. - The tunnel service has been removed from the docker-compose.yml and it has been added as an isolated service in case users wants to use it - Little changes on tunnel.sh

--- docker/tunnel/tunnel.sh
+++ docker/tunnel/tunnel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-chmod -R 400 /home/${TUNNEL_User}/.ssh/id_rsa*
+chmod -R 400 /home/${TUNNEL_USER}/.ssh/id_rsa*
 
-ssh -o StrictHostKeyChecking=no -i /home/${TUNNEL_User}/.ssh/id_rsa -N -g -L 0.0.0.0:${TUNNEL_LocalPort}:${TUNNEL_RemoteHost}:${TUNNEL_RemotePort} ${TUNNEL_JumpServer} &
+ssh -o StrictHostKeyChecking=no -i /home/${TUNNEL_USER}/.ssh/id_rsa -N -g -L 0.0.0.0:${TUNNEL_LOCAL_PORT}:${TUNNEL_REMOTE_HOST}:${TUNNEL_REMOTE_PORT} ${TUNNEL_JUMP_SERVER} &
 #Espera infinita
 /bin/bash -c "trap : TERM INT; sleep infinity & wait"
 
