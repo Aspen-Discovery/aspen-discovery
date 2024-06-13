a18ca279f2af28813fa7635c28cfcfbb27298863
Kirstien Kroeger <kirstien@bywatersolutions.com>
2024-06-13 12:28:33 -0500

    disable gesture navigation

--- code/aspen_app/src/navigations/stack/AccountStackNavigator.js
+++ code/aspen_app/src/navigations/stack/AccountStackNavigator.js
@@ -34,6 +34,7 @@ const AccountStackNavigator = () => {
                screenOptions={{
                     headerShown: true,
                     headerBackTitleVisible: false,
+                    gestureEnabled: false,
                     headerBackImage: () => <BackIcon />,
                }}>
                <Stack.Group>
