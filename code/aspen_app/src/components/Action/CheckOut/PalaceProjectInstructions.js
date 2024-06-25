b398cb1842152d5fa4d7075c24fe04e0c8bd0b05
Kirstien Kroeger <kirstien@bywatersolutions.com>
2024-06-25 09:23:12 -0500

    Update PalaceProjectInstructions.js

--- code/aspen_app/src/components/Action/CheckOut/PalaceProjectInstructions.js
+++ code/aspen_app/src/components/Action/CheckOut/PalaceProjectInstructions.js
@@ -29,7 +29,7 @@ export const PalaceProjectInstructions = () => {
           },
           a: {
                color: textColor,
-               text-decoration-color: textColor
+               textDecorationColor: textColor,
           },
      };
 
