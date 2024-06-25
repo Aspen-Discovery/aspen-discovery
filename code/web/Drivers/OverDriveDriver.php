ca070d8630be1ecca2f086e77f96910689d15247
Katherine Perdue <katherine.perdue@bywatersolutions.com>
2024-06-25 14:37:47 -0400

    Fixed tabs

--- code/web/Drivers/OverDriveDriver.php
+++ code/web/Drivers/OverDriveDriver.php
@@ -1295,14 +1295,14 @@ class OverDriveDriver extends AbstractEContentDriver {
 				'isPublicFacing' => true,
 			]);
 			if (isset($response->message)) {
-                if ($response->message == "An unmapped error has occurred. '7'") {
-                    $cancelHoldResult['message'] .= "  " . translate([
-                        'text' => "An unmapped error has occurred. '7'",
-                        'isPublicFacing' => true,
-                    ]);
-                } else {
-                    $cancelHoldResult['message'] .= "  {$response->message}";
-                }
+				if ($response->message == "An unmapped error has occurred. '7'") {
+					$cancelHoldResult['message'] .= "  " . translate([
+						'text' => "An unmapped error has occurred. '7'",
+						'isPublicFacing' => true,
+					]);
+				} else {
+						$cancelHoldResult['message'] .= "  {$response->message}";
+				}
 			}
 
 			// Result for API or app use
@@ -1315,14 +1315,14 @@ class OverDriveDriver extends AbstractEContentDriver {
 				'isPublicFacing' => true,
 			]);
 			if (isset($response->message)) {
-                if ($response->message == "An unmapped error has occurred. '7'") {
-                    $cancelHoldResult['api']['message'] .= "  " . translate([
-                        'text' => "An unmapped error has occurred. '7'",
-                        'isPublicFacing' => true,
-                    ]);
-                } else {
-                    $cancelHoldResult['api']['message'] .= "  {$response->message}";
-                }
+				if ($response->message == "An unmapped error has occurred. '7'") {
+					$cancelHoldResult['api']['message'] .= "  " . translate([
+						'text' => "An unmapped error has occurred. '7'",
+						'isPublicFacing' => true,
+					]);
+				} else {
+					$cancelHoldResult['api']['message'] .= "  {$response->message}";
+				}
 			}
 
 			$this->incrementStat('numApiErrors');
