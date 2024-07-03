3d3f41aa9d8ef6714242443cb7a524e2af0d367d
Lucas Montoya <lucasmontoya13@mi.unc.edu.ar>
2024-07-03 14:12:23 -0300

    Create new subdirectories
    - Move files into new subdirectories for a better organization of the root directory

--- docker/tunnel/tunnel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-chmod -R 400 /home/${TUNNEL_User}/.ssh/id_rsa*
-
-ssh -o StrictHostKeyChecking=no -i /home/${TUNNEL_User}/.ssh/id_rsa -N -g -L 0.0.0.0:${TUNNEL_LocalPort}:${TUNNEL_RemoteHost}:${TUNNEL_RemotePort} ${TUNNEL_JumpServer} &
-#Espera infinita
-/bin/bash -c "trap : TERM INT; sleep infinity & wait"
-
