a3c109f88d6a4be50114a4f382daa5340a39721c
Kirstien Kroeger <kirstien@bywatersolutions.com>
2024-07-17 15:18:20 -0500

    store message queue from koha

--- code/web/sys/DBMaintenance/version_updates/24.08.00.php
+++ code/web/sys/DBMaintenance/version_updates/24.08.00.php
@@ -15,6 +15,37 @@ function getUpdates24_08_00(): array {
 		//mark - ByWater
 
 		//kirstien - ByWater
+		'add_ils_notification_setting' => [
+			'title' => 'Add table ils_notification_setting',
+			'description' => '',
+			'continueOnError' => false,
+			'sql' => [
+				'CREATE TABLE IF NOT EXISTS ils_notification_setting (
+					id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+					messageId VARCHAR(100) NOT NULL,
+					type VARCHAR(255),
+					status VARCHAR(255)
+				) ENGINE INNODB',
+			]
+		], //add_ils_notification_settings
+
+		'add_user_ils_messages' => [
+			 'title' => 'Add table user_ils_messages',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 "CREATE TABLE IF NOT EXISTS user_ils_messages (
+					id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+					messageId VARCHAR(100) NOT NULL,
+					userId INT(11),
+					type VARCHAR(50),
+					status enum('pending', 'sent', 'failed') DEFAULT 'pending',
+					error VARCHAR(255),
+					dateQueued TIMESTAMP,
+					dateSent INT(11)
+				) ENGINE INNODB",
+			 ]
+		 ], //add_user_ils_messages
 
 		//kodi - ByWater
 
