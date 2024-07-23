2235b0be9df8e63603054dc243b1e720b92e110d
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-07-23 11:07:07 +0100

    feat: add a launch function
    Adds a launch method to the ILSUsageGraph class

--- code/web/services/ILS/UsageGraphs.php
+++ code/web/services/ILS/UsageGraphs.php
@@ -6,4 +6,26 @@ require_once ROOT_DIR . '/sys/ILS/UserILSUsage.php';
 require_once ROOT_DIR . '/sys/ILS/ILSRecordUsage.php';
 
 class ILS_UsageGraphs extends Admin_Admin {
+	function launch() {
+		global $interface;
+		global $enabledModules;
+		global $library;
+		$title = 'ILS Usage Graph';
+		$stat = $_REQUEST['stat'];
+		if (!empty($_REQUEST['instance'])) {
+			$instanceName = $_REQUEST['instance'];
+		} else {
+			$instanceName = '';
+		}
+
+		$dataSeries = [];
+		$columnLabels = [];
+		$interface->assign('columnLabels', $columnLabels);
+		$interface->assign('dataSeries', $dataSeries);
+		$interface->assign('translateDataSeries', true);
+		$interface->assign('translateColumnLabels', false);
+
+		$interface->assign('graphTitle', $title);
+		$this->display('usage-graph.tpl', $title);
+	}
 }
