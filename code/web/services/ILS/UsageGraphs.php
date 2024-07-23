a5b2ccaef7c42674c4be1c9b8af485a705ef9f58
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-07-23 10:59:28 +0100

    feat: ILS_UsageGraphs class
    Adds a new UsageGraphs.php file in the services/ILS folder, and creates
    the ILS_UsageGraphs class within it.

---
+++ code/web/services/ILS/UsageGraphs.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once ROOT_DIR . '/services/Admin/Admin.php';
+require_once ROOT_DIR . '/sys/SystemLogging/AspenUsage.php';
+require_once ROOT_DIR . '/sys/ILS/UserILSUsage.php';
+require_once ROOT_DIR . '/sys/ILS/ILSRecordUsage.php';
+
+class ILS_UsageGraphs extends Admin_Admin {
+}
