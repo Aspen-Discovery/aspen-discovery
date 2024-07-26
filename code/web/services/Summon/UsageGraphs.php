5fec5c91fb222dda2b0b210bbf4debe5dddaaeda
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-07-26 17:59:32 +0100

    feat: keep summon section active

--- code/web/services/Summon/UsageGraphs.php
+++ code/web/services/Summon/UsageGraphs.php
@@ -6,6 +6,9 @@ require_once ROOT_DIR . '/sys/Summon/UserSummonUsage.php';
 require_once ROOT_DIR . '/sys/Summon/SummonRecordUsage.php';
 
 class Summon_UsageGraphs extends Admin_Admin {
+	function getActiveAdminSection(): string {
+		return 'summon';
+	}
 	function canView(): bool {
 		return UserAccount::userHasPermission([
 			'View Dashboards',
