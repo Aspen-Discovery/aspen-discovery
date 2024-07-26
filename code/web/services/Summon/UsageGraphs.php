cc17fc0edb84b660aeb33c3ade1c64b01d5b777e
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-07-26 18:01:37 +0100

    feat: get breadcrumbs

--- code/web/services/Summon/UsageGraphs.php
+++ code/web/services/Summon/UsageGraphs.php
@@ -6,13 +6,24 @@ require_once ROOT_DIR . '/sys/Summon/UserSummonUsage.php';
 require_once ROOT_DIR . '/sys/Summon/SummonRecordUsage.php';
 
 class Summon_UsageGraphs extends Admin_Admin {
+
 	function getActiveAdminSection(): string {
 		return 'summon';
 	}
+
 	function canView(): bool {
 		return UserAccount::userHasPermission([
 			'View Dashboards',
 			'View System Reports',
 		]);
+	}
+
+	function getBreadcrumbs(): array {
+		$breadcrumbs = [];
+		$breadcrumbs[] = new Breadcrumb('/Admin/Home', 'Administration Home');
+		$breadcrumbs[] = new Breadcrumb('/Admin/Home#summon', 'Summon');
+		$breadcrumbs[] = new Breadcrumb('/Summon/SummonDashboard', 'Summon Usage Dashboard');
+		$breadcrumbs[] = new Breadcrumb('', 'Usage Graph');
+		return $breadcrumbs;
 	}
 }
