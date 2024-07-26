84319b00900abb292381353c404e1799a93cc65e
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-07-26 17:50:45 +0100

    feat: Summon_UsageGraphs class

---
+++ code/web/services/Summon/UsageGraphs.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once ROOT_DIR . '/services/Admin/Admin.php';
+require_once ROOT_DIR . '/sys/SystemLogging/AspenUsage.php';
+require_once ROOT_DIR . '/sys/Summon/UserSummonUsage.php';
+require_once ROOT_DIR . '/sys/Summon/SummonRecordUsage.php';
+
+class Summon_UsageGraphs extends Admin_Admin {}
