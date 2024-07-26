4a6d03cd0911e4d2e95babd3f411d26634ff1002
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-07-26 17:53:11 +0100

    feat: check user access permission

--- code/web/services/Summon/UsageGraphs.php
+++ code/web/services/Summon/UsageGraphs.php
@@ -5,4 +5,11 @@ require_once ROOT_DIR . '/sys/SystemLogging/AspenUsage.php';
 require_once ROOT_DIR . '/sys/Summon/UserSummonUsage.php';
 require_once ROOT_DIR . '/sys/Summon/SummonRecordUsage.php';
 
-class Summon_UsageGraphs extends Admin_Admin {}
+class Summon_UsageGraphs extends Admin_Admin {
+	function canView(): bool {
+		return UserAccount::userHasPermission([
+			'View Dashboards',
+			'View System Reports',
+		]);
+	}
+}
