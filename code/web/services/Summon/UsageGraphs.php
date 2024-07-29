e39e49501b5003a5e56770ffcbe3e81d4c0d0f98
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-07-29 12:34:22 +0100

    feat: fetch and render summon usage record data
    This uses the same SQL queries as in the summon dashboard in order to
    fetch the relevant data for each graph and raw data table from the
    summon_usage table in the aspen db, so that they are then rendered on
    the matching usage graph page (implementing the logic from the admin
    usage graphs).

--- code/web/services/Summon/UsageGraphs.php
+++ code/web/services/Summon/UsageGraphs.php
@@ -12,6 +12,14 @@ class Summon_UsageGraphs extends Admin_Admin {
 
 		$title = 'Summon Usage Graph';
 		$stat = $_REQUEST['stat'];
+		if (!empty($_REQUEST['instance'])) {
+			$instanceName = $_REQUEST['instance'];
+		} else {
+			$instanceName = '';
+		}
+
+		$dataSeries = [];
+		$columnLabels = [];
 
 		switch ($stat) {
 			case 'activeUsers':
@@ -27,6 +35,69 @@ class Summon_UsageGraphs extends Admin_Admin {
 				$title .= ' - Total Clicks';
 			break;
 		}
+		if (
+			$stat == 'numRecordsViewed' ||
+			$stat == 'numRecordsClicked' ||
+			$stat == 'totalClicks'
+			) {
+				$summonRecordUsage = new SummonRecordUsage();
+				$summonRecordUsage->groupBy('year, month');
+				if (!empty($instanceName)) {
+					$summonRecordUsage->instance = $instanceName;
+				}
+				$summonRecordUsage->selectAdd();
+				$summonRecordUsage->selectAdd('year');
+				$summonRecordUsage->selectAdd('month');
+				$summonRecordUsage->orderBy('year, month');
+				if ($stat == 'numRecordsViewed') {
+					$dataSeries['Number of Records Viewed'] = [
+						'borderColor' => 'rgba(255, 99, 132, 1)',
+						'backgroundColor' => 'rgba(255, 99, 132, 0.2)',
+						'data' => [],
+					];
+					$summonRecordUsage ->selectAdd('SUM(IF(timesViewedInSearch>0,1,0)) as numRecordsViewed');
+				}
+				if ($stat == 'numRecordsClicked') {
+					$dataSeries['Number of Records Clicked'] = [
+						'borderColor' => 'rgba(255, 99, 132, 1)',
+						'backgroundColor' => 'rgba(255, 99, 132, 0.2)',
+						'data' => [],
+					];
+					$summonRecordUsage ->selectAdd('SUM(IF(timesUsed>0,1,0)) as numRecordsUsed');
+				}
+				if ($stat == 'totalClicks') {
+					$dataSeries['Total Clicks'] = [
+						'borderColor' => 'rgba(255, 99, 132, 1)',
+						'backgroundColor' => 'rgba(255, 99, 132, 0.2)',
+						'data' => [],
+					];
+					$summonRecordUsage ->selectAdd('SUM(timesUsed) as numClicks');
+				}
+
+				// Collect results
+				$summonRecordUsage->find();
+				while ($summonRecordUsage->fetch()) {
+					$curPeriod = "{$summonRecordUsage->month}-{$summonRecordUsage->year}";
+					$columnLabels[] = $curPeriod;
+					if ($stat == 'numRecordsViewed') {
+						/** @noinspection PhpUndefinedFieldInspection */
+						$dataSeries['Number of Records Viewed']['data'][$curPeriod] = $summonRecordUsage->numRecordsViewed;
+					}
+					if ($stat == 'numRecordsClicked') {
+						/** @noinspection PhpUndefinedFieldInspection */
+						$dataSeries['Number of Records Clicked']['data'][$curPeriod] = $summonRecordUsage->numRecordsUsed;
+					}
+					if ($stat == 'totalClicks') {
+						/** @noinspection PhpUndefinedFieldInspection */
+						$dataSeries['Total Clicks']['data'][$curPeriod] = $summonRecordUsage->numClicks;
+					}
+
+				}
+			}
+		$interface->assign('columnLabels', $columnLabels);
+		$interface->assign('dataSeries', $dataSeries);
+		$interface->assign('translateDataSeries', true);
+		$interface->assign('translateColumnLabels', false);
 
 		$interface->assign('graphTitle', $title);
 		$this->display('usage-graph.tpl', $title);
