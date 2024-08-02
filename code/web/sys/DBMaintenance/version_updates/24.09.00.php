c1bf629db781811e5eafc442ef732ce6be646735
Alexander Blanchard <alexander.blanchard@ptfs-europe.com>
2024-08-02 18:19:47 +0100

    feat: Update db with new consent and tracking

--- code/web/sys/DBMaintenance/version_updates/24.09.00.php
+++ code/web/sys/DBMaintenance/version_updates/24.09.00.php
@@ -21,6 +21,17 @@ function getUpdates24_09_00(): array {
 		//kodi - ByWater
 
 		//alexander - PTFS-Europe
+		'update_cookie_management_preferences_more_options' => [
+			'title' => 'Update Cookie Management Preferences: More Options',
+			'description' => 'Update cookie management preferences for user tracking - adding more options',
+			'continueOnError' => false,
+			'sql' => [
+				"ALTER TABLE user ADD COLUMN userCookiePreferenceEvents TINYINT(1) DEFAULT 0",
+				"ALTER TABLE user ADD COLUMN userCookiePreferenceOpenArchives TINYINT(1) DEFAULT 0",
+				"ALTER TABLE user ADD COLUMN userCookiePreferenceWebsite TINYINT(1) DEFAULT 0",
+				"ALTER TABLE user ADD COLUMN userCookiePreferenceExternalSearchServices TINYINT(1) DEFAULT 0",
+			],
+		], 
 
 		//chloe - PTFS-Europe
 
