02e8cd12f44951a5f7e266674946bd4058df8aab
Mark Noble <mdnoble73@gmail.com>
2024-08-05 08:41:44 -0600

    Remove old non-numeric Palace Project identifiers

--- code/web/sys/DBMaintenance/version_updates/24.09.00.php
+++ code/web/sys/DBMaintenance/version_updates/24.09.00.php
@@ -18,10 +18,18 @@ function getUpdates24_09_00(): array {
 			'description' => 'Add the ability to enable or disable the Sierra checkout grid by Format',
 			'continueOnError' => false,
 			'sql' => [
+				"INSERT INTO record_identifiers_to_reload (type, identifier) SELECT type, identifier from grouped_work_primary_identifiers where type = 'palace_project' and NOT identifier REGEXP '^[0-9]+$'"
+			]
+		], //show_checkout_grid_by_format
+		'force_reindex_of_old_style_palace_project_identifiers' => [
+			'title' => 'Force Reindex of Old Style Palace Project Identifiers',
+			'description' => 'Force Reindex of Old Style Palace Project Identifiers',
+			'continueOnError' => false,
+			'sql' => [
 				'ALTER TABLE format_map_values ADD COLUMN displaySierraCheckoutGrid TINYINT(1) DEFAULT 0',
 				"UPDATE format_map_values SET displaySierraCheckoutGrid = 1 where format IN ('Journal', 'Newspaper', 'Print Periodical', 'Magazine')"
 			]
-		], //show_checkout_grid_by_format
+		]
 
 		//katherine - ByWater
 
