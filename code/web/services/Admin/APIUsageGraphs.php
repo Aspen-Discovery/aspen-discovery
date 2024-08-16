9735f9779273c3a58d7c000dc103c3e346757b98
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-08-16 12:33:57 +0100

    feat: Admin_APIUsageGraphs class
    launches the usage graph template render

---
+++ code/web/services/Admin/APIUsageGraphs.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once ROOT_DIR . '/services/Admin/Admin.php';
+require_once ROOT_DIR . '/sys/SystemLogging/APIUsage.php';
+
+class Admin_APIUsageGraphs extends Admin_Admin
+{
+	function launch()
+	{
+		global $interface;
+
+		$stat = $_REQUEST['stat'];
+		if (!empty($_REQUEST['instance'])) {
+			$instanceName = $_REQUEST['instance'];
+		} else {
+			$instanceName = '';
+		}
+
+		$title = 'Aspen Discovery API Usage Graph';
+		$this->display('usage-graph.tpl', $title);
+	}
+	
+	function getBreadcrumbs(): array
+	{
+		$breadcrumbs = [];
+		$breadcrumbs[] = new Breadcrumb('/Admin/Home', 'Administration Home');
+		$breadcrumbs[] = new Breadcrumb('/Admin/Home#system_reports', 'System Reports');
+		$breadcrumbs[] = new Breadcrumb('/Admin/APIUsageDashboard', 'Usage Dashboard');
+		$breadcrumbs[] = new Breadcrumb('', 'Usage Graph');
+		return $breadcrumbs;
+	}
+
+	function getActiveAdminSection(): string
+	{
+		return 'system_reports';
+	}
+
+	function canView(): bool
+	{
+		return UserAccount::userHasPermission([
+			'View Dashboards',
+			'View System Reports',
+		]);
+	}
+
+}
