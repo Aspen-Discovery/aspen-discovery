08a049a6c55ef00c5626cea96b100b464df14691
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-08-16 12:36:38 +0100

    feat: get and set data series for APIUsage graphs
    is used both when launching the page and when building CSV files

--- code/web/services/Admin/APIUsageGraphs.php
+++ code/web/services/Admin/APIUsageGraphs.php
@@ -19,6 +19,7 @@ class Admin_APIUsageGraphs extends Admin_Admin
 		$title = 'Aspen Discovery API Usage Graph';
 		$interface->assign('graphTitle', $title);
 		$this->assignGraphSpecificTitle($stat);
+		$this->getAndSetInterfaceDataSeries($stat, $instanceName);
 		$title = $interface->getVariable('graphTitle');
 		$this->display('usage-graph.tpl', $title);
 	}
@@ -44,6 +45,44 @@ class Admin_APIUsageGraphs extends Admin_Admin
 			'View Dashboards',
 			'View System Reports',
 		]);
+	}
+
+	private function getAndSetInterfaceDataSeries($stat, $instanceName) {
+		global $interface;
+
+		$dataSeries = [];
+		$columnLabels = [];
+		$usage = new APIUsage();
+		$usage->groupBy('year, month');
+		if (!empty($instanceName)) {
+			$usage->instance = $instanceName;
+		}
+		$usage->selectAdd();
+		$usage->selectAdd('year');
+		$usage->selectAdd('month');
+		$usage->orderBy('year, month');
+
+		// get runPendingDatabaseUpdates stats
+		$dataSeries['runPendingDatabaseUpdates'] = [
+			'borderColor' => 'rgba(255, 99, 132, 1)',
+			'backgroundColor' => 'rgba(255, 99, 132, 0.2)',
+			'data' => [],
+		];
+		$usage->selectAdd('SUM(numCalls) as numCalls');
+
+		//Collect results
+		$usage->find();
+
+		while ($usage->fetch()) {
+			$curPeriod = "{$usage->month}-{$usage->year}";
+			$columnLabels[] = $curPeriod;
+			/** @noinspection PhpUndefinedFieldInspection */
+			$dataSeries['runPendingDatabaseUpdates']['data'][$curPeriod] = $usage->numCalls;
+		}
+		$interface->assign('columnLabels', $columnLabels);
+		$interface->assign('dataSeries', $dataSeries);
+		$interface->assign('translateDataSeries', true);
+		$interface->assign('translateColumnLabels', false);
 	}
 
 	private function assignGraphSpecificTitle()
