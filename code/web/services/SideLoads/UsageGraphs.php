ccc8e546024f8e5345a06b222c46cc47683a966a
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-08-16 15:53:04 +0100

    feat: render SideLoad data in usage graphs

--- code/web/services/SideLoads/UsageGraphs.php
+++ code/web/services/SideLoads/UsageGraphs.php
@@ -17,6 +17,7 @@ class SideLoads_UsageGraphs extends Admin_Admin {
 
 		$interface->assign('graphTitle', $title);
 		$this->assignGraphSpecificTitle($stat);
+		$this->getAndSetInterfaceDataSeries($stat, $instanceName);
 		$title = $interface->getVariable('graphTitle');
 		$this->display('usage-graph.tpl', $title);
 	}
@@ -39,6 +40,76 @@ class SideLoads_UsageGraphs extends Admin_Admin {
 			'View Dashboards',
 			'View System Reports',
 		]);
+	}
+
+	private function getAndSetInterfaceDataSeries($stat, $instanceName) {
+		global $interface;
+
+		$dataSeries = [];
+		$columnLabels = [];
+		$usage = [];
+
+		// for the graph displaying data retrieved from the user_sideload_usage table
+		if ($stat == 'activeUsers') {
+			$usage = new UserSideLoadUsage();
+			$usage->groupBy('year, month');
+			if (!empty($instanceName)) {
+				$usage->instance = $instanceName;
+			}
+			$usage->selectAdd();
+			$usage->selectAdd('year');
+			$usage->selectAdd('month');
+			$usage->orderBy('year, month');
+
+			$dataSeries['Active Users'] = [
+				'borderColor' => 'rgba(255, 99, 132, 1)',
+				'backgroundColor' => 'rgba(255, 99, 132, 0.2)',
+				'data' => [],
+			];
+			$usage->selectAdd('COUNT(id) as numUsers');
+		}
+
+		// for the graph displaying data retrieved from the sideload_record_usage table
+		if ($stat == 'recordsAccessedOnline' ) {
+			$usage = new SideLoadedRecordUsage();
+			$usage->groupBy('year, month');
+			if (!empty($instanceName)) {
+				$usage->instance = $instanceName;
+			}
+
+			$usage->selectAdd(null);
+			$usage->selectAdd();
+			$usage->selectAdd('year');
+			$usage->selectAdd('month');
+			$usage->orderBy('year, month');
+
+			$dataSeries['Records Accessed Online'] = [
+				'borderColor' => 'rgba(255, 99, 132, 1)',
+				'backgroundColor' => 'rgba(255, 99, 132, 0.2)',
+				'data' => [],
+			];
+			$usage->selectAdd('SUM(IF(timesUsed>0,1,0)) as numRecordsUsed');
+		}
+
+		// collect results
+		$usage->find();
+		while ($usage->fetch()) {
+			$curPeriod = "{$usage->month}-{$usage->year}";
+			$columnLabels[] = $curPeriod;
+			if ($stat == 'activeUsers') {
+				/** @noinspection PhpUndefinedFieldInspection */
+				$dataSeries['Active Users']['data'][$curPeriod] = $usage->numUsers;
+			}
+			if ($stat == 'recordsAccessedOnline') {
+				/** @noinspection PhpUndefinedFieldInspection */
+				$dataSeries['Records Accessed Online']['data'][$curPeriod] = $usage->numRecordsUsed;
+			}
+		}
+
+		$interface->assign('columnLabels', $columnLabels);
+		$interface->assign('dataSeries', $dataSeries);
+		$interface->assign('translateDataSeries', true);
+		$interface->assign('translateColumnLabels', false);
 	}
 
 	private function assignGraphSpecificTitle($stat) {
