469a047d958db10064375e561cdfdd60d6d47bca
Chloé Zermatten <chloe.zermatten@ptfs-europe.com>
2024-08-19 17:24:22 +0100

    feat: assign axis360 graph titles

--- code/web/services/Axis360/UsageGraphs.php
+++ code/web/services/Axis360/UsageGraphs.php
@@ -10,6 +10,7 @@ class Axis360_UsageGraphs extends Admin_Admin {
 	function launch() {
 		$title = 'Boundless Usage Graph';
 		$interface->assign('graphTitle', $title);
+		$this->assignGraphSpecificTitle($stat);
 		$this->display('../Admin/usage-graph.tpl', $title);
 	}
 
@@ -33,4 +34,47 @@ class Axis360_UsageGraphs extends Admin_Admin {
 		]);
 	}
 
+	private function assignGraphSpecificTitle($stat) {
+		global $interface;
+		$title = $interface->getVariable('graphTitle');
+		switch ($stat) {
+			case 'activeUsers':
+				$title .= ' - Active Users';
+				break;
+			case 'recordsWithUsage':
+				$title .= ' - Records With Usage';
+				break;
+			case 'loans':
+				$title .= ' - Total Checkouts';
+				break;
+			case 'holds':
+				$title .= ' - Total Holds';
+				break;
+			case 'renewals':
+				$title .= ' - Total Renewals';
+				break;
+			case 'earlyReturns':
+				$title .= ' - Total Early Returns';
+				break;
+			case 'holdsCancelled':
+				$title .= ' - Total Holds Cancelled';
+				break;
+			case 'holdsFrozen':
+				$title .= ' - Total Holds Frozen';
+				break;
+			case 'holdsThawed':
+				$title .= ' - Total Holds Thawed';
+				break;
+			case 'apiErrors':
+				$title .= ' - Total API Errors';
+				break;
+			case 'connectionFailures':
+				$title .= ' - Total Connection Failures';
+				break;
+			case 'general':
+				$title .= ' - General';
+				break;
+		}
+		$interface->assign('graphTitle', $title);
+	}
 }
