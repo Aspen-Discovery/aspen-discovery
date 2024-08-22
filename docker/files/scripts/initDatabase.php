e30a4396313272a8b469cecc36c440ad0b385512
Lucas Montoya <lucasmontoya13@mi.unc.edu.ar>
2024-08-22 11:43:01 -0300

    Add supportingCompany support in Docker scripts
    - Add and set supporting company field to aspen database in docker script

--- docker/files/scripts/initDatabase.php
+++ docker/files/scripts/initDatabase.php
@@ -1,10 +1,11 @@
 <?php
 
-//Initialize database
+// Initialize database
 
 echo "--> Initializing database...\n";
 
 $aspenAdminPassword = getenv('ASPEN_ADMIN_PASSWORD');
+$supportingCompany = getenv('SUPPORTING_COMPANY') ?? 'ByWater Solutions';
 $databaseHost = getenv('DATABASE_HOST') ?? 'localhost';
 $databasePort = getenv('DATABASE_PORT') ?? 3306;
 $databaseName = getenv('DATABASE_NAME');
@@ -13,25 +14,25 @@ $databasePassword = getenv('DATABASE_PASSWORD');
 $databaseDsn = "mysql:host=$databaseHost;port=$databasePort;dbname=$databaseName";
 
 $mysqlConnectionCommand = "mariadb -u$databaseUser -p$databasePassword -h$databaseHost";
-    $databasePort != "3306" ?? $mysqlConnectionCommand .= " --port=$databasePort";
+	$databasePort != "3306" ?? $mysqlConnectionCommand .= " --port=$databasePort";
 
 //Check if aspen database has already been initialized
 try {
-    $statement = 'SELECT libraryId FROM library LIMIT 1;';
-    $aspenDatabase = new PDO($databaseDsn, $databaseUser, $databasePassword);
-    $updateUserStmt = $aspenDatabase->prepare($statement);
+	$statement = 'SELECT libraryId FROM library LIMIT 1;';
+	$aspenDatabase = new PDO($databaseDsn, $databaseUser, $databasePassword);
+	$updateUserStmt = $aspenDatabase->prepare($statement);
 } catch (PDOException $e) {
-    echo "ERROR MESSAGE : " . $e->getMessage() . "\n";
-    echo "IN : " . $e->getFile() . ":" . $e->getLine() . "\n";
-    die(1);
+	echo "ERROR MESSAGE : " . $e->getMessage() . "\n";
+	echo "IN : " . $e->getFile() . ":" . $e->getLine() . "\n";
+	die(1);
 }
 
 try {
-    $updateUserStmt->execute();
-    echo "--> Aspen database has already been initialized!\n";
-    die(0);
+	$updateUserStmt->execute();
+	echo "--> Aspen database has already been initialized!\n";
+	die(0);
 } catch (PDOException $e) {
-    # Aspen database is empty yet
+	# Aspen database is empty yet
 }
 
 //Load default database
@@ -39,12 +40,17 @@ $aspenDir = '/usr/local/aspen-discovery/';
 echo "--> Loading default database...\n";
 exec("$mysqlConnectionCommand $databaseName < $aspenDir/install/aspen.sql", $output, $errorCode);
 if ($errorCode != 0) {
-    echo " ERROR : Database '{$databaseName}' could not be loaded\n";
-    die(1);
+	echo " ERROR : Database '{$databaseName}' could not be loaded\n";
+	die(1);
 }
 echo "--> Default database has been successfully loaded\n";
 
-//Connect to the database
+// Connect to the database
 $aspenDatabase = new PDO($databaseDsn, $databaseUser, $databasePassword);
 $updateUserStmt = $aspenDatabase->prepare("UPDATE user set cat_password=" . $aspenDatabase->quote($aspenAdminPassword) . ", password=" . $aspenDatabase->quote($aspenAdminPassword) . " where username = 'aspen_admin'");
 $updateUserStmt->execute();
+
+// Assign supportingCompany in the database
+
+$postSupportingCompanyStmt = $aspenDatabase->prepare("UPDATE system_variables set supportingCompany=" . $aspenDatabase->quote($supportingCompany));
+$postSupportingCompanyStmt->execute();
