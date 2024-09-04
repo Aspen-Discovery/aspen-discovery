cb3f50034caaf6a62eab07e93efbe31f628cb14f
Mark Noble <mdnoble73@gmail.com>
2024-09-04 16:15:05 -0600

    Setup new branch for 24.10

---
+++ code/web/sys/DBMaintenance/version_updates/24.10.00.php
@@ -0,0 +1,34 @@
+<?php
+
+function getUpdates24_10_00(): array {
+	$curTime = time();
+	return [
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+
+		//mark - Grove
+
+		//katherine - ByWater
+
+		//kirstien - ByWater
+
+		//kodi - ByWater
+
+		//alexander - PTFS-Europe
+
+		//chloe - PTFS-Europe
+
+		//pedro - PTFS-Europe
+
+		//James Staub - Nashville Public Library
+
+		//other
+
+	];
+}
