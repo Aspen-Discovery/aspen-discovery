7724ce48269bc8541ec07fe96ed52db1edb09e66
Katherine Perdue <katherine.perdue@bywatersolutions.com>
2024-09-09 16:53:13 -0400

    Allow sorting for checkouts
    Add the same sort options for checkouts as Discovery: sort by title, author, due date asc and desc, format, library account, and times renewed.  Resolves ByWater Tickets 106685, 130068, 134795, 136283 which asked for sorting by library account.

--- code/aspen_app/src/screens/MyAccount/CheckedOutTitles/MyCheckouts.js
+++ code/aspen_app/src/screens/MyAccount/CheckedOutTitles/MyCheckouts.js
@@ -2,7 +2,7 @@ import { MaterialIcons } from '@expo/vector-icons';
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import { useIsFetching, useQuery, useQueryClient } from '@tanstack/react-query';
 import _ from 'lodash';
-import { AlertDialog, Box, Button, Center, CheckIcon, FlatList, FormControl, HStack, Icon, ScrollView, Select, Text } from 'native-base';
+import { AlertDialog, Box, Button, Center, CheckIcon, FlatList, FormControl, HStack, Icon, ScrollView, Select, Text, VStack } from 'native-base';
 import React from 'react';
 import { Platform, SafeAreaView } from 'react-native';
 
@@ -15,6 +15,7 @@ import { confirmRenewAllCheckouts, confirmRenewCheckout, renewAllCheckouts } fro
 import { getPatronCheckedOutItems } from '../../../util/api/user';
 import { stripHTML } from '../../../util/apiAuth';
 import { MyCheckout } from './MyCheckout';
+import { ManageSelectedHolds } from '../TitlesOnHold/MyHold';
 
 export const MyCheckouts = () => {
      const isFetchingCheckouts = useIsFetching({ queryKey: ['checkouts'] });
@@ -22,7 +23,7 @@ export const MyCheckouts = () => {
      const navigation = useNavigation();
      const { user, updateUser } = React.useContext(UserContext);
      const { library } = React.useContext(LibrarySystemContext);
-     const { checkouts, updateCheckouts } = React.useContext(CheckoutsContext);
+     const { checkouts, updateCheckouts, sortMethod, updateSortMethod } = React.useContext(CheckoutsContext);
      const { language } = React.useContext(LanguageContext);
      const [isLoading, setLoading] = React.useState(false);
      const [renewAll, setRenewAll] = React.useState(false);
@@ -47,6 +48,16 @@ export const MyCheckouts = () => {
           all: 'Checked Out Titles',
      });
 
+     const [sortBy, setSortBy] = React.useState({
+          title: 'Sort by Title',
+          author: 'Sort by Author',
+          due_asc: 'Sort by Due Date Ascending',
+          due_desc: 'Sort by Due Date Descending',
+          format: 'Sort by Format',
+          library_account: 'Sort by Library Account',
+          times_renewed: 'Sort by Times Renewed',
+     });
+
      React.useLayoutEffect(() => {
           navigation.setOptions({
                headerLeft: () => <Box />,
@@ -60,6 +71,15 @@ export const MyCheckouts = () => {
           },
           onSettle: (data) => setLoading(false),
      });
+
+     const toggleSort = async (value) => {
+          updateSortMethod(value);
+          const sortedCheckouts = sortCheckouts(checkouts, value);
+          setLoading(true);
+          queryClient.setQueryData(['checkouts', library.baseUrl, language, sortMethod, source], sortedCheckouts);
+          setLoading(false);
+          updateCheckouts(sortedCheckouts);
+     };
 
      const toggleSource = async (value) => {
           console.log('toggleSource: ' + value);
@@ -151,6 +171,50 @@ export const MyCheckouts = () => {
                          setCheckoutBy(tmp);
                     }
 
+                    tmp = sortBy;
+
+                    term = getTermFromDictionary(language, 'sort_by_title');
+                    if (!term.includes('%1%')) {
+                         tmp = _.set(tmp, 'title', term);
+                         setSortBy(tmp);
+                    }
+
+                    term = getTermFromDictionary(language, 'sort_by_author');
+                    if (!term.includes('%1%')) {
+                         tmp = _.set(tmp, 'author', term);
+                         setSortBy(tmp);
+                    }
+
+                    term = getTermFromDictionary(language, 'sort_by_due_asc');
+                    if (!term.includes('%1%')) {
+                         tmp = _.set(tmp, 'due_asc', term);
+                         setSortBy(tmp);
+                    }
+
+                    term = getTermFromDictionary(language, 'sort_by_due_desc');
+                    if (!term.includes('%1%')) {
+                         tmp = _.set(tmp, 'due_desc', term);
+                         setSortBy(tmp);
+                    }
+
+                    term = getTermFromDictionary(language, 'sort_by_format');
+                    if (!term.includes('%1%')) {
+                         tmp = _.set(tmp, 'format', term);
+                         setSortBy(tmp);
+                    }
+
+                    term = getTermFromDictionary(language, 'sort_by_library_account');
+                    if (!term.includes('%1%')) {
+                         tmp = _.set(tmp, 'library_account', term);
+                         setSortBy(tmp);
+                    }
+
+                    term = getTermFromDictionary(language, 'sort_by_times_renewed');
+                    if (!term.includes('%1%')) {
+                         tmp = _.set(tmp, 'times_renewed', term);
+                         setSortBy(tmp);
+                    }
+
                     setLoading(false);
                };
                update().then(() => {
@@ -205,79 +269,122 @@ export const MyCheckouts = () => {
 
           let holdSourceLength = 8 * holdSource.length + 80;
 
+          let sortLength = 8 * sortBy.title.length + 80;
+          if (sortMethod === 'author') {
+               sortLength = 8 * sortBy.author.length + 80;
+          } else if (sortMethod === 'format') {
+               sortLength = 8 * sortBy.format.length + 80;
+          } else if (sortMethod === 'dueAsc') {
+               sortLength = 8 * sortBy.due_asc.length + 80;
+          } else if (sortMethod === 'dueDesc') {
+               sortLength = 8 * sortBy.due_desc.length + 80;
+          } else if (sortMethod === 'libraryAccount') {
+               sortLength = 8 * sortBy.library_account.length + 80;
+          } else if (sortMethod === 'timesRenewed') {
+               sortLength = 8 * sortBy.times_renewed.length + 80;
+          }
+
           if (numCheckedOut > 0) {
                return (
-                    <HStack space={2}>
-                         <Button
-                              isLoading={renewAll}
-                              isLoadingText={getTermFromDictionary(language, 'renewing_all', true)}
-                              size="sm"
-                              variant="solid"
-                              colorScheme="primary"
-                              onPress={() => {
-                                   setRenewAll(true);
-                                   renewAllCheckouts(library.baseUrl, language).then((result) => {
-                                        if (result?.confirmRenewalFee && result.confirmRenewalFee) {
-                                             setRenewConfirmationResponse({
-                                                  message: result.api.message,
-                                                  title: result.api.title,
-                                                  confirmRenewalFee: result.confirmRenewalFee ?? false,
-                                                  recordId: record ?? null,
-                                                  action: result.api.action,
-                                                  renewType: 'all',
-                                             });
-                                        }
+                    <VStack space={2}>
+                         <HStack space={2}>
+                              <Button
+                                   isLoading={renewAll}
+                                   isLoadingText={getTermFromDictionary(language, 'renewing_all', true)}
+                                   size="sm"
+                                   variant="solid"
+                                   colorScheme="primary"
+                                   onPress={() => {
+                                        setRenewAll(true);
+                                        renewAllCheckouts(library.baseUrl, language).then((result) => {
+                                             if (result?.confirmRenewalFee && result.confirmRenewalFee) {
+                                                  setRenewConfirmationResponse({
+                                                       message: result.api.message,
+                                                       title: result.api.title,
+                                                       confirmRenewalFee: result.confirmRenewalFee ?? false,
+                                                       recordId: record ?? null,
+                                                       action: result.api.action,
+                                                       renewType: 'all',
+                                                  });
+                                             }
 
-                                        if (result?.confirmRenewalFee && result.confirmRenewalFee) {
-                                             setRenewConfirmationIsOpen(true);
-                                        } else {
-                                             reloadCheckouts();
-                                        }
+                                             if (result?.confirmRenewalFee && result.confirmRenewalFee) {
+                                                  setRenewConfirmationIsOpen(true);
+                                             } else {
+                                                  reloadCheckouts();
+                                             }
 
-                                        setRenewAll(false);
-                                   });
-                              }}
-                              startIcon={<Icon as={MaterialIcons} name="autorenew" size={5} />}>
-                              {getTermFromDictionary(language, 'checkout_renew_all')}
-                         </Button>
-                         <Button
-                              _dark={{
-                                   borderWidth: '1',
-                                   borderColor: 'gray.400',
-                              }}
-                              size="sm"
-                              variant="outline"
-                              onPress={() => {
-                                   setLoading(true);
-                                   reloadCheckouts();
-                              }}>
-                              {getTermFromDictionary(language, 'checkouts_reload')}
-                         </Button>
-                         <FormControl w={holdSourceLength}>
-                              <Select
-                                   isReadOnly={Platform.OS === 'android'}
-                                   name="holdSource"
+                                             setRenewAll(false);
+                                        });
+                                   }}
+                                   startIcon={<Icon as={MaterialIcons} name="autorenew" size={5} />}>
+                                   {getTermFromDictionary(language, 'checkout_renew_all')}
+                              </Button>
+                              <Button
                                    _dark={{
                                         borderWidth: '1',
                                         borderColor: 'gray.400',
                                    }}
-                                   selectedValue={source}
-                                   accessibilityLabel={getTermFromDictionary(language, 'filter_by_source_label')}
-                                   _selectedItem={{
-                                        bg: 'tertiary.300',
-                                        endIcon: <CheckIcon size="5" />,
-                                   }}
-                                   onValueChange={(itemValue) => toggleSource(itemValue)}>
-                                   <Select.Item label={getTermFromDictionary(language, 'filter_by_all') + ' (' + (user.numCheckedOut ?? 0) + ')'} value="all" key={0} />
-                                   <Select.Item label={getTermFromDictionary(language, 'filter_by_ils') + ' (' + (user.numCheckedOutIls ?? 0) + ')'} value="ils" key={1} />
-                                   {user.isValidForOverdrive ? <Select.Item label={filterByLibby + ' (' + (user.numCheckedOutOverDrive ?? 0) + ')'} value="overdrive" key={2} /> : null}
-                                   {user.isValidForHoopla ? <Select.Item label={getTermFromDictionary(language, 'filter_by_hoopla') + ' (' + (user.numCheckedOut_Hoopla ?? 0) + ')'} value="hoopla" key={3} /> : null}
-                                   {user.isValidForCloudLibrary ? <Select.Item label={getTermFromDictionary(language, 'filter_by_cloud_library') + ' (' + (user.numCheckedOut_cloudLibrary ?? 0) + ')'} value="cloud_library" key={4} /> : null}
-                                   {user.isValidForAxis360 ? <Select.Item label={getTermFromDictionary(language, 'filter_by_boundless') + ' (' + (user.numCheckedOut_axis360 ?? 0) + ')'} value="axis360" key={5} /> : null}
-                                   {user.isValidForPalaceProject ? <Select.Item label={getTermFromDictionary(language, 'filter_by_palace_project') + ' (' + (user.numCheckedOut_PalaceProject ?? 0) + ')'} value="palace_project" key={6} /> : null}
-                              </Select>
-                         </FormControl>
-                    </HStack>
+                                   size="sm"
+                                   variant="outline"
+                                   onPress={() => {
+                                        setLoading(true);
+                                        reloadCheckouts();
+                                   }}>
+                                   {getTermFromDictionary(language, 'checkouts_reload')}
+                              </Button>
+                              <FormControl w={holdSourceLength}>
+                                   <Select
+                                        isReadOnly={Platform.OS === 'android'}
+                                        name="holdSource"
+                                        _dark={{
+                                             borderWidth: '1',
+                                             borderColor: 'gray.400',
+                                        }}
+                                        selectedValue={source}
+                                        accessibilityLabel={getTermFromDictionary(language, 'filter_by_source_label')}
+                                        _selectedItem={{
+                                             bg: 'tertiary.300',
+                                             endIcon: <CheckIcon size="5" />,
+                                        }}
+                                        onValueChange={(itemValue) => toggleSource(itemValue)}>
+                                        <Select.Item label={getTermFromDictionary(language, 'filter_by_all') + ' (' + (user.numCheckedOut ?? 0) + ')'} value="all" key={0} />
+                                        <Select.Item label={getTermFromDictionary(language, 'filter_by_ils') + ' (' + (user.numCheckedOutIls ?? 0) + ')'} value="ils" key={1} />
+                                        {user.isValidForOverdrive ? <Select.Item label={filterByLibby + ' (' + (user.numCheckedOutOverDrive ?? 0) + ')'} value="overdrive" key={2} /> : null}
+                                        {user.isValidForHoopla ? <Select.Item label={getTermFromDictionary(language, 'filter_by_hoopla') + ' (' + (user.numCheckedOut_Hoopla ?? 0) + ')'} value="hoopla" key={3} /> : null}
+                                        {user.isValidForCloudLibrary ? <Select.Item label={getTermFromDictionary(language, 'filter_by_cloud_library') + ' (' + (user.numCheckedOut_cloudLibrary ?? 0) + ')'} value="cloud_library" key={4} /> : null}
+                                        {user.isValidForAxis360 ? <Select.Item label={getTermFromDictionary(language, 'filter_by_boundless') + ' (' + (user.numCheckedOut_axis360 ?? 0) + ')'} value="axis360" key={5} /> : null}
+                                        {user.isValidForPalaceProject ? <Select.Item label={getTermFromDictionary(language, 'filter_by_palace_project') + ' (' + (user.numCheckedOut_PalaceProject ?? 0) + ')'} value="palace_project" key={6} /> : null}
+                                   </Select>
+                              </FormControl>
+                         </HStack>
+                         <HStack space={2}>
+                              <FormControl w={sortLength}>
+                                   <Select
+                                        isReadOnly={Platform.OS === 'android'}
+                                        _dark={{
+                                             borderWidth: '1',
+                                             borderColor: 'gray.400',
+                                        }}
+                                        name="sortBy"
+                                        selectedValue={sortMethod}
+                                        accessibilityLabel={getTermFromDictionary(language, 'select_sort_method')}
+                                        _selectedItem={{
+                                             bg: 'tertiary.300',
+                                             endIcon: <CheckIcon size="5" />,
+                                        }}
+                                        onValueChange={(itemValue) => toggleSort(itemValue)}>
+                                        <Select.Item label={sortBy.title} value="sortTitle" key={0} />
+                                        <Select.Item label={sortBy.author} value="author" key={1} />
+                                        <Select.Item label={sortBy.due_asc} value="dueAsc" key={2} />
+                                        <Select.Item label={sortBy.due_desc} value="dueDesc" key={3} />
+                                        <Select.Item label={sortBy.format} value="format" key={4} />
+                                        <Select.Item label={sortBy.library_account} value="libraryAccount" key={5} />
+                                        <Select.Item label={sortBy.times_renewed} value="timesRenewed" key={6} />
+                                   </Select>
+                              </FormControl>
+                         </HStack>
+                    </VStack>
                );
           } else {
                return (
@@ -362,3 +469,29 @@ export const MyCheckouts = () => {
           </SafeAreaView>
      );
 };
+
+function sortCheckouts(checkouts, sort) {
+     let sortedCheckouts = [];
+
+     let sortMethod = sort;
+     let order = 'asc';
+     if (sort === 'sortTitle') {
+          sortMethod = 'title';
+     } else if (sort === 'libraryAccount') {
+          sortMethod = 'user';
+     } else if (sort === 'dueDesc') {
+          sortMethod = 'dueDate';
+          order = 'desc';
+     } else if (sort === 'dueAsc') {
+          sortMethod = 'dueDate';
+     } else if (sort === 'timesRenewed') {
+          sortMethod = 'renewCount';
+          order = 'desc';
+     }
+
+     if (checkouts) {
+          sortedCheckouts = _.orderBy(checkouts, [sortMethod], [order]);
+     }
+
+     return sortedCheckouts;
+}
