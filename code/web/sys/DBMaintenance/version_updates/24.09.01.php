649139f5033d3a1ee3f6c835feb5b37d84705253
Kodi <kodilein@gmail.com>
2024-09-26 08:40:35 -0700

    debuginfo column length
    Increase the length for debuginfo column in the grouped_work_debug_info table to prevent data truncation errors
    Updated release notes

---
+++ code/web/sys/DBMaintenance/version_updates/24.09.01.php
@@ -0,0 +1,25 @@
+<?php
+
+function getUpdates24_09_01(): array {
+	$curTime = time();
+	return [
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+
+		//kodi - ByWater
+		'debug_info_update' => [
+			'title' => 'Update Debug Info Column',
+			'description' => 'Increase size of debuginfo column to accommodate more information.',
+			'sql' => [
+				'ALTER TABLE grouped_work_debug_info CHANGE COLUMN debugInfo debugInfo MEDIUMTEXT',
+			],
+		],
+
+	];
+}
