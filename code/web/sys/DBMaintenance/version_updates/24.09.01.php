c0e2527b2209be98e47e9c1f220ea931eeb3bb1a
Kodi <kodilein@gmail.com>
2024-09-26 08:46:38 -0700

    Update 24.09.01.php

--- code/web/sys/DBMaintenance/version_updates/24.09.01.php
+++ code/web/sys/DBMaintenance/version_updates/24.09.01.php
@@ -4,13 +4,13 @@ function getUpdates24_09_01(): array {
 	$curTime = time();
 	return [
 		/*'name' => [
-			 'title' => '',
-			 'description' => '',
-			 'continueOnError' => false,
-			 'sql' => [
-				 ''
-			 ]
-		 ], //name*/
+			'title' => '',
+			'description' => '',
+			'continueOnError' => false,
+			'sql' => [
+				''
+			]
+		], //name*/
 
 		//kodi - ByWater
 		'debug_info_update' => [
@@ -20,6 +20,5 @@ function getUpdates24_09_01(): array {
 				'ALTER TABLE grouped_work_debug_info CHANGE COLUMN debugInfo debugInfo MEDIUMTEXT',
 			],
 		],
-
 	];
 }
