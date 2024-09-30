c5dc572b90260f159713bd9b11a3816ece51b5ee
Alexander Blanchard <alexander.blanchard@ptfs-europe.com>
2024-09-30 12:07:31 +0100

    refactor: Remove db updates from 24.09.00
    interim commit
    
    interim commit

--- code/web/sys/DBMaintenance/version_updates/24.09.00.php
+++ code/web/sys/DBMaintenance/version_updates/24.09.00.php
@@ -169,51 +169,6 @@ function getUpdates24_09_00(): array {
 		],
 
 		//alexander - PTFS-Europe
-		'update_cookie_management_preferences_more_options' => [
-			'title' => 'Update Cookie Management Preferences: More Options',
-			'description' => 'Update cookie management preferences for user tracking - adding more options',
-			'continueOnError' => false,
-			'sql' => [
-				"ALTER TABLE user ADD COLUMN userCookiePreferenceEvents TINYINT(1) DEFAULT 0",
-				"ALTER TABLE user ADD COLUMN userCookiePreferenceOpenArchives TINYINT(1) DEFAULT 0",
-				"ALTER TABLE user ADD COLUMN userCookiePreferenceWebsite TINYINT(1) DEFAULT 0",
-				"ALTER TABLE user ADD COLUMN userCookiePreferenceExternalSearchServices TINYINT(1) DEFAULT 0",
-			],
-		], 
-		'add_local_analytics_column_to_user' => [
-			'title' => 'Add Local Analytics Column To User',
-			'description' => 'Add a column to hold local analytics tracking choices',
-			'continueOnError' => false,
-			'sql' => [
-				"ALTER TABLE user ADD COLUMN userCookiePreferenceLocalAnalytics TINYINT(1) DEFAULT 0",
-				"UPDATE user
-					INNER JOIN location ON user.homeLocationId = location.locationId
-					INNER JOIN library ON location.libraryId = library.libraryId
-					SET user.userCookiePreferenceLocalAnalytics = CASE
-						WHEN library.cookieStorageConsent = 0 THEN 1
-						ELSE 0
-					END"
-			],
-		],
-		'drop_columns_from_user_table' => [
-			'title' => 'Drop Columns From User Table',
-			'description' => 'Remove unneeded columns from user table',
-			'continueOnError' => false,
-			'sql' => [
-				"ALTER TABLE user DROP COLUMN userCookiePreferenceEvents",
-				"ALTER TABLE user DROP COLUMN userCookiePreferenceOpenArchives",
-				"ALTER TABLE user DROP COLUMN userCookiePreferenceWebsite",
-				"ALTER TABLE user DROP COLUMN userCookiePreferenceExternalSearchServices",
-			],
-		],
-		'add_analytics_data_cleared_flag' => [
-			'title' => 'Add Analytics Data Cleared Flag',
-			'description' => 'Add a flag to ensure analytics data clearing fucntion runs only once',
-			'continueOnError' => false,
-			'sql' => [
-				"ALTER TABLE user ADD COLUMN analyticsDataCleared TINYINT DEFAULT 0",
-			],
-		],
 
 		//chloe - PTFS-Europe
 		'sourceId_allow_255_char' => [
