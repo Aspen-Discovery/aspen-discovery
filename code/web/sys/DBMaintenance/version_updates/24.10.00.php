292a9aea67d87ca19d8ca21cbcc7f951652e7107
Mark Noble <mdnoble73@gmail.com>
2024-10-15 20:44:42 -0600

    Update 24.10.00.php

--- code/web/sys/DBMaintenance/version_updates/24.10.00.php
+++ code/web/sys/DBMaintenance/version_updates/24.10.00.php
@@ -111,13 +111,13 @@ function getUpdates24_10_00(): array {
 			'sql' => [
 				"UPDATE materials_request_status SET isOpen = 1, checkForHolds = 1 where description='Item purchased' and libraryId = -1",
 				"INSERT INTO materials_request_status (description, sendEmailToPatron, emailTemplate, isOpen, isPatronCancel, holdPlacedSuccessfully, libraryId) 
-					VALUES ('Hold Placed', 1, 0, '{title} has been received by the library and you have been added to the hold queue. 
+					VALUES ('Hold Placed', 1, '{title} has been received by the library and you have been added to the hold queue. 
 
-Thank you for your purchase suggestion!', 0, 1, -1)",
+Thank you for your purchase suggestion!', 0, 0, 1, -1)",
 				"INSERT INTO materials_request_status (description, sendEmailToPatron, emailTemplate, isOpen, isPatronCancel, holdFailed, libraryId) 
-					VALUES ('Hold Failed', 1, 0, '{title} has been received by the library, however we were not able to add you to the hold queue. Please ensure that your account is in good standing and then visit our catalog to place your hold.
+					VALUES ('Hold Failed', 1, '{title} has been received by the library, however we were not able to add you to the hold queue. Please ensure that your account is in good standing and then visit our catalog to place your hold.
 
-	Thanks', 0, 1, -1)",
+	Thanks', 0, 0, 1, -1)",
 			]
 		], //update_default_request_statuses
 		'update_default_request_statuses_2' => [
@@ -126,7 +126,7 @@ Thank you for your purchase suggestion!', 0, 1, -1)",
 			'continueOnError' => false,
 			'sql' => [
 				"INSERT INTO materials_request_status (description, sendEmailToPatron, emailTemplate, isOpen, isPatronCancel, holdNotNeeded, libraryId) 
-					VALUES ('Hold Not Needed', 1, 0, '', 0, 1, -1)",
+					VALUES ('Hold Not Needed', 1, '', 0, 0, 1, -1)",
 			]
 		], //update_default_request_statuses_2
 		'materials_request_hold_candidate_generation_log' => [
