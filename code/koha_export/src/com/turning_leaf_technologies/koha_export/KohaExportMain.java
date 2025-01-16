6792ef12bd44fdb0c8f90783372fb898ba55b137
Imani Thomas <imani.thomas@bywatersolutions.com>
2025-01-16 15:38:32 -0500

    DIS-231 Add notification to Koha exporter if offline mode is enabled
    to test:
    * put aspen in offline mode: https://help.aspendiscovery.org/help/admin/SystemAdministration#Cell-1804-PanelBody
    * enter the adb shell and run the command aspen_koha_import
    * you should see a line in the output about the ILS being in offline mode
    * log into aspen and navigate to the ILS export log
    * the same note about the ILS being in offline mode should appear in the latest entry here as well.

--- code/koha_export/src/com/turning_leaf_technologies/koha_export/KohaExportMain.java
+++ code/koha_export/src/com/turning_leaf_technologies/koha_export/KohaExportMain.java
@@ -134,6 +134,8 @@ public class KohaExportMain {
 
 				//Check to see if the ILS connection is offline and don't index
 				if (isOffline()) {
+					logEntry.addNote("ILS is offline, won't index.");
+					logEntry.saveResults();
 					System.exit(0);
 				}
 
