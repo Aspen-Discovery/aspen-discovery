91ac08cafb66f9abb7f22bd8e3ad99a3f3cccd54
Lucas Montoya <lucasmontoya@MacBook-Air-de-Lucas.local>
2025-03-05 19:28:53 -0300

    Add a column to store the 'Enable Payments Debugging' option

--- code/web/sys/DBMaintenance/version_updates/25.03.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.03.00.php
@@ -379,6 +379,8 @@ function getUpdates25_03_00(): array {
 				'ALTER TABLE square_settings ADD COLUMN enablePaymentsDebugging TINYINT(1) DEFAULT 0',
 				'ALTER TABLE stripe_settings ADD COLUMN enablePaymentsDebugging TINYINT(1) DEFAULT 0',
 				'ALTER TABLE propay_settings ADD COLUMN enablePaymentsDebugging TINYINT(1) DEFAULT 0',
+				'ALTER TABLE ncr_payments_settings ADD COLUMN enablePaymentsDebugging TINYINT(1) DEFAULT 0',
+
 			]
 		], //enable_payments_debugging
 
