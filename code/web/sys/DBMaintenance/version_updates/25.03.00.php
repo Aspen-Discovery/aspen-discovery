e76b4319617262e97f7ff1f5a655c587aa360a5f
Lucas Montoya <lucasmontoya@MacBook-Air-de-Lucas.local>
2025-03-05 08:58:58 -0300

    Add a column to store the 'Enable Payments Debugging' option

--- code/web/sys/DBMaintenance/version_updates/25.03.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.03.00.php
@@ -377,6 +377,7 @@ function getUpdates25_03_00(): array {
 			'sql' => [
 				'ALTER TABLE paypal_settings ADD COLUMN enablePaymentsDebugging TINYINT(1) DEFAULT 0',
 				'ALTER TABLE square_settings ADD COLUMN enablePaymentsDebugging TINYINT(1) DEFAULT 0',
+				'ALTER TABLE stripe_settings ADD COLUMN enablePaymentsDebugging TINYINT(1) DEFAULT 0',
 			]
 		], //enable_payments_debugging
 
