db8f94de3a727fc9c4d39b2a68d0acd10cf4d4b2
Mark Noble <mdnoble73@gmail.com>
2025-04-07 16:43:16 -0600

    Create initial files for 25.05

---
+++ code/web/sys/DBMaintenance/version_updates/25.05.00.php
@@ -0,0 +1,38 @@
+<?php
+
+function getUpdates25_05_00(): array {
+	$curTime = time();
+	return [
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+
+		//mark - Grove
+
+		//katherine - Grove
+
+		//kirstien - Grove
+
+		//kodi - Grove
+
+		//Yanjun Li - ByWater
+
+		// Leo Stoyanov - BWS
+
+		//alexander - PTFS-Europe
+
+		//chloe - PTFS-Europe
+
+		//James Staub - Nashville Public Library
+
+		//Lucas Montoya - Theke Solutions
+
+		//other
+
+	];
+}
