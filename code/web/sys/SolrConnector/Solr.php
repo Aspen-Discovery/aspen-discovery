3670be9cf7254125aa2d8c0ea2c46b82ab306fc3
LeoStoyanovByWater <leo.stoyanov@bywatersolutions.com>
2025-05-05 17:09:21 -0500

    DIS-763: Replace the double-hyphen during a Solr query with a space.

--- code/web/sys/SolrConnector/Solr.php
+++ code/web/sys/SolrConnector/Solr.php
@@ -1907,6 +1907,9 @@ abstract class Solr {
 		//Remove any semi-colons that Solr will handle incorrectly.
 		$input = str_replace(';', ' ', $input);
 
+		// Remove any double hyphens that Solr will handle incorrectly.
+		$input = str_replace('--', ' ', $input);
+
 		//Remove slashes occur in the middle of a word that Solr will handle incorrectly.
 		$input = preg_replace("/([0-9a-zA-Z])([\/])([0-9a-zA-Z])/", "$1 $3", $input);
 		$input = preg_replace("/([0-9a-zA-Z])([\\\\])([0-9a-zA-Z])/", "$1 $3", $input);
