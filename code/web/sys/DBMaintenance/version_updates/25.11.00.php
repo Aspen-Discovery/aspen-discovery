b6a8185d98f218fd6349507580f71cbe6eb64fa2
Lucas Montoya <lucasmontoya@theke.io>
2025-10-16 10:17:49 -0300

    Add a new column to store the 'Force Debug Log' checkbox value

--- code/web/sys/DBMaintenance/version_updates/25.11.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.11.00.php
@@ -39,6 +39,13 @@ function getUpdates25_11_00(): array {
 		//James Staub - Nashville Public Library
 
 		//Lucas Montoya - Theke Solutions
+		'forceDebugLog' => [
+			'title' => 'Enable Forced Logging of Debugging Information for WorldPay Payments',
+			'description' => 'Enable to show debugging information about WorldPay payments regardless of whether the user IP is authorized or not',
+			'sql' => [
+				'ALTER TABLE worldpay_settings ADD COLUMN forceDebugLog TINYINT(1) DEFAULT 0',
+			]
+		], //enable_worldpay_debug_logging
 
 		//other
 
