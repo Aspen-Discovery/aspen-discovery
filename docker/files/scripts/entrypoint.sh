5e2eea7fa8e888e330a51dc88e4e7153e3514c74
Lucas Montoya <lucasmontoya@theke.io>
2025-11-13 10:46:54 -0300

    DIS-1596: Remove LOCAL_USER_ID dependency
    - Remove condition that avoids script for keep running if the variables
      is not set.
    
    - Refactor entrypoint code by moving :
        - Matching id code snippet
      out from the service selection logic
    
    - Avoid modifying host permissions by removing source code chown

--- docker/files/scripts/entrypoint.sh
+++ docker/files/scripts/entrypoint.sh
@@ -9,49 +9,24 @@ USER_NAME="www-data"
 SOURCE_DIR="/usr/local/aspen-discovery"
 APACHE_LOG_DIR="/var/log/apache2"
 
-if [ -z "${LOCAL_USER_ID}" ]; then
-	log "LOCAL_USER_ID must be set!. Exiting..."
-	exit 1
+# Adjust user ID if needed
+if [[ -n "${LOCAL_USER_ID}" && "${LOCAL_USER_ID}" != "33" ]]; then
+	log "Setting UID for ${USER_NAME} to ${LOCAL_USER_ID}"
+	usermod -o -u "$LOCAL_USER_ID" "$USER_NAME"
 fi
 
+# Determine service to run
 if [ "$#" -eq 0 ]; then
-
-	log "Starting container with UID=${LOCAL_USER_ID}"
-
-	if [[ "${LOCAL_USER_ID}" != "33" ]]; then
-		log "Setting UID for ${USER_NAME} to ${LOCAL_USER_ID}"
-		usermod -o -u "$LOCAL_USER_ID" "$USER_NAME"
-	fi
-
-	if [[ -d "${SOURCE_DIR}" ]]; then
-		log "Fixing ownership on ${SOURCE_DIR}"
-		chown -R "$USER_NAME" ${SOURCE_DIR}
-	fi
 
 	exec "/start.sh"
 
-
 elif [ "$1" = 'apache' ]; then
 
-	# Adjust permissions if required
-	if [[ "${LOCAL_USER_ID}" != "33" ]]; then
-		log "Setting UID for ${USER_NAME} to ${LOCAL_USER_ID}"
-		usermod -o -u ${LOCAL_USER_ID} "$USER_NAME"
-
-		# Fix permissions due to UID change
-		chown -R ${USER_NAME} "${APACHE_LOG_DIR}"
-	fi
-	chown -R "$USER_NAME" "$SOURCE_DIR"
+	chown -R ${USER_NAME} "${APACHE_LOG_DIR}"
 	exec /apache.sh
 
 elif [ "$1" = 'cron' ]; then
 
-	# Adjust permissions if required
-	if [[ "${LOCAL_USER_ID}" != "33" ]]; then
-		log "Setting UID for ${USER_NAME} to ${LOCAL_USER_ID}"
-		usermod -o -u ${LOCAL_USER_ID} "$USER_NAME"
-	fi
-	chown -R ${USER_NAME} ${SOURCE_DIR}
 	exec /cron.sh
 
 else
