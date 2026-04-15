f52e7e0a9beed89b4e24c5a9c403f869ca3d3d77
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-04-15 18:06:49 +0100

    DIS-2287: chore: DB update

--- code/web/sys/DBMaintenance/version_updates/26.08.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.08.00.php
@@ -104,7 +104,28 @@ function getUpdates26_08_00(): array {
 		//galen
 
 		//chloe
-
+		'add_enable_patron_ils_registration_by_staff' => [
+			'title' => 'Add Enable Patron ILS Registration By Staff Library Setting',
+			'description' => 'Add library setting to enable staff to register new ILS patrons from within Aspen.',
+			'continueOnError' => false,
+			'sql' => [
+				"ALTER TABLE library ADD COLUMN enablePatronIlsRegistrationByStaff TINYINT(1) NOT NULL DEFAULT 0",
+			],
+		], //add_enable_patron_ils_registration_by_staff
+		'add_register_new_ils_patrons_permissions' => [
+			'title' => 'Add Register New ILS Patrons Permission Family',
+			'description' => 'Add Patron Management permissions allowing staff to register new ILS patrons, scoped by home library / location, mirroring the Masquerade scoping pattern.',
+			'continueOnError' => false,
+			'sql' => [
+				"INSERT INTO permissions (sectionName, name, requiredModule, weight, description) VALUES
+					('Patron Management', 'Register New ILS Patrons for any home library', '', 30, 'Allows the user to register new ILS patrons with any home library.'),
+					('Patron Management', 'Register New ILS Patrons for patrons with same home library', '', 31, 'Allows the user to register new ILS patrons whose home library matches the staff member''s.'),
+					('Patron Management', 'Register New ILS Patrons for patrons with same home location', '', 32, 'Allows the user to register new ILS patrons whose home location matches the staff member''s.')
+				",
+				"INSERT INTO role_permissions(roleId, permissionId) VALUES ((SELECT roleId from roles where name='opacAdmin'), (SELECT id from permissions where name='Register New ILS Patrons for any home library'))",
+			],
+		], //add_register_new_ils_patrons_permissions
+	
 		//pedro
 
 		//mark j
