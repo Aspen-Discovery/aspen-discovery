2b3961de3095d76b1141a704a23e74536ac74af6
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-05-06 17:46:59 +0100

    DIS-164: feat(place-booking): get booking action

--- code/web/RecordDrivers/RecordActionGenerator.php
+++ code/web/RecordDrivers/RecordActionGenerator.php
@@ -408,3 +408,16 @@ function getDownloadMultiSupplementalFileAction($id) : array {
 		'type' => 'download_supplemental_file',
 	];
 }
+
+function getBookingAction(string $id, string $itemId) : array {
+	return [
+		'title' => translate([
+			'text' => 'Place Booking',
+			'isPublicFacing' => true,
+		]),
+		'url' => '',
+		'onclick' => "return AspenDiscovery.Record.showPlaceBooking('$id', '$itemId', this);",
+		'requireLogin' => false,
+		'type' => 'ils_booking',
+	];
+}
