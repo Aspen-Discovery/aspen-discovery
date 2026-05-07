2c334d1b08602d033c4e7f851e376c90d6656282
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-05-07 18:22:46 +0100

    DIS-164: feat(ils-integration): build booked ranges
    Accounts for existing bookings, bookings_lead_period, and
    bookings_trail_period if any apply to the selected item.

--- code/web/services/Record/AJAX.php
+++ code/web/services/Record/AJAX.php
@@ -2794,7 +2794,7 @@ class Record_AJAX extends JSON_Action {
 
 		return [
 			'success'     => true,
-			'bookedDates' => $driver->getBookingsForItem((int)$_REQUEST['itemId'], $user),
+			'bookedDates' => $driver->getBookedRanges((int)$_REQUEST['itemId'], $user),
 		];
 	}
 
