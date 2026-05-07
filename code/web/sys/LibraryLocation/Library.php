2ed37609670d2570c6a3601bd7815ad6dfbd474c
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2026-05-07 14:00:37 +0100

    DIS-164: feat(api): expose enableBookings on library info

--- code/web/sys/LibraryLocation/Library.php
+++ code/web/sys/LibraryLocation/Library.php
@@ -6789,6 +6789,7 @@ class Library extends DataObject {
 			'allowRememberPickupLocation' => $this->allowRememberPickupLocation,
 			'allowPickupLocationUpdates' => $this->allowPickupLocationUpdates,
 			'offerImmediateHoldFreeze' => $this->offerImmediateHoldFreeze,
+			'enableBookings' => (int)$this->enableBookings,
 		];
 		if (empty($this->baseUrl)) {
 			$apiInfo['baseUrl'] = $configArray['Site']['url'];
