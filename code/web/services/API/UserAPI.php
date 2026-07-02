be6594660551a9cd6c8c5fb58a05528afe4b7d3c
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2026-07-02 11:24:22 +0100

    DIS-2570: Fix LiDA campaign enroll/unenroll dying on missing userId

--- code/web/services/API/UserAPI.php
+++ code/web/services/API/UserAPI.php
@@ -7225,15 +7225,19 @@ class UserAPI extends AbstractAPI {
 			];
 		}
 		$originalGet = $_GET;
+		$originalRequest = $_REQUEST;
 
 		$_GET['campaignId'] = $campaignId;
 		$_GET['userId'] = $userId;
+		$_REQUEST['campaignId'] = $campaignId;
+		$_REQUEST['userId'] = $userId;
 
 		$ajaxHandler = new MyAccount_AJAX();
 
 		$response = $ajaxHandler->enrollCampaign();
 
 		$_GET = $originalGet;
+		$_REQUEST = $originalRequest;
 
 		return $response;
 	}
@@ -7279,15 +7283,19 @@ class UserAPI extends AbstractAPI {
 		}
 
 		$originalGet = $_GET;
+		$originalRequest = $_REQUEST;
 
 		$_GET['campaignId'] = $campaignId;
 		$_GET['userId'] = $userId;
+		$_REQUEST['campaignId'] = $campaignId;
+		$_REQUEST['userId'] = $userId;
 
 		$ajaxHandler = new MyAccount_AJAX();
 
 		$response = $ajaxHandler->unenrollCampaign();
 
 		$_GET = $originalGet;
+		$_REQUEST = $originalRequest;
 
 		return $response;
 	}
