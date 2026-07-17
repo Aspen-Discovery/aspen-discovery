67c05711fab870ccfe464c88afd6bc65cc5f1951
Mark Jarrell <mark.jarrell@gmail.com>
2026-07-17 17:56:36 -0400

    DIS-2441: Don't display Year In Review in menus when inactive.

--- code/web/sys/Account/User.php
+++ code/web/sys/Account/User.php
@@ -6673,7 +6673,7 @@ class User extends DataObject {
 					$this->_yearInReviewResults = $userYearInReview;
 					$yearInReviewSetting = new YearInReviewSetting();
 					$yearInReviewSetting->id = $userYearInReview->settingId;
-					if ($yearInReviewSetting->find(true)) {
+					if ($yearInReviewSetting->find(true) && $yearInReviewSetting->endDate > time()) {
 						$this->_yearInReviewSetting = $yearInReviewSetting;
 						global $interface;
 						$interface->assign('yearInReviewName', $yearInReviewSetting->name);
