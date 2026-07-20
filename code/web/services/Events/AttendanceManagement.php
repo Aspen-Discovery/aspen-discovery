49aa4c21b17bf636b203583879a07d501fce8b92
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-07-20 14:46:01 +0100

    DIS-2699: feat(attendance-management): include custom event description fields in CSV

--- code/web/services/Events/AttendanceManagement.php
+++ code/web/services/Events/AttendanceManagement.php
@@ -246,7 +246,8 @@ class Events_AttendanceManagement extends Admin_Admin {
 			return;
 		}
 
-		$customFields = $eventType->getFieldSetFieldsByUse(2);
+		$customFields = $eventType->getFieldSetFieldsByUse(1) + $eventType->getFieldSetFieldsByUse(2);
+		$informationFieldValues = $parentEvent->getAllTypeFields();
 		$attendeeCategories = $eventType->getEventTypeAttendeeCategories();
 
 		header('Content-Type: text/csv');
@@ -298,7 +299,7 @@ class Events_AttendanceManagement extends Admin_Admin {
 				[...$this->buildBaseRegistrationRow($registration, $user), $registration->attended ? 'Yes' : 'No']
 			);
 
-			$customFieldValues = $registration->getCustomFieldValues();
+			$customFieldValues = $registration->getCustomFieldValues() + $informationFieldValues;
 			foreach ($customFields as $fieldId => $field) {
 				$raw = $customFieldValues[(int)$fieldId] ?? '';
 				if (($field['type'] ?? '') === 'enum' && isset($field['values'][$raw])) {
