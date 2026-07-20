41264e197949db0b30cf47d8dff62edafe68c9d8
Chloé Zermatten <chloe.zermatten@openfifth.co.uk>
2026-07-20 15:24:26 +0100

    DIS-2699: feat(attendance-management): include event instance id in CSV

--- code/web/services/Events/AttendanceManagement.php
+++ code/web/services/Events/AttendanceManagement.php
@@ -256,6 +256,7 @@ class Events_AttendanceManagement extends Admin_Admin {
 		$output = fopen('php://output', 'w');
 
 		$headers = [
+			'Event instance id',
 			'Event date',
 			'Event title',
 			'Event start time',
@@ -290,6 +291,7 @@ class Events_AttendanceManagement extends Admin_Admin {
 				continue;
 			}
 			$row = array_merge([
+					$eventInstance->id,
 					$eventInstance->date,
 					$parentEvent->title,
 					$eventInstance->time,
