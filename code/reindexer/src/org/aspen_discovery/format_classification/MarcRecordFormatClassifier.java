ac5ca22d0522609160b5880ada4809db1b73dedb
Kodi <kodilein@gmail.com>
2026-07-24 16:13:07 -0700

    DIS-2380: Manga vs. Book format
    Remove "Book" format if printFormats contains "Manga"
    
    Update release notes
    
    Tested on my local instance of Aspen

--- code/reindexer/src/org/aspen_discovery/format_classification/MarcRecordFormatClassifier.java
+++ code/reindexer/src/org/aspen_discovery/format_classification/MarcRecordFormatClassifier.java
@@ -1530,6 +1530,7 @@ public class MarcRecordFormatClassifier {
 		}
 		if (printFormats.contains("Manga")){
 			printFormats.remove("GraphicNovel");
+			printFormats.remove("Book");
 		}
 		if (printFormats.contains("MusicalScore")){
 			printFormats.remove("Book");
