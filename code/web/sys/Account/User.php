2e587729cc266f71eb50529e2a17f0e41c24933a
Mark Noble <mdnoble73@gmail.com>
2026-07-31 10:20:08 -0600

    DIS-2757 Opt user into all channels when adding notification token
    - Test by opting into notifications in LiDA and making sure that all channels are on by default.

--- code/web/sys/Account/User.php
+++ code/web/sys/Account/User.php
@@ -5967,6 +5967,9 @@ class User extends DataObject {
 		$pushToken->deviceModel = $device;
 		$pushToken->onboardAppNotifications = 0;
 		$pushToken->tokenType = $tokenType;
+		$pushToken->notifyAccount = 1;
+		$pushToken->notifyCustom = 1;
+		$pushToken->notifySavedSearch = 1;
 		if ($pushToken->find(true)) {
 			return true;
 		} else {
