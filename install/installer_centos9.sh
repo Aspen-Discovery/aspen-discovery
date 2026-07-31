6d30f89ee5d27c6a5cc90ca71273ec1ac16dbe6d
Mark Noble <mdnoble73@gmail.com>
2026-07-31 16:12:06 -0600

    DIS-2762 Solr 9 upgrade
    - Install Solr 9.10.1 on upgrade
    
    # Conflicts:
    #	install/installer_centos9.sh
    #	install/installer_debian.sh
    #	install/upgrade_26.08.00.sh
    #	install/upgrade_debian_26.08.00.sh

--- install/installer_centos9.sh
+++ install/installer_centos9.sh
@@ -30,7 +30,7 @@ mv /etc/my.cnf /etc/my.cnf.old
 cp my.cnf /etc/my.cnf
 systemctl start mariadb
 systemctl enable mariadb
-yum -y install java-17-openjdk
+yum -y install java-1.8.0-openjdk
 yum -y install unzip
 yum -y install strace
 yum -y install mytop
