e9a4343b5524cbe900971df8012beec2ba159aa2
Mark Noble <mdnoble73@gmail.com>
2026-08-02 12:45:50 -0600

    DIS-2762 Update to Solr 9
    - Update startup scripts for solr 9, remove unneeded modules line from solrconfig files

--- install/upgrade_debian_26.08.00.sh
+++ install/upgrade_debian_26.08.00.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sudo ./install_solr_9.sh
+if [ -z "$1" ]
+  then
+    echo "Please provide the server name to update as the first argument."
+    exit 1
+fi
+
+./install_solr_9.sh
+./update_startup_script_for_solr9.sh "$1"
