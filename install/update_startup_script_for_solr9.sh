c8880f2b304da3e5785e47ad0b2d71c7a1f06986
Mark Noble <mdnoble73@gmail.com>
2026-08-02 12:45:50 -0600

    DIS-2762 Update to Solr 9
    - Update startup scripts for solr 9, remove unneeded modules line from solrconfig files

--- install/update_startup_script_for_solr9.sh
+++ install/update_startup_script_for_solr9.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Updates the Solr path in the startup scripts for a specific site.
-# Usage: ./update_solr_path.sh {sitename}
+# Usage: ./update_startup_script_for_solr9.sh {sitename}
 
 set -e
 
@@ -26,6 +26,9 @@ SH_FILE="${SITE_DIR}/${SITENAME}.sh"
 if [ -f "$SH_FILE" ]; then
     echo "Updating $SH_FILE"
     sed -i "s|$OLD_PATH|$NEW_PATH|g" "$SH_FILE"
+    if ! grep -q "solr.modules=analysis-extras" "$SH_FILE"; then
+        sed -i "s|bin/solr start|bin/solr start -Dsolr.modules=analysis-extras|g" "$SH_FILE"
+    fi
 else
     echo "Warning: $SH_FILE not found."
 fi
@@ -38,6 +41,10 @@ if [ -f "$BAT_FILE" ]; then
     # but we'll check for the full path just in case.
     # In the example seen, it just used 'solr', which might rely on PATH.
     sed -i "s|$OLD_PATH|$NEW_PATH|g" "$BAT_FILE"
+    if ! grep -q "solr.modules=analysis-extras" "$BAT_FILE"; then
+        sed -i "s|solr start|solr start -Dsolr.modules=analysis-extras|g" "$BAT_FILE"
+        sed -i "s|solr restart|solr restart -Dsolr.modules=analysis-extras|g" "$BAT_FILE"
+    fi
 fi
 
 echo "Done."
