2425a4527693e1e884d700f52488e5bcaabbbd45
Kodi <kodilein@gmail.com>
2026-08-24 18:16:09 -0700

    DIS-2821: Allow Publisher to be removed from the Keyword search
    Add setting in indexing profiles to exclude publisher from the keyword index in solr
    Update BaseIndexingSettings.java, IndexingProfile.java, MarcUtil.java, and MarcRecordProcessor.java to use this setting to determine if field 260b/264b are excluded or not
    Update release notes
    
    Tested on my local instance of Aspen

--- code/web/sys/DBMaintenance/version_updates/26.09.00.php
+++ code/web/sys/DBMaintenance/version_updates/26.09.00.php
@@ -20,6 +20,14 @@ function getUpdates26_09_00(): array {
 		//kirstien
 
 		//kodi
+		'publisher_keyword_exclusion' => [
+			'title' => 'Publisher Keyword Exclusion',
+			'description' => 'Add a variable in indexing profiles to determine if publisher info is excluded from the keyword index or not.',
+			'continueOnError' => false,
+			'sql' => [
+				'ALTER TABLE `indexing_profiles` ADD COLUMN `excludePublisherFromKeywordIndex` TINYINT(1) NOT NULL DEFAULT 0;'
+			]
+		]
 
 		//yanjun
 
