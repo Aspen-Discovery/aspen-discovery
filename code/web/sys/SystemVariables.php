672516d33395a2ba64e8f1bad34cc7e2363821c8
Mark Noble <mdnoble73@gmail.com>
2026-09-01 21:25:41 -0600

    DIS-2535 - re-enable Grouped Works V3

--- code/web/sys/SystemVariables.php
+++ code/web/sys/SystemVariables.php
@@ -189,7 +189,7 @@ class SystemVariables extends DataObject {
 						'type' => 'enum',
 						'values' => [
 							2 => 'Version 2 (Edition information)',
-							//3 => 'Version 3 (Optimization of solr fields)'
+							3 => 'Version 3 (Optimization of solr fields)'
 						],
 						'label' => 'Grouped Work Indexing Version',
 						'description' => 'The Solr Core Version to index with.  In 26.08 and above this should be version 3 in most cases.',
@@ -201,7 +201,7 @@ class SystemVariables extends DataObject {
 						'type' => 'enum',
 						'values' => [
 							2 => 'Version 2 (Edition information)',
-							//3 => 'Version 3 (Optimization of solr fields)'
+							3 => 'Version 3 (Optimization of solr fields)'
 						],
 						'label' => 'Grouped Work Search Version',
 						'description' => 'The Solr Core Version to search with.  In 26.08 and above this should be version 3 in most cases.',
