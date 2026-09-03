ea04efd3c3651d0b429beac79a9464a2ef9f63ef
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2026-09-03 09:13:12 +0100

    DIS-2679: Add seed data options to default database export script

--- code/web/cron/createDefaultDatabaseScript.php
+++ code/web/cron/createDefaultDatabaseScript.php
@@ -1,5 +1,6 @@
 <?php
-$serverNameProvidedAsArgument = empty($_SERVER['aspen_server']) && !empty($argv[1]);
+$firstArgumentIsOption = !empty($argv[1]) && str_starts_with($argv[1], '--');
+$serverNameProvidedAsArgument = empty($_SERVER['aspen_server']) && !empty($argv[1]) && !$firstArgumentIsOption;
 if ($serverNameProvidedAsArgument) {
 	$_SERVER['aspen_server'] = $argv[1];
 }
@@ -14,5 +15,24 @@ require_once ROOT_DIR . '/sys/DBMaintenance/DefaultDatabaseExporter.php';
 $localDirectory = $configArray['Site']['local'];
 $installDirectory = $localDirectory . '/../../install/';
 
+function getOptionValue(array $argv, string $option): string {
+	foreach ($argv as $arg) {
+		$isRequestedOption = str_starts_with($arg, "$option=");
+		if ($isRequestedOption) {
+			return substr($arg, strlen("$option="));
+		}
+	}
+	return '';
+}
+
 $exporter = new DefaultDatabaseExporter($aspen_db);
-$exporter->exportToFile($installDirectory . 'aspen.sql');
+
+$exportSeedDataFile = getOptionValue($argv, '--export-seed-data');
+$exportSeedDataRequested = !empty($exportSeedDataFile);
+if ($exportSeedDataRequested) {
+	$exporter->exportSeedDataToFile($exportSeedDataFile);
+	return;
+}
+
+$seedDataFile = getOptionValue($argv, '--seed-data');
+$exporter->exportToFile($installDirectory . 'aspen.sql', $seedDataFile);
